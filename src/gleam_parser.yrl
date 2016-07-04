5ef516c7b0df4b8916f36407177ee5271ddd53a3
Louis Pilfold <louis@lpil.uk>
2016-07-04 20:29:09 +0100

    List parsing

--- src/gleam_parser.yrl
+++ src/gleam_parser.yrl
@@ -1,12 +1,22 @@
 Nonterminals
-document literal.
+document literal list elements element.
 
 Terminals
-num atom string.
+'[' ']' ',' num atom string.
 
 Rootsymbol document.
 
 document -> literal : ['$1'].
+document -> list    : ['$1'].
+
+list -> '[' ']'              : [].
+list -> '[' elements ']'     : '$2'.
+
+elements -> element              : ['$1'].
+elements -> element ','          : ['$1'].
+elements -> element ',' elements : ['$1'|'$3'].
+
+element -> literal : '$1'.
 
 literal -> num    : v('$1').
 literal -> string : v('$1').
--- test/gleam_parser_test.erl
+++ test/gleam_parser_test.erl
@@ -7,8 +7,15 @@ parse(Tokens) -> element(2, gleam_parser:parse(Tokens)).
 -define(assertAST(Code, Tokens),
         ?assertMatch(Tokens, parse(tokens(Code)))).
 
-literals_test() ->
+literal_test() ->
   ?assertAST("1",   [1]),
   ?assertAST("1.2", [1.2]),
   ?assertAST(":ok", [ok]),
   ?assertAST("\"Hello world\"",  [<<"Hello world">>]).
+
+list_test() ->
+  ?assertAST("[]",        [[]]),
+  ?assertAST("[100]",     [[100]]),
+  ?assertAST("[  200,]",  [[200]]),
+  ?assertAST("[2, 4, 8]", [[2, 4, 8]]),
+  ?assertAST("[2,10, ]",  [[2, 10]]).
