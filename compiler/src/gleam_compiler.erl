a327f8c022aea632212fd6d7f7867da21f7c2c23
Louis Pilfold <louis@lpil.uk>
2018-08-09 12:41:12 +0100

    Decompiler WIP
    I wish to find out if it is possible to "decompile" core Erlang into
    Erlang source. If it is possible it would provide an "escape hatch"
    through which Gleam could be compiled into Erlang.

--- compiler/src/gleam_compiler.erl
+++ compiler/src/gleam_compiler.erl
@@ -9,9 +9,13 @@ source_to_binary(Source) ->
 source_to_binary(Source, Options) ->
   {ok, Tokens, _} = gleam_tokenizer:string(Source),
   {ok, #ast_module{} = AST} = gleam_parser:parse(Tokens),
-  {ok, Forms} = gleam_codegen:module(AST, Options),
-  {ok, _, Bin} = compile:forms(Forms, [report, verbose, from_core]),
-  Bin.
+  {ok, Core} = gleam_codegen:module(AST, Options),
+  {ok, _, Beam0} = compile:forms(Core, [report, verbose, from_core, debug_info]),
+
+  % Add debug chunks
+  {ok, AbstractChunk} = get_abstract(Core),
+  {ok, Beam1} = add_chunks(Beam0, [AbstractChunk]),
+  Beam1.
 
 compile_file(Path) ->
   {ok, Source} = file:read_file(Path),
@@ -20,3 +24,11 @@ compile_file(Path) ->
   BeamPath = filename:join(filename:dirname(Path), BeamFileName),
   Beam = source_to_binary(ListSource),
   ok = file:write_file(BeamPath, Beam).
+
+add_chunks(Beam, Chunks) ->
+  {ok, _Name, ExistingChunks} = beam_lib:all_chunks(Beam),
+  {ok, _BeamWithChunks} = beam_lib:build_module(Chunks ++ ExistingChunks).
+
+get_abstract(Core) ->
+  Opts = [],
+  {ok, _Chunk} = gleam_abstract_code:make_chunk(Core, Opts).
