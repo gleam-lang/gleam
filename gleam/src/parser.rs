5821a27b373aeee901bda7b2d26290a6f21abc07
Louis Pilfold <louis@lpil.uk>
2018-12-13 17:25:11 +0000

    Parse nil, let, atom, string, record nil

--- gleam/src/parser.rs
+++ gleam/src/parser.rs
@@ -1,5 +1,6 @@
 use crate::ast::{Expr, Meta};
 use crate::grammar::ExprParser;
+use crate::pattern::Pattern;
 
 pub fn meta(start: usize, end: usize) -> Meta {
     Meta { start, end }
@@ -44,6 +45,73 @@ fn expr_test() {
         Ok(Expr::Float {
             meta: Meta { start: 0, end: 5 },
             value: -1.23
+        })
+    );
+
+    assert_eq!(
+        ExprParser::new().parse(r#""Hello, world!""#),
+        Ok(Expr::String {
+            meta: Meta { start: 0, end: 15 },
+            value: "Hello, world!".to_string(),
+        })
+    );
+
+    assert_eq!(
+        ExprParser::new().parse(r#""quote -> \"""#),
+        Ok(Expr::String {
+            meta: Meta { start: 0, end: 13 },
+            value: "quote -> \\\"".to_string(),
+        })
+    );
+
+    assert_eq!(
+        ExprParser::new().parse(r#"'Hello, world!'"#),
+        Ok(Expr::Atom {
+            meta: Meta { start: 0, end: 15 },
+            value: "Hello, world!".to_string(),
+        })
+    );
+
+    assert_eq!(
+        ExprParser::new().parse(r#""quote -> \'""#),
+        Ok(Expr::String {
+            meta: Meta { start: 0, end: 13 },
+            value: "quote -> \\'".to_string(),
+        })
+    );
+
+    assert_eq!(
+        ExprParser::new().parse("[]"),
+        Ok(Expr::Nil {
+            meta: Meta { start: 0, end: 2 },
+            typ: (),
+        })
+    );
+
+    assert_eq!(
+        ExprParser::new().parse("{}"),
+        Ok(Expr::RecordNil {
+            meta: Meta { start: 0, end: 2 },
+        })
+    );
+
+    assert_eq!(
+        ExprParser::new().parse("x = 1 2"),
+        Ok(Expr::Let {
+            meta: Meta { start: 0, end: 6 },
+            typ: (),
+            pattern: Pattern::Var {
+                meta: Meta { start: 0, end: 1 },
+                name: "x".to_string(),
+            },
+            value: Box::new(Expr::Int {
+                meta: Meta { start: 4, end: 5 },
+                value: 1
+            }),
+            then: Box::new(Expr::Int {
+                meta: Meta { start: 6, end: 7 },
+                value: 2
+            })
         })
     );
 
