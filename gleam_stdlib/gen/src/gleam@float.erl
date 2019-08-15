f03fd242c959c603190ec3038923ee987119c52a
Dirk Gadsden <dirk@esherido.com>
2019-08-15 11:53:05 -0700

    Add `float:compare` function to stdlib
    Fixes #209

--- gleam_stdlib/gen/src/gleam@float.erl
+++ gleam_stdlib/gen/src/gleam@float.erl
@@ -1,13 +1,28 @@
 -module(gleam@float).
 -compile(no_auto_import).
 
--export([parse/1, to_string/1, ceiling/1, floor/1, round/1, truncate/1]).
+-export([parse/1, to_string/1, compare/2, ceiling/1, floor/1, round/1, truncate/1]).
 
 parse(A) ->
     gleam__stdlib:parse_float(A).
 
 to_string(F) ->
     gleam@iodata:to_string(gleam@iodata:from_float(F)).
+
+compare(A, B) ->
+    case A =:= B of
+        true ->
+            eq;
+
+        false ->
+            case A < B of
+                true ->
+                    lt;
+
+                false ->
+                    gt
+            end
+    end.
 
 ceiling(A) ->
     math:ceil(A).
