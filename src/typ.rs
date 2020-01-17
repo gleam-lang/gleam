baa70c6858b14ba434107c3c1cd7264c4cbe10ae
Ahmad Sattar <thehabbos007@gmail.com>
2020-01-17 01:17:08 +0100

    Enclosing type error reporting

--- src/typ.rs
+++ src/typ.rs
@@ -2271,6 +2271,21 @@ enum UnifyError {
     RecursiveType,
 }
 
+fn unify_enclosed_type(
+    e1: &Type,
+    e2: &Type,
+    result: Result<(), UnifyError>,
+) -> Result<(), UnifyError> {
+    // If types cannot unify, show the type error with the enclosing types, e1 and e2.
+    match result {
+        Err(UnifyError::CouldNotUnify { .. }) => Err(UnifyError::CouldNotUnify {
+            expected: (*e1).clone(),
+            given: (*e2).clone(),
+        }),
+
+        _ => result,
+    }
+}
 fn unify(t1: &Type, t2: &Type, env: &mut Env) -> Result<(), UnifyError> {
     if t1 == t2 {
         return Ok(());
@@ -2346,7 +2361,7 @@ fn unify(t1: &Type, t2: &Type, env: &mut Env) -> Result<(), UnifyError> {
             },
         ) if m1 == m2 && n1 == n2 && args1.len() == args2.len() => {
             for (a, b) in args1.iter().zip(args2) {
-                unify(a, b, env)?;
+                unify_enclosed_type(t1, t2, unify(a, b, env))?;
             }
             Ok(())
         }
@@ -2355,7 +2370,7 @@ fn unify(t1: &Type, t2: &Type, env: &mut Env) -> Result<(), UnifyError> {
             if elems1.len() == elems2.len() =>
         {
             for (a, b) in elems1.iter().zip(elems2) {
-                unify(a, b, env)?;
+                unify_enclosed_type(t1, t2, unify(a, b, env))?;
             }
             Ok(())
         }
