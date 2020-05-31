c82a2d83bd0c06cafdc196820deb3f89a9b3ff7c
Louis Pilfold <louis@lpil.uk>
2020-05-31 22:27:20 +0100

    Extract PackageAnalyser

--- src/build.rs
+++ src/build.rs
@@ -1,6 +1,7 @@
 #![allow(warnings)]
 
 mod dep_tree;
+mod package_analyser;
 mod project_root;
 
 use crate::{
@@ -10,6 +11,7 @@ use crate::{
     file, grammar, parser, typ,
 };
 use itertools::Itertools;
+use package_analyser::PackageAnalyser;
 use project_root::ProjectRoot;
 use std::collections::HashMap;
 use std::fs::DirEntry;
@@ -37,7 +39,7 @@ pub fn main(package_config: PackageConfig, root: PathBuf) -> Result<(), Error> {
 }
 
 fn analyse_packages<'a>(
-    root: &ProjectRoot,
+    root: &'a ProjectRoot,
     configs: &'a HashMap<String, PackageConfig>,
     sequence: Vec<&'a str>,
 ) -> Result<HashMap<&'a str, Package<'a>>, Error> {
@@ -46,102 +48,14 @@ fn analyse_packages<'a>(
 
     for name in sequence {
         let config = configs.get(name).gleam_expect("Missing package config");
-        let analysed = analyse_package(root, config, &public_modules)?;
+        let mut analyser = PackageAnalyser::new(root, config);
+        analyser.read_package_source_files()?;
+        let analysed = analyser.analyse(&public_modules)?;
         packages.insert(name, analysed);
         // TODO: insert public modules for package into public_modules
     }
 
     Ok(packages)
-}
-
-fn analyse_package<'a>(
-    root: &ProjectRoot,
-    config: &'a PackageConfig,
-    other_package_modules: &HashMap<&str, typ::Module>,
-) -> Result<Package<'a>, Error> {
-    let package_path = root.default_build_lib_package_path(&config.name);
-    let mut modules = HashMap::with_capacity(20); // A guess at how many modules will be in a package
-
-    for path in file::gleam_files(&package_path) {
-        let name = module_name(root, &package_path, &path);
-        let code = file::read(&path)?;
-        let ast = parse_source(code.as_str(), name.as_str(), &path)?;
-
-        // TODO: parse
-        // TODO: type check
-        // TODO: ensure this is not a duplicate module
-
-        let module = Module { name, code, ast };
-
-        modules.insert(module.name.clone(), module);
-    }
-
-    Ok(Package {
-        name: config.name.as_str(),
-        modules,
-    })
-}
-
-fn parse_source(src: &str, name: &str, path: &PathBuf) -> Result<UntypedModule, Error> {
-    // Strip comments, etc
-    let (cleaned, comments) = parser::strip_extra(src);
-
-    // Parse source into AST
-    let mut module = grammar::ModuleParser::new()
-        .parse(&cleaned)
-        .map_err(|e| Error::Parse {
-            path: path.clone(),
-            src: src.to_string(),
-            error: e.map_token(|crate::grammar::Token(a, b)| (a, b.to_string())),
-        })?;
-
-    // Attach documentation
-    parser::attach_doc_comments(&mut module, &comments.doc_comments);
-    module.documentation = comments
-        .module_comments
-        .iter()
-        .map(|s| s.to_string())
-        .collect();
-
-    // Store the name
-    module.name = name.split("/").map(String::from).collect(); // TODO: store the module name as a string
-
-    Ok(module)
-}
-
-fn module_name(root: &ProjectRoot, package_path: &PathBuf, full_module_path: &PathBuf) -> String {
-    // /path/to/project/_build/default/lib/the_package/src/my/module.gleam
-
-    // src/my/module.gleam
-    let project_path = full_module_path
-        .strip_prefix(package_path)
-        .gleam_expect("Stripping package prefix from module path");
-
-    // my/module.gleam
-    let mut module_path = project_path
-        .strip_prefix("src")
-        .or_else(|_| project_path.strip_prefix("test"))
-        .gleam_expect("Stripping src/test prefix")
-        .to_path_buf();
-
-    // my/module
-    module_path.set_extension("");
-
-    // Stringify
-    let name = module_path
-        .to_str()
-        .gleam_expect("Module name path to str")
-        .to_string();
-
-    // normalise windows paths
-    name.replace("\\", "/")
-}
-
-#[derive(Debug, PartialEq)]
-pub struct Input {
-    pub name: Vec<String>,
-    pub path: PathBuf,
-    pub src: String,
 }
 
 #[derive(Debug)]
