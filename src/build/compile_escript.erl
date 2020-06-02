cd5e9d8fc87a95a2ce06ebafd360cb44d6f90b20
Louis Pilfold <louis@lpil.uk>
2020-06-02 21:16:14 +0100

    Compile Erlang

---
+++ src/build/compile_escript.erl
@@ -0,0 +1,16 @@
+#!/usr/bin/env erlang
+
+% TODO: build in parallel
+main([ProfilePath]) ->
+    ErlangFiles = filelib:wildcard([ProfilePath, "/**/src/*.erl"]),
+    lists:foreach(fun(F) -> compile(F) end, ErlangFiles).
+
+compile(ErlangFile) ->
+    EBin = ebin_path(ErlangFile),
+    ok = filelib:ensure_dir([EBin, $/]),
+    ErlangName = filename:rootname(ErlangFile),
+    {ok, _} = compile:file(ErlangName, [{outdir, EBin}, return_errors]).
+
+ebin_path(File) ->
+    PackageRoot = filename:dirname(filename:dirname(File)),
+    filename:join(PackageRoot, "ebin").
