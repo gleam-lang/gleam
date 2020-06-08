9131adc2e83f996257616dd4b0699de2cb262ed2
Ahmad Sattar <thehabbos007@gmail.com>
2020-06-09 00:33:40 +0200

    Preliminary test pass

--- src/erl.rs
+++ src/erl.rs
@@ -196,7 +196,8 @@ fn statement(statement: &TypedStatement, module: &[String]) -> Option<Document> 
         Statement::CustomType { .. } => None,
         Statement::Import { .. } => None,
         Statement::ExternalType { .. } => None,
-        Statement::ModuleConstant { .. } => None,
+
+        Statement::ModuleConstant { name, value, .. } => Some(mod_const(name, value, module)),
 
         Statement::Fn {
             args, name, body, ..
@@ -216,6 +217,15 @@ fn statement(statement: &TypedStatement, module: &[String]) -> Option<Document> 
             args.len(),
         )),
     }
+}
+
+fn mod_const(name: &str, value: &TypedExpr, module: &[String]) -> Document {
+    let mut env = Env::new(module);
+
+    atom(name.to_string())
+        .append("() -> ")
+        .append(expr(value, &mut env))
+        .append(".")
 }
 
 fn mod_fun(name: &str, args: &[TypedArg], body: &TypedExpr, module: &[String]) -> Document {
