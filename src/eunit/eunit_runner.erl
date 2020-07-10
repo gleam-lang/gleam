a476d66954e9b4120b137628b9558ce27c9ab86a
Ahmad Sattar <thehabbos007@gmail.com>
2020-07-10 21:22:31 +0200

    Fix eunit runner with multiple paths

--- src/eunit/eunit_runner.erl
+++ src/eunit/eunit_runner.erl
@@ -1,10 +1,11 @@
 #!/usr/bin/env erlang
 -mode(compile).
 
-main([Ebin, Args])->
+main([EbinPaths, AllModules])->
     true = code:add_patha(filename:dirname(escript:script_name())),
-    true = code:add_patha(Ebin),
-    Strings = string:tokens(Args, ","),
-    Modules = lists:map(fun(X) -> list_to_atom(X) end, Strings),
+    SeperatedEbinPaths = string:tokens(EbinPaths, ","),
+    ok = code:add_paths(SeperatedEbinPaths),
+    SeperatedModules = string:tokens(AllModules, ","),
+    Modules = lists:map(fun(X) -> list_to_atom(X) end, SeperatedModules),
     code:load_file(eunit_progress),
     halt(case eunit:test(Modules, [inparallel, verbose, no_tty, {report, {eunit_progress, [{colored, true}]}}]) of ok -> 0; error -> 1 end).
