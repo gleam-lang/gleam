59c9875499d0dfe65db2648f0f0a685428a51643
Iris Ward <aditu.venyhandottir@gmail.com>
2020-11-05 12:36:27 +0000

    Test and fix apply_content_change

--- src/language_server/document.rs
+++ src/language_server/document.rs
@@ -37,18 +37,134 @@ fn apply_content_change(contents: &str, change: &TextDocumentContentChangeEvent)
             let end_col = range.end.character as usize;
         
             let unchanged_init = lines[..start_line].join("\n");
-            let unchanged_tail = lines[end_line..].join("\n");
+            let unchanged_tail = lines[end_line+1..].join("\n");
         
             let changed_head = &lines[start_line][..start_col];
             let changed_last = &lines[end_line][end_col..];
-
-            format!("{}{}{}{}{}", unchanged_init, changed_head, change.text, changed_last, unchanged_tail).to_string()
+            
+            format!("{}{}{}{}{}{}{}",
+                unchanged_init,
+                if unchanged_init.is_empty() { "" } else { "\n" },
+                changed_head,
+                change.text,
+                changed_last,
+                if unchanged_tail.is_empty() { "" } else { "\n" },
+                unchanged_tail)
         },
         None => {
             change.text.clone()
         }
+    }    
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use tower_lsp::lsp_types::{ Position, Range };
+
+    #[test]
+    fn apply_content_change_no_range_replaces_all_text() {
+        let change = TextDocumentContentChangeEvent {
+            range: None,
+            range_length: None,
+            text: "Salut, tout la monde!".to_string(),
+        };
+
+        let original = "Hello, world!";
+        let result = apply_content_change(original, &change);
+        let expected = "Salut, tout la monde!";
+
+        assert_eq!(result, expected);
     }
 
+    #[test]
+    fn apply_content_change_empty_range_inserts_text() {
+        let comma_pos = Position{
+            line: 0u64,
+            character: 5u64,
+        };
 
-    
+        let change = TextDocumentContentChangeEvent {
+            range: Some(Range { start: comma_pos, end: comma_pos }),
+            range_length: None,
+            text: ",".to_string(),
+        };
+
+        let original = "Hello world!";
+        let result = apply_content_change(original, &change);
+        let expected = "Hello, world!";
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn apply_content_change_empty_text_deletes_range() {
+        let comma_start = Position{
+            line: 0u64,
+            character: 5u64,
+        };
+        let comma_end = Position{
+            line: 0u64,
+            character: 6u64,
+        };
+
+        let change = TextDocumentContentChangeEvent {
+            range: Some(Range { start: comma_start, end: comma_end }),
+            range_length: None,
+            text: "".to_string(),
+        };
+
+        let original = "Hello, world!";
+        let result = apply_content_change(original, &change);
+        let expected = "Hello world!";
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn apply_content_change_replaces_range() {
+        let range_start = Position{
+            line: 0u64,
+            character: 7u64,
+        };
+        let range_end = Position{
+            line: 0u64,
+            character: 12u64,
+        };
+        let change = TextDocumentContentChangeEvent {
+            range: Some(Range { start: range_start, end: range_end }),
+            range_length: None,
+            text: "everyone".to_string(),
+        };
+
+        let original = "Hello, world!";
+        let result = apply_content_change(original, &change);
+        let expected = "Hello, everyone!";
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn apply_content_change_replaces_range_multiline() {
+        let range_start = Position{
+            line: 2u64,
+            character: 0u64,
+        };
+        let range_end = Position{
+            line: 3u64,
+            character: 1u64,
+        };
+
+        let change = TextDocumentContentChangeEvent {
+            range: Some(Range { start: range_start, end: range_end }),
+            range_length: None,
+            text: "3, 4".to_string(),
+        };
+        let original = "a\nb\nc\nd\ne";
+        let result = apply_content_change(original, &change);
+        let expected = "a\nb\n3, 4\ne";
+
+        assert_eq!(result, expected);
+    }
 }
