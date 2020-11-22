5871f6c310e77847cf69c1bc3dc67881f37a8b6f
Louis Pilfold <louis@lpil.uk>
2020-11-22 17:26:54 +0000

    Erlang app code generator

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,6 +1,8 @@
+mod erlang_app;
 mod erlang_modules;
 mod erlang_record_headers;
 
+pub use erlang_app::ErlangApp;
 pub use erlang_modules::ErlangModules;
 pub use erlang_record_headers::ErlangRecordHeaders;
 
