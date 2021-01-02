f150e1623056ebec7951e4f79ffe3671e789fd1f
greggreg <greg@greggreg.org>
2021-01-02 15:08:57 -0600

    Light weight source linker

--- src/docs/source_links.rs
+++ src/docs/source_links.rs
@@ -3,156 +3,87 @@ use crate::{
     config::{PackageConfig, Repository},
     project::Analysed,
 };
-use codespan_reporting::files::Files;
 use std::path::{Path, PathBuf};
 
-pub trait SourceLinks {
-    fn url(&self, location: &SrcSpan) -> Option<String>;
+pub struct SourceLinker {
+    line_starts: Vec<usize>,
+    url_pattern: Option<(String, String)>,
 }
 
-// Returns None for the urls which we can ignore in the template
-struct NoSourceLinks {}
+impl SourceLinker {
+    pub fn new(
+        project_root: impl AsRef<Path>,
+        project_config: &PackageConfig,
+        module: &Analysed,
+    ) -> Self {
+        let path_in_repo = get_path_in_repo(project_root, &module.path);
 
-impl SourceLinks for NoSourceLinks {
-    fn url(&self, _location: &SrcSpan) -> Option<String> {
-        None
-    }
-}
+        let url_pattern = match &project_config.repository {
+            Repository::GitHub { user, repo } => Some((
+                format!(
+                    "https://github.com/{}/{}/blob/v{}/{}#L",
+                    user, repo, project_config.version, path_in_repo
+                ),
+                "-L".to_string(),
+            )),
+            Repository::GitLab { user, repo } => Some((
+                format!(
+                    "https://gitlab.com/{}/{}/-/blob/v{}/{}#L",
+                    user, repo, project_config.version, path_in_repo
+                ),
+                "-".to_string(),
+            )),
+            Repository::BitBucket { user, repo } => Some((
+                format!(
+                    "https://bitbucket.com/{}/{}/src/v{}/{}#lines-",
+                    user, repo, project_config.version, path_in_repo
+                ),
+                ":".to_string(),
+            )),
+            Repository::Custom { .. } | Repository::None => None,
+        };
 
-// Creates links to the GitHub repository with the #LN-LM anchor syntax for linking directly to a
-// range of line numbers
-struct GitHubSourceLinks {
-    codespan_file: codespan_reporting::files::SimpleFile<String, String>,
-    user: String,
-    repo: String,
-    version: String,
-    relative_path: String,
-}
-
-impl SourceLinks for GitHubSourceLinks {
-    fn url(&self, location: &SrcSpan) -> Option<String> {
-        let (start_line, end_line) = get_lines(&self.codespan_file, location);
-        Some(format!(
-            "https://github.com/{}/{}/blob/v{}/{}#L{}-L{}",
-            &self.user, &self.repo, &self.version, &self.relative_path, start_line, end_line,
-        ))
-    }
-}
-
-// Creates links to the GitLab repository with the #LN-M anchor syntax for linking directly to a
-// range of line numbers
-struct GitLabSourceLinks {
-    codespan_file: codespan_reporting::files::SimpleFile<String, String>,
-    user: String,
-    repo: String,
-    version: String,
-    relative_path: String,
-}
-
-impl SourceLinks for GitLabSourceLinks {
-    fn url(&self, location: &SrcSpan) -> Option<String> {
-        let (start_line, end_line) = get_lines(&self.codespan_file, location);
-        Some(format!(
-            "https://gitlab.com/{}/{}/-/blob/v{}/{}#L{}-{}",
-            &self.user, &self.repo, &self.version, &self.relative_path, start_line, end_line,
-        ))
-    }
-}
-
-// Creates links to the BitBucket repository with the #lines-M:N anchor syntax for linking directly
-// to a range of line numbers
-struct BitBucketSourceLinks {
-    codespan_file: codespan_reporting::files::SimpleFile<String, String>,
-    user: String,
-    repo: String,
-    version: String,
-    relative_path: String,
-}
-
-impl SourceLinks for BitBucketSourceLinks {
-    fn url(&self, location: &SrcSpan) -> Option<String> {
-        let (start_line, end_line) = get_lines(&self.codespan_file, location);
-        Some(format!(
-            "https://bitbucket.com/{}/{}/src/v{}/{}#lines-{}:{}",
-            &self.user, &self.repo, &self.version, &self.relative_path, start_line, end_line,
-        ))
-    }
-}
-
-fn get_lines(
-    codespan_file: &codespan_reporting::files::SimpleFile<String, String>,
-    location: &SrcSpan,
-) -> (usize, usize) {
-    let start_line = codespan_file
-        .line_index((), location.start)
-        .unwrap_or_default()
-        + 1;
-    let end_line = codespan_file
-        .line_index((), location.end)
-        .unwrap_or_default()
-        + 1;
-
-    (start_line, end_line)
-}
-
-pub fn build(
-    project_root: impl AsRef<Path>,
-    project_config: &PackageConfig,
-    module: &Analysed,
-) -> Box<dyn SourceLinks> {
-    match &project_config.repository {
-        Repository::GitHub { user, repo } => {
-            let codespan_file = codespan_reporting::files::SimpleFile::new(
-                module.name.join("/"),
-                module.src.clone(),
-            );
-
-            Box::new(GitHubSourceLinks {
-                codespan_file,
-                relative_path: get_relative_path(project_root, &module.path),
-                user: user.clone(),
-                repo: repo.clone(),
-                version: project_config.version.clone(),
-            })
+        SourceLinker {
+            line_starts: if url_pattern.is_some() {
+                line_starts(&module.src)
+            } else {
+                vec![]
+            },
+            url_pattern,
         }
-        Repository::GitLab { user, repo } => {
-            let codespan_file = codespan_reporting::files::SimpleFile::new(
-                module.name.join("/"),
-                module.src.clone(),
-            );
+    }
+    pub fn url(&self, span: &SrcSpan) -> String {
+        match &self.url_pattern {
+            Some((base, line_sep)) => {
+                let start_line = line_no(&self.line_starts, span.start);
+                let end_line = line_no(&self.line_starts, span.end);
+                format!("{}{}{}{}", base, start_line, line_sep, end_line)
+            }
 
-            Box::new(GitLabSourceLinks {
-                codespan_file,
-                relative_path: get_relative_path(project_root, &module.path),
-                user: user.clone(),
-                repo: repo.clone(),
-                version: project_config.version.clone(),
-            })
+            None => "".to_string(),
         }
-        Repository::BitBucket { user, repo } => {
-            let codespan_file = codespan_reporting::files::SimpleFile::new(
-                module.name.join("/"),
-                module.src.clone(),
-            );
-
-            Box::new(BitBucketSourceLinks {
-                codespan_file,
-                relative_path: get_relative_path(project_root, &module.path),
-                user: user.clone(),
-                repo: repo.clone(),
-                version: project_config.version.clone(),
-            })
-        }
-        // If we either don't have a version or aren't dealing with a GitHub repository then we
-        // can't generate source code links
-        _ => Box::new(NoSourceLinks {}),
     }
 }
 
-fn get_relative_path(project_root: impl AsRef<Path>, path: &PathBuf) -> String {
+// get all the line start byte offsets for a str
+fn line_starts(src: &str) -> Vec<usize> {
+    std::iter::once(0)
+        .chain(src.match_indices('\n').map(|(i, _)| i + 1))
+        .collect()
+}
+
+// get the line number for a byte index
+fn line_no(line_starts: &Vec<usize>, byte_index: usize) -> usize {
+    line_starts
+        .binary_search(&byte_index)
+        .unwrap_or_else(|next_line| next_line - 1)
+        + 1
+}
+
+fn get_path_in_repo(project_root: impl AsRef<Path>, path: &PathBuf) -> String {
     path.strip_prefix(&project_root)
-        .map(|path| path.to_str())
-        .unwrap_or_default()
-        .unwrap_or_default()
+        .ok()
+        .and_then(Path::to_str)
+        .unwrap_or("")
         .to_string()
 }
