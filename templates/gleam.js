7e50c8912919281618ba35aa8c68110a83a19b8b
tynanbe <contact@tynan.be>
2021-02-06 02:15:50 -0600

    Fix default dark mode, history icon; code refactor; add icon-gleam-chasse

--- templates/gleam.js
+++ templates/gleam.js
@@ -13,13 +13,23 @@ window.Gleam = function() {
   self.getProperty = function(property) {
     let value;
     try {
-      value = localStorage.getItem("Gleam." + property);
-    } catch (_error) {}
+      value = localStorage.getItem(`Gleam.${property}`);
+    }
+    catch (_error) {}
     if (-1 < [null, undefined].indexOf(value)) {
-      return gleamConfig[property].value;
+      return gleamConfig[property].values[0].value;
     }
     return value;
   };
+
+  self.icons = function() {
+    return Array.from(arguments).reduce(
+      (acc, name) =>
+        `${acc}
+        <svg class="icon icon-${name}"><use xlink:href="#icon-${name}"></use></svg>`,
+      ""
+    );
+  }
 
   self.scrollToHash = function() {
     const locationHash = arguments[0] || window.location.hash;
@@ -31,9 +41,7 @@ window.Gleam = function() {
 
   self.toggleSidebar = function() {
     const previousState =
-      bodyClasses.contains("drawer-open")
-      ? "open"
-      : "closed";
+      bodyClasses.contains("drawer-open") ? "open" : "closed";
 
     let state;
     if (0 < arguments.length) {
@@ -43,8 +51,8 @@ window.Gleam = function() {
       state = "open" === previousState ? "closed" : "open";
     }
 
-    bodyClasses.remove("drawer-" + previousState);
-    bodyClasses.add("drawer-" + state);
+    bodyClasses.remove(`drawer-${previousState}`);
+    bodyClasses.add(`drawer-${state}`);
 
     if ("open" === state) {
       document.addEventListener("click", closeSidebar, false);
@@ -58,10 +66,50 @@ window.Gleam = function() {
   const bodyClasses = body.classList;
   const sidebar = document.querySelector(".sidebar");
   const sidebarToggles = document.querySelectorAll(".sidebar-toggle");
+  const displayControls = document.createElement("div");
+
+  displayControls.classList.add("display-controls");
+  sidebar.appendChild(displayControls);
 
   /* Private Methods */
 
   const initProperty = function(property) {
+    const config = gleamConfig[property];
+
+    displayControls.insertAdjacentHTML(
+      "beforeend",
+      config.values.reduce(
+        (acc, item, index) => {
+          const tooltip =
+            item.label
+            ? `alt="${item.label}" title="${item.label}"`
+            : "";
+          let inner;
+          if (item.icons) {
+            inner = self.icons(...item.icons);
+          }
+          else if (item.label) {
+            inner = item.label;
+          }
+          else {
+            inner = "";
+          }
+          return `
+            ${acc}
+            <span class="label label-${index}" ${tooltip}>
+              ${inner}
+            </span>
+          `;
+        },
+        `<button
+          id="${property}-toggle"
+          class="control control-${property} toggle toggle-0">
+        `
+      ) + `
+        </button>
+      `
+    );
+
     setProperty(null, property, function() {
       return self.getProperty(property);
     });
@@ -71,9 +119,7 @@ window.Gleam = function() {
     const previousValue = self.getProperty(property);
 
     const update =
-      2 < arguments.length
-      ? arguments[2]
-      : gleamConfig[property].update;
+      2 < arguments.length ? arguments[2] : gleamConfig[property].update;
     const value = update();
 
     try {
@@ -81,14 +127,14 @@ window.Gleam = function() {
     }
     catch (_error) {}
 
-    bodyClasses.remove(property + "-" + previousValue);
-    bodyClasses.add(property + "-" + value);
+    bodyClasses.remove(`${property}-${previousValue}`);
+    bodyClasses.add(`${property}-${value}`);
 
-    const isDefault = value === gleamConfig[property].value;
+    const isDefault = value === gleamConfig[property].values[0].value;
     const toggleClasses =
-      document.querySelector("#" + property + "-toggle").classList;
-    toggleClasses.remove("toggle-" + (isDefault ? "right" : "left"));
-    toggleClasses.add("toggle-" + (isDefault ? "left" : "right"));
+      document.querySelector(`#${property}-toggle`).classList;
+    toggleClasses.remove(`toggle-${isDefault ? 1 : 0}`);
+    toggleClasses.add(`toggle-${isDefault ? 0 : 1}`);
 
     try {
       gleamConfig[property].callback(value);
@@ -101,15 +147,16 @@ window.Gleam = function() {
   const setHashOffset = function() {
     const el = document.createElement("div");
     el.style.cssText =
-      "height: var(--hash-offset);"
-      + "pointer-events: none;"
-      + "position: absolute;"
-      + "visibility: hidden;"
-      + "width: 0;";
+      `
+      height: var(--hash-offset);
+      pointer-events: none;
+      position: absolute;
+      visibility: hidden;
+      width: 0;
+      `;
     body.appendChild(el);
     self.hashOffset = parseInt(
-      getComputedStyle(el).getPropertyValue("height")
-      || "0"
+      getComputedStyle(el).getPropertyValue("height") || "0"
     );
     body.removeChild(el);
   };
@@ -124,7 +171,7 @@ window.Gleam = function() {
   const init = function() {
     for (const property in gleamConfig) {
       initProperty(property);
-      const toggle = document.querySelector("#" + property + "-toggle");
+      const toggle = document.querySelector(`#${property}-toggle`);
       toggle.addEventListener("click", function(event) {
         setProperty(event, property);
       });
