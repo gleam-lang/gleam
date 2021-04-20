ac48f336aff923ebf2dce79cbce26b8d5adefd8d
Peter Saxton <peterhsaxton@gmail.com>
2021-04-20 11:17:51 +0100

    JavaScript number literals (#1055)

--- src/javascript/expression.rs
+++ src/javascript/expression.rs
@@ -21,8 +21,8 @@ impl Generator {
         let document = match expression {
             TypedExpr::String { value, .. } => Ok(string(value)),
 
-            TypedExpr::Int { .. } => unsupported("Integer values"),
-            TypedExpr::Float { .. } => unsupported("Float values"),
+            TypedExpr::Int { value, .. } => Ok(int(value)),
+            TypedExpr::Float { value, .. } => Ok(float(value)),
 
             TypedExpr::List { .. } => unsupported("List"),
 
@@ -41,7 +41,9 @@ impl Generator {
             TypedExpr::Seq { first, then, .. } => self.sequence(first, then),
             TypedExpr::Assignment { .. } => unsupported("Assigning variables"),
 
-            TypedExpr::BinOp { .. } => unsupported("Binary operation"),
+            TypedExpr::BinOp {
+                name, left, right, ..
+            } => self.bin_op(name, left, right),
 
             TypedExpr::Todo { .. } => unsupported("todo keyword"),
 
@@ -111,12 +113,62 @@ impl Generator {
             array(elements.iter().map(|element| gen.wrap_expression(element)))
         })
     }
+
+    fn bin_op<'a>(
+        &mut self,
+        name: &'a BinOp,
+        left: &'a TypedExpr,
+        right: &'a TypedExpr,
+    ) -> Output<'a> {
+        match name {
+            BinOp::And => unsupported("Boolean operator"),
+            BinOp::Or => unsupported("Boolean operator"),
+            BinOp::LtInt | BinOp::LtFloat => self.print_bin_op(left, right, "<"),
+            BinOp::LtEqInt | BinOp::LtEqFloat => self.print_bin_op(left, right, "<="),
+            // https://dmitripavlutin.com/how-to-compare-objects-in-javascript/
+            BinOp::Eq => unsupported("Equality operator"),
+            BinOp::NotEq => unsupported("Equality operator"),
+            BinOp::GtInt | BinOp::GtFloat => self.print_bin_op(left, right, ">"),
+            BinOp::GtEqInt | BinOp::GtEqFloat => self.print_bin_op(left, right, ">="),
+            BinOp::AddInt | BinOp::AddFloat => self.print_bin_op(left, right, "+"),
+            BinOp::SubInt | BinOp::SubFloat => self.print_bin_op(left, right, "-"),
+            BinOp::MultInt | BinOp::MultFloat => self.print_bin_op(left, right, "*"),
+            BinOp::DivInt => Ok(self.print_bin_op(left, right, "/")?.append(" | 0")),
+            BinOp::DivFloat => Ok(self
+                .print_bin_op(left, right, "/")?
+                .surround("(function(x) { return isFinite(x) ? x: 0})(", ")")),
+            BinOp::ModuloInt => self.print_bin_op(left, right, "%"),
+        }
+    }
+
+    fn print_bin_op<'a>(
+        &mut self,
+        left: &'a TypedExpr,
+        right: &'a TypedExpr,
+        op: &'a str,
+    ) -> Output<'a> {
+        let left = self.not_in_tail_position(|gen| gen.expression(left))?;
+        let right = self.not_in_tail_position(|gen| gen.expression(right))?;
+
+        Ok(left
+            .append(" ")
+            .append(op.to_doc())
+            .append(" ")
+            .append(right))
+    }
 }
 
+fn int(value: &str) -> Document<'_> {
+    value.to_doc()
+}
+
+fn float(value: &str) -> Document<'_> {
+    value.to_doc()
+}
 pub fn constant_expression<'a, T, Y>(expression: &'a Constant<T, Y>) -> Output<'a> {
     match expression {
-        Constant::Int { .. } => unsupported("Integer as constant"),
-        Constant::Float { .. } => unsupported("Float as constant"),
+        Constant::Int { value, .. } => Ok(int(value)),
+        Constant::Float { value, .. } => Ok(float(value)),
         Constant::String { value, .. } => Ok(string(&value.as_str())),
         Constant::Tuple { elements, .. } => array(elements.iter().map(|e| constant_expression(&e))),
         Constant::List { .. } => unsupported("List as constant"),
---
+++ src/javascript/tests/numbers.rs
@@ -0,0 +1,118 @@
+use crate::assert_js;
+
+#[test]
+fn integer_literals() {
+    assert_js!(
+        r#"
+fn go() {
+    1
+    2
+    -3
+    4001
+    0b00001111
+    0o17
+    0xF
+    1_000
+}
+"#,
+        r#""use strict";
+
+function go() {
+  1;
+  2;
+  -3;
+  4001;
+  0b00001111;
+  0o17;
+  0xF;
+  return 1_000;
+}
+"#
+    );
+}
+#[test]
+fn float_literals() {
+    assert_js!(
+        r#"
+fn go() {
+    1.5
+    2.0
+    -0.1
+    1.
+}
+"#,
+        r#""use strict";
+
+function go() {
+  1.5;
+  2.0;
+  -0.1;
+  return 1.;
+}
+"#
+    );
+}
+
+#[test]
+fn integer_operators() {
+    assert_js!(
+        r#"
+fn go() {
+    1 + 1 // => 2
+    5 - 1 // => 4
+    5 / 2 // => 2
+    3 * 3 // => 9
+    5 % 2 // => 1
+    2 > 1  // => True
+    2 < 1  // => False
+    2 >= 1 // => True
+    2 <= 1 // => False
+}
+"#,
+        r#""use strict";
+
+function go() {
+  1 + 1;
+  5 - 1;
+  5 / 2 | 0;
+  3 * 3;
+  5 % 2;
+  2 > 1;
+  2 < 1;
+  2 >= 1;
+  return 2 <= 1;
+}
+"#
+    );
+}
+#[test]
+fn float_operators() {
+    assert_js!(
+        r#"
+fn go() {
+    1.0 +. 1.4 // => 2.4
+    5.0 -. 1.5 // => 3.5
+    5.0 /. 2.0 // => 2.5
+    3.0 *. 3.1 // => 9.3
+    
+    2.0 >. 1.0  // => True
+    2.0 <. 1.0  // => False
+    2.0 >=. 1.0 // => True
+    2.0 <=. 1.0 // => False
+}
+"#,
+        r#""use strict";
+
+function go() {
+  1.0 + 1.4;
+  5.0 - 1.5;
+  (function(x) { return isFinite(x) ? x: 0})(5.0 / 2.0);
+  3.0 * 3.1;
+  2.0 > 1.0;
+  2.0 < 1.0;
+  2.0 >= 1.0;
+  return 2.0 <= 1.0;
+}
+"#
+    );
+}
