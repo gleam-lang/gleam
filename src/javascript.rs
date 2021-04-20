fce2531c738e700763a462d0ac3d848d6cb0bbd6
Louis Pilfold <louis@lpil.uk>
2021-04-20 22:11:10 +0100

    JavaScript Generator struct
    To hold state relating the module generation

--- src/javascript.rs
+++ src/javascript.rs
@@ -2,27 +2,112 @@ mod expression;
 #[cfg(test)]
 mod tests;
 
-use crate::{ast::*, fs::Utf8Writer, line_numbers::LineNumbers, pretty::*, type_::Type};
+use crate::{ast::*, fs::Utf8Writer, line_numbers::LineNumbers, pretty::*};
 use itertools::Itertools;
-use std::sync::Arc;
 
 const INDENT: isize = 2;
 
 pub type Output<'a> = Result<Document<'a>, Error>;
+
+#[derive(Debug)]
+pub struct Generator<'a> {
+    line_numbers: &'a LineNumbers,
+    module: &'a TypedModule,
+}
+
+impl<'a> Generator<'a> {
+    pub fn new(line_numbers: &'a LineNumbers, module: &'a TypedModule) -> Self {
+        Self {
+            line_numbers,
+            module,
+        }
+    }
+
+    pub fn compile(&mut self) -> Output<'a> {
+        let statements = self
+            .module
+            .statements
+            .iter()
+            .flat_map(|s| self.statement(s));
+        let statements =
+            Itertools::intersperse(statements, Ok(lines(2))).collect::<Result<Vec<_>, _>>()?;
+        Ok(docvec!(r#""use strict";"#, lines(2), statements, line()))
+    }
+
+    pub fn statement(&mut self, statement: &'a TypedStatement) -> Option<Output<'a>> {
+        match statement {
+            Statement::TypeAlias { .. } => None,
+            Statement::CustomType { .. } => None,
+            Statement::Import { .. } => Some(unsupported("Importing modules")),
+            Statement::ExternalType { .. } => None,
+            Statement::ModuleConstant {
+                public,
+                name,
+                value,
+                ..
+            } => Some(self.module_constant(*public, name, value)),
+            Statement::Fn {
+                arguments,
+                name,
+                body,
+                public,
+                ..
+            } => Some(self.module_function(*public, name, arguments, body)),
+            Statement::ExternalFn { .. } => Some(unsupported("Using an external function")),
+        }
+    }
+
+    fn module_constant(
+        &mut self,
+        public: bool,
+        name: &'a str,
+        value: &'a TypedConstant,
+    ) -> Output<'a> {
+        let head = if public { "export const " } else { "const " };
+        Ok(docvec![
+            head,
+            name,
+            " = ",
+            expression::constant_expression(value)?,
+            ";",
+        ])
+    }
+
+    fn module_function(
+        &mut self,
+        public: bool,
+        name: &'a str,
+        args: &'a [TypedArg],
+        body: &'a TypedExpr,
+    ) -> Output<'a> {
+        let head = if public {
+            "export function "
+        } else {
+            "function "
+        };
+        Ok(docvec![
+            head,
+            name,
+            fun_args(args),
+            " {",
+            docvec![line(), expression::Generator::compile(body)?]
+                .nest(INDENT)
+                .group(),
+            line(),
+            "}",
+        ])
+    }
+}
 
 pub fn module(
     module: &TypedModule,
     line_numbers: &LineNumbers,
     writer: &mut impl Utf8Writer,
 ) -> Result<(), crate::Error> {
-    let statements = module
-        .statements
-        .iter()
-        .flat_map(|s| statement(&module.name, s, &module.name, line_numbers));
-    let statements = Itertools::intersperse(statements, Ok(lines(2)))
-        .collect::<Result<Vec<_>, _>>()
-        .map_err(crate::Error::JavaScript)?;
-    docvec!(r#""use strict";"#, lines(2), statements, line()).pretty_print(80, writer)
+    Generator::new(line_numbers, module)
+        .compile()
+        .map_err(crate::Error::JavaScript)?
+        .pretty_print(80, writer)
 }
 
 #[derive(Debug, Clone, PartialEq)]
@@ -34,81 +119,6 @@ fn unsupported<M: ToString, T>(label: M) -> Result<T, Error> {
     Err(Error::Unsupported {
         feature: label.to_string(),
     })
-}
-
-pub fn statement<'a>(
-    _current_module: &'a [String],
-    statement: &'a TypedStatement,
-    module: &'a [String],
-    line_numbers: &'a LineNumbers,
-) -> Option<Output<'a>> {
-    match statement {
-        Statement::TypeAlias { .. } => None,
-        Statement::CustomType { .. } => None,
-        Statement::Import { .. } => Some(unsupported("Importing modules")),
-        Statement::ExternalType { .. } => None,
-        Statement::ModuleConstant {
-            public,
-            name,
-            value,
-            ..
-        } => Some(module_constant(*public, name, value)),
-        Statement::Fn {
-            arguments,
-            name,
-            body,
-            return_type,
-            public,
-            ..
-        } => Some(module_function(
-            *public,
-            name,
-            arguments,
-            body,
-            module,
-            return_type,
-            line_numbers,
-        )),
-        Statement::ExternalFn { .. } => Some(unsupported("Using an external function")),
-    }
-}
-
-fn module_constant<'a, T, Y>(public: bool, name: &'a str, value: &'a Constant<T, Y>) -> Output<'a> {
-    let head = if public { "export const " } else { "const " };
-    Ok(docvec![
-        head,
-        name,
-        " = ",
-        expression::constant_expression(value)?,
-        ";",
-    ])
-}
-
-fn module_function<'a>(
-    public: bool,
-    name: &'a str,
-    args: &'a [TypedArg],
-    body: &'a TypedExpr,
-    _module: &'a [String],
-    _return_type: &'a Arc<Type>,
-    _line_numbers: &'a LineNumbers,
-) -> Output<'a> {
-    let head = if public {
-        "export function "
-    } else {
-        "function "
-    };
-    Ok(docvec![
-        head,
-        name,
-        fun_args(args),
-        " {",
-        docvec![line(), expression::Generator::compile(body)?]
-            .nest(INDENT)
-            .group(),
-        line(),
-        "}",
-    ])
 }
 
 fn fun_args<'a>(args: &'a [TypedArg]) -> Document<'a> {
