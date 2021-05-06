dad12fcdfa4294b3925bbd072ebe743dffefd207
Peter <peterhsaxton@gmail.com>
2021-05-06 20:02:05 +0100

    test matching against a string

--- src/javascript/tests/strings.rs
+++ src/javascript/tests/strings.rs
@@ -16,3 +16,20 @@ function go() {
 "#
     );
 }
+
+#[test]
+fn string_patterns() {
+    assert_js!(
+        r#"
+fn go(x) {
+  let "Hello" = x
+}
+"#,
+        r#""use strict";
+
+function go(x) {
+  if (!(gleam$tmp === "Hello")) throw new Error("Bad match")
+}
+"#
+    );
+}
