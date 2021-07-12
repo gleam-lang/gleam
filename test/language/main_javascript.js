062e6ec18dd310e98d9ec6c55dba10bd962ba483
Louis Pilfold <louis@lpil.uk>
2021-07-12 10:37:10 +0100

    Remove dead code

--- test/language/main_javascript.js
+++ test/language/main_javascript.js
@@ -1,13 +1,3 @@
 import { main } from "./target-javascript/main.js";
 
-function print(string) {
-  process.stdout.write(string);
-  return string;
-}
-
-function append(a, b) {
-  return `${a}${b}`;
-}
-
-let status = main(print, String, append);
-process.exit(status);
+process.exit(main());
