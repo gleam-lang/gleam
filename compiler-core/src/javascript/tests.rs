2171fa1d030e54db395ff4627c77fd93acb4eeb1
Louis Pilfold <louis@lpil.uk>
2021-08-15 13:51:53 +0100

    Insta snapshot tests

--- compiler-core/src/javascript/tests.rs
+++ compiler-core/src/javascript/tests.rs
@@ -20,6 +20,35 @@ pub static CURRENT_PACKAGE: &str = "thepackage";
 
 #[macro_export]
 macro_rules! assert_js {
+    ($src:expr $(,)?) => {{
+        use crate::javascript::*;
+        use std::path::Path;
+        let mut modules = std::collections::HashMap::new();
+        let mut uid = 0;
+        // DUPE: preludeinsertion
+        // TODO: Currently we do this here and also in the tests. It would be better
+        // to have one place where we create all this required state for use in each
+        // place.
+        let _ = modules.insert("gleam".to_string(), crate::type_::build_prelude(&mut uid));
+
+        let (mut ast, _) = crate::parse::parse_module($src).expect("syntax error");
+        ast.name = vec!["my".to_string(), "mod".to_string()];
+        let ast = crate::type_::infer_module(
+            crate::build::Target::JavaScript,
+            &mut 0,
+            ast,
+            crate::build::Origin::Src,
+            "thepackage",
+            &modules,
+            &mut vec![],
+        )
+        .expect("should successfully infer");
+        let mut output = String::new();
+        let line_numbers = LineNumbers::new($src);
+        module(&ast, &line_numbers, Path::new(""), "", &mut output).unwrap();
+        insta::assert_snapshot!(insta::internals::AutoName, output, $src);
+    }};
+
     ($src:expr, $erl:expr $(,)?) => {{
         use crate::javascript::*;
         use std::path::Path;
