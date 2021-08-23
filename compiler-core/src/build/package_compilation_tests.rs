e9a3c66cacb48f82c8848e492aac6168e3d45abf
Louis Pilfold <louis@lpil.uk>
2021-08-23 16:55:46 +0100

    Import CustomType when used

--- compiler-core/src/build/package_compilation_tests.rs
+++ compiler-core/src/build/package_compilation_tests.rs
@@ -1982,7 +1982,10 @@ const x = two.A"#
             },
             OutputFile {
                 path: PathBuf::from("_build/default/lib/the_package/src/one/two.js"),
-                text: "export class A extends CustomType {}\n".to_string(),
+                text: "import { CustomType } from \"../gleam.js\";
+
+export class A extends CustomType {}\n"
+                    .to_string(),
             },
             OutputFile {
                 path: PathBuf::from("_build/default/lib/the_package/src/two.js"),
