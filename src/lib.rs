8ade2571cc6ab6bde6454df7ddf93cd1f38c9be3
Louis Pilfold <louis@lpil.uk>
2021-10-03 17:24:15 +0100

    Remove export

--- src/lib.rs
+++ src/lib.rs
@@ -19,8 +19,6 @@ use std::{collections::HashMap, convert::TryFrom, convert::TryInto, io::BufReade
 use thiserror::Error;
 use version::{Range, Version};
 
-pub use pubgrub::error::PubGrubError;
-
 #[derive(Debug, Clone)]
 pub struct Config {
     /// Defaults to https://hex.pm/api/
