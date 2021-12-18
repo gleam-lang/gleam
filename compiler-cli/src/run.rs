63890446289ae7526e4b9bdc1161427332e18265
Louis Pilfold <louis@lpil.uk>
2021-12-18 23:53:22 +0000

    Run node

--- compiler-cli/src/run.rs
+++ compiler-cli/src/run.rs
@@ -1,5 +1,9 @@
 use gleam_core::{
-    build::{Mode, Target},
+    build::{
+        Mode,
+        Target::{Erlang, JavaScript},
+    },
+    config::PackageConfig,
     error::Error,
     paths,
 };
@@ -16,7 +20,7 @@ pub fn command(arguments: &[String], which: Which) -> Result<(), Error> {
 
     // Determine which module to run
     let module = match which {
-        Which::Src => config.name,
+        Which::Src => config.name.to_string(),
         Which::Test => format!("{}_test", &config.name),
     };
 
@@ -27,26 +31,10 @@ pub fn command(arguments: &[String], which: Which) -> Result<(), Error> {
     ctrlc::set_handler(move || {}).expect("Error setting Ctrl-C handler");
 
     // Prepare the Erlang shell command
-    let mut command = Command::new("erl");
-
-    // Specify locations of .beam files
-    let packages = paths::build_packages(Mode::Dev, Target::Erlang);
-    for entry in crate::fs::read_dir(&packages)?.filter_map(Result::ok) {
-        let _ = command.arg("-pa").arg(entry.path().join("ebin"));
-    }
-
-    // Run the main function.
-    let _ = command.arg("-eval");
-    let _ = command.arg(&format!("gleam@@main:run({})", &module));
-
-    // Don't run the Erlang shell
-    let _ = command.arg("-noshell");
-
-    // Tell the BEAM that any following argument are for the program
-    let _ = command.arg("-extra");
-    for argument in arguments {
-        let _ = command.arg(argument);
-    }
+    let mut command = match config.target {
+        Erlang => run_erlang_command(&config, &module, arguments),
+        JavaScript => run_javascript_command(&config, &module, arguments),
+    }?;
 
     crate::cli::print_running(&format!("{}.main", module));
 
@@ -58,4 +46,66 @@ pub fn command(arguments: &[String], which: Which) -> Result<(), Error> {
     })?;
 
     std::process::exit(status.code().unwrap_or_default());
+}
+
+fn run_erlang_command(
+    config: &PackageConfig,
+    module: &String,
+    arguments: &[String],
+) -> Result<Command, Error> {
+    let mut command = Command::new("erl");
+
+    // Specify locations of .beam files
+    let packages = paths::build_packages(Mode::Dev, config.target);
+
+    for entry in crate::fs::read_dir(&packages)?.filter_map(Result::ok) {
+        let _ = command.arg("-pa").arg(entry.path().join("ebin"));
+    }
+
+    // Run the main function.
+    let _ = command.arg("-eval");
+    let _ = command.arg(format!("gleam@@main:run({})", &module));
+
+    // Don't run the Erlang shell
+    let _ = command.arg("-noshell");
+
+    // Tell the BEAM that any following argument are for the program
+    let _ = command.arg("-extra");
+    for argument in arguments {
+        let _ = command.arg(argument);
+    }
+    Ok(command)
+}
+
+fn run_javascript_command(
+    config: &PackageConfig,
+    module: &String,
+    arguments: &[String],
+) -> Result<Command, Error> {
+    let mut command = Command::new("node");
+
+    // // Specify locations of .beam files
+    // let packages = paths::build_packages(Mode::Dev, config.target);
+
+    // for entry in crate::fs::read_dir(&packages)?.filter_map(Result::ok) {
+    //     let _ = command.arg("-pa").arg(entry.path().join("ebin"));
+    // }
+
+    // // Run the main function.
+    // let _ = command.arg("-eval");
+    // let _ = command.arg(format!("gleam@@main:run({})", &module));
+
+    // // Don't run the Erlang shell
+    // let _ = command.arg("-noshell");
+
+    // // Tell the BEAM that any following argument are for the program
+    // let _ = command.arg("-extra");
+    // for argument in arguments {
+    //     let _ = command.arg(argument);
+    // }
+
+    let _ = command.arg("-e");
+    let _ = command.arg("console.log('Hello, from Gleam')");
+
+    Ok(command)
 }
