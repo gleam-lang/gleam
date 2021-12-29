70f2a6729de69a46f2e31e15ceccb6f491b5973c
Michael Jones <m.pricejones@gmail.com>
2021-12-29 16:00:42 +0000

    Switch remaining erlang tests to snapshots
    Still need to break them out to separate files but this step is a bit
    more automatable and still gets us a win.

--- compiler-core/src/erlang/tests/statement_if.rs
+++ compiler-core/src/erlang/tests/statement_if.rs
@@ -5,8 +5,7 @@ fn excluded() {
     assert_erl!(
         "if javascript {
   pub fn main() { 1 }
-}",
-        "-module(the_app).\n"
+}"
     );
 }
 
@@ -15,15 +14,6 @@ fn included() {
     assert_erl!(
         "if erlang {
   pub fn main() { 1 }
-}",
-        "-module(the_app).
--compile(no_auto_import).
-
--export([main/0]).
-
--spec main() -> integer().
-main() ->
-    1.
-"
+}"
     );
 }
