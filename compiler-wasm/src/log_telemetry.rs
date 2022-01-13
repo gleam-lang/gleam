a6c9c024f532022422f3120d8f66b60f442bd98c
JohnDoneth <doneth7@gmail.com>
2022-01-13 18:38:02 -0500

    Implement warning for LogTelemetry

--- compiler-wasm/src/log_telemetry.rs
+++ compiler-wasm/src/log_telemetry.rs
@@ -1,4 +1,6 @@
 use gleam_core::build::Telemetry;
+use gleam_core::Warning;
+use termcolor::Buffer;
 
 #[derive(Debug)]
 pub struct LogTelemetry;
@@ -10,5 +12,15 @@ impl Telemetry for LogTelemetry {
 
     fn checking_package(&self, name: &str) {
         tracing::info!("Checking package: {}", name);
+    }
+
+    fn warning(&self, warning: &Warning) {
+        let mut buffer = Buffer::no_color();
+        warning.pretty(&mut buffer);
+
+        let string =
+            std::str::from_utf8(buffer.as_slice()).expect("buffer should be utf-8 encoded");
+
+        tracing::warn!("Warning: {:?}", string);
     }
 }
