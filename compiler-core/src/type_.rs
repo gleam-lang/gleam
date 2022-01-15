7636e5f13f33b6e25c89b94c771bba1cbab2556b
Michał Łępicki <michallepicki@users.noreply.github.com>
2022-01-15 21:11:17 +0100

    try keeping track of user-defined custom types

--- compiler-core/src/type_.rs
+++ compiler-core/src/type_.rs
@@ -1551,8 +1551,6 @@ pub fn register_types<'a>(
                 },
             )?;
 
-            // TODO_EXH_CHECK do we insert type to constructor mapping here? (probably not because can't match on external types?)
-
             // Keep track of private types so we can tell if they are later unused
             if !public {
                 let _ = environment.init_usage(name.clone(), EntityKind::PrivateType, *location);
@@ -1564,7 +1562,7 @@ pub fn register_types<'a>(
             public,
             parameters,
             location,
-            constructors: _,
+            constructors,
             ..
         } => {
             assert_unique_type_name(names, name, location)?;
@@ -1591,7 +1589,8 @@ pub fn register_types<'a>(
                 },
             )?;
 
-            // TODO_EXH_CHECK insert type to constructor mapping
+            let constructor_names = constructors.iter().map(|c| c.name.clone()).collect();
+            environment.insert_type_to_constructors(name.clone(), constructor_names);
 
             // Keep track of private types so we can tell if they are later unused
             if !public {
@@ -1628,8 +1627,6 @@ pub fn register_types<'a>(
                     typ,
                 },
             )?;
-
-            // TODO_EXH_CHECK insert type to constructor mapping, I think
 
             // Keep track of private types so we can tell if they are later unused
             if !public {
@@ -1721,8 +1718,6 @@ pub fn register_import(
                         Ok(_) => (),
                         Err(e) => return Err(e),
                     };
-
-                    // TODO_EXH_CHECK insert type to constructor mapping, I think
 
                     type_imported = true;
                 }
