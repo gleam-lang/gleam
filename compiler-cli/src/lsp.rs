23b19b2d8f41438b02756fe8f7b98111065f98ee
Louis Pilfold <louis@lpil.uk>
2022-02-25 15:18:13 +0000

    Formatting via LSP

--- compiler-cli/src/lsp.rs
+++ compiler-cli/src/lsp.rs
@@ -1,9 +1,13 @@
 // TODO: remove this
 #![allow(clippy::unwrap_used)]
 
+use std::path::PathBuf;
+
 use gleam_core::Result;
 use lsp_server::Message;
-use lsp_types::{InitializeParams, ServerCapabilities};
+use lsp_types::{
+    request::Formatting, InitializeParams, OneOf, Position, Range, ServerCapabilities, TextEdit,
+};
 
 pub fn main() -> Result<()> {
     tracing::info!("language_server_starting");
@@ -13,16 +17,45 @@ pub fn main() -> Result<()> {
     let (connection, io_threads) = lsp_server::Connection::stdio();
 
     // Run the server and wait for the two threads to end (typically by trigger LSP Exit event).
-    let server_capabilities = ServerCapabilities::default();
+    let server_capabilities = ServerCapabilities {
+        text_document_sync: None,
+        selection_range_provider: None,
+        hover_provider: None,
+        completion_provider: None,
+        signature_help_provider: None,
+        definition_provider: None,
+        type_definition_provider: None,
+        implementation_provider: None,
+        references_provider: None,
+        document_highlight_provider: None,
+        document_symbol_provider: None,
+        workspace_symbol_provider: None,
+        code_action_provider: None,
+        code_lens_provider: None,
+        document_formatting_provider: Some(OneOf::Left(true)),
+        document_range_formatting_provider: None,
+        document_on_type_formatting_provider: None,
+        rename_provider: None,
+        document_link_provider: None,
+        color_provider: None,
+        folding_range_provider: None,
+        declaration_provider: None,
+        execute_command_provider: None,
+        workspace: None,
+        call_hierarchy_provider: None,
+        semantic_tokens_provider: None,
+        moniker_provider: None,
+        linked_editing_range_provider: None,
+        experimental: None,
+    };
+
     let server_capabilities_json =
         serde_json::to_value(&server_capabilities).expect("server_capabilities_serde");
 
-    let initialization_params = connection
-        .initialize(server_capabilities_json)
-        // TODO: handle protocol error
-        .unwrap();
+    let initialization_params: InitializeParams =
+        serde_json::from_value(connection.initialize(server_capabilities_json).unwrap()).unwrap();
 
-    main_loop(connection, initialization_params)?;
+    LanguageServer::new(connection, initialization_params).run()?;
     io_threads.join().expect("joining_lsp_threads");
 
     // Shut down gracefully.
@@ -30,44 +63,106 @@ pub fn main() -> Result<()> {
     Ok(())
 }
 
-fn main_loop(connection: lsp_server::Connection, params: serde_json::Value) -> Result<()> {
-    let _params: InitializeParams = serde_json::from_value(params).unwrap();
-    for msg in &connection.receiver {
-        tracing::info!("in {:?}", msg);
-        match msg {
-            Message::Request(req) => {
-                if connection
-                    .handle_shutdown(&req)
-                    // TODO: handle protocol error
-                    .unwrap()
-                {
-                    return Ok(());
-                }
-                eprintln!("got request: {:?}", req);
-                // match cast::<GotoDefinition>(req) {
-                //     Ok((id, params)) => {
-                //         eprintln!("got gotoDefinition request #{}: {:?}", id, params);
-                //         let result = Some(GotoDefinitionResponse::Array(Vec::new()));
-                //         let result = serde_json::to_value(&result).unwrap();
-                //         let resp = Response {
-                //             id,
-                //             result: Some(result),
-                //             error: None,
-                //         };
-                //         connection.sender.send(Message::Response(resp))?;
-                //         continue;
-                //     }
-                //     Err(req) => req,
-                // };
-                // // ...
-            }
-            Message::Response(resp) => {
-                eprintln!("got response: {:?}", resp);
-            }
-            Message::Notification(not) => {
-                eprintln!("got notification: {:?}", not);
-            }
+pub struct LanguageServer {
+    connection: lsp_server::Connection,
+    _params: InitializeParams,
+}
+
+impl LanguageServer {
+    pub fn new(connection: lsp_server::Connection, params: InitializeParams) -> Self {
+        Self {
+            connection,
+            _params: params,
         }
     }
-    Ok(())
+
+    pub fn run(&self) -> Result<()> {
+        for msg in &self.connection.receiver {
+            tracing::debug!("Got message {:?}", msg);
+            match msg {
+                Message::Request(request) => {
+                    if self.connection.handle_shutdown(&request).unwrap() {
+                        return Ok(());
+                    }
+                    let id = request.id.clone();
+                    let response = match self.handle_request(request) {
+                        Ok(result) => lsp_server::Response {
+                            id,
+                            error: None,
+                            result: Some(result),
+                        },
+                        Err(error) => lsp_server::Response {
+                            id,
+                            error: Some(error),
+                            result: None,
+                        },
+                    };
+                    self.connection
+                        .sender
+                        .send(Message::Response(response))
+                        .unwrap();
+                }
+
+                Message::Response(_) => {
+                    // Nothing to do here...
+                }
+
+                Message::Notification(_) => {
+                    // Nothing to do here...
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn handle_request(
+        &self,
+        request: lsp_server::Request,
+    ) -> Result<serde_json::Value, lsp_server::ResponseError> {
+        match request.method.as_str() {
+            "textDocument/formatting" => {
+                let text_edit = format(cast::<Formatting>(request).unwrap())?;
+                Ok(serde_json::to_value(text_edit).unwrap())
+            }
+            _ => unimplemented!("Unsupported LSP request"),
+        }
+    }
+}
+
+fn cast<R>(req: lsp_server::Request) -> Result<R::Params, lsp_server::Request>
+where
+    R: lsp_types::request::Request,
+    R::Params: serde::de::DeserializeOwned,
+{
+    let (_, params) = req.extract(R::METHOD)?;
+    Ok(params)
+}
+
+// TODO: handle unable to read file
+// TODO: handle syntax errors
+fn format(
+    params: lsp_types::DocumentFormattingParams,
+) -> Result<Vec<TextEdit>, lsp_server::ResponseError> {
+    let path = PathBuf::from(params.text_document.uri.path());
+    let src = crate::fs::read(&path).unwrap();
+    let mut new_text = String::new();
+    gleam_core::format::pretty(&mut new_text, &src).unwrap();
+
+    Ok(vec![text_edit_replace(new_text)])
+}
+
+fn text_edit_replace(new_text: String) -> TextEdit {
+    TextEdit {
+        range: Range {
+            start: Position {
+                line: 0,
+                character: 0,
+            },
+            end: Position {
+                line: u32::MAX,
+                character: 0,
+            },
+        },
+        new_text,
+    }
 }
