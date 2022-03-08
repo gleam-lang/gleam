b16adffa20287c389fb7466d4800e321111416ce
Louis Pilfold <louis@lpil.uk>
2022-03-08 20:08:08 +0000

    Convert column and line to byte index

--- compiler-core/src/line_numbers.rs
+++ compiler-core/src/line_numbers.rs
@@ -1,11 +1,13 @@
 #[derive(Debug)]
 pub struct LineNumbers {
     line_starts: Vec<usize>,
+    length: usize,
 }
 
 impl LineNumbers {
     pub fn new(src: &str) -> Self {
         Self {
+            length: src.len(),
             line_starts: std::iter::once(0)
                 .chain(src.match_indices('\n').map(|(i, _)| i + 1))
                 .collect(),
@@ -20,11 +22,37 @@ impl LineNumbers {
             + 1
     }
 
+    // TODO: handle unicode characters that may be more than 1 byte in width
     pub fn line_and_column_number(&self, byte_index: usize) -> LineColumn {
         let line = self.line_number(byte_index);
         let column = byte_index - self.line_starts.get(line - 1).copied().unwrap_or_default() + 1;
         LineColumn { line, column }
     }
+
+    // TODO: handle unicode characters that may be more than 1 byte in width
+    /// 1 indexed line and character to byte index
+    pub fn byte_index(&self, line: u32, character: u32) -> usize {
+        match self.line_starts.get((line - 1) as usize) {
+            Some(line_index) => *line_index + character as usize - 1,
+            None => self.length,
+        }
+    }
+}
+
+#[test]
+fn byte_index() {
+    let src = &r#"import gleam/io
+
+pub fn main() {
+  io.println("Hello, world!")
+}
+"#;
+    let line_numbers = LineNumbers::new(src);
+
+    assert_eq!(line_numbers.byte_index(1, 1), 0);
+    assert_eq!(line_numbers.byte_index(1, 5), 4);
+    assert_eq!(line_numbers.byte_index(100, 1), src.len());
+    assert_eq!(line_numbers.byte_index(3, 1), 17);
 }
 
 #[derive(Debug, Clone, Copy)]
