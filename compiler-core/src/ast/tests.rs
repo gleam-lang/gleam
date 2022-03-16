949f58cce70002a3f652b2af52002936ef0d66ff
Louis Pilfold <louis@lpil.uk>
2022-03-16 18:47:48 +0000

    Find var and seq tests

--- compiler-core/src/ast/tests.rs
+++ compiler-core/src/ast/tests.rs
@@ -1,65 +1,83 @@
 use crate::{
     ast::{SrcSpan, TypedExpr},
-    type_,
+    type_::{self, Environment, ExprTyper, ValueConstructor, ValueConstructorVariant},
+    uid::UniqueIdGenerator,
 };
+
+fn compile_expression(src: &str) -> TypedExpr {
+    let ast = crate::parse::parse_expression_sequence(src).expect("syntax error");
+
+    let mut modules = im::HashMap::new();
+    let ids = UniqueIdGenerator::new();
+    // DUPE: preludeinsertion
+    // TODO: Currently we do this here and also in the tests. It would be better
+    // to have one place where we create all this required state for use in each
+    // place.
+    let _ = modules.insert("gleam".to_string(), type_::build_prelude(&ids));
+    ExprTyper::new(&mut Environment::new(ids, &[], &modules, &mut vec![]))
+        .infer(ast)
+        .expect("should successfully infer")
+}
 
 #[test]
 fn find_node_string() {
-    let expr = TypedExpr::String {
-        location: SrcSpan { start: 5, end: 8 },
-        typ: type_::string(),
-        value: "".into(),
-    };
-
-    assert_eq!(expr.find_node(3), None);
-    assert_eq!(expr.find_node(5), Some(&expr));
-    assert_eq!(expr.find_node(7), Some(&expr));
-    assert_eq!(expr.find_node(8), None);
+    let expr = compile_expression(r#" "ok" "#);
+    assert_eq!(expr.find_node(0), None);
+    assert_eq!(expr.find_node(1), Some(&expr));
+    assert_eq!(expr.find_node(4), Some(&expr));
+    assert_eq!(expr.find_node(5), None);
 }
 
 #[test]
 fn find_node_float() {
-    let expr = TypedExpr::Float {
-        location: SrcSpan { start: 5, end: 8 },
-        typ: type_::float(),
-        value: "".into(),
-    };
-
-    assert_eq!(expr.find_node(3), None);
-    assert_eq!(expr.find_node(5), Some(&expr));
-    assert_eq!(expr.find_node(7), Some(&expr));
-    assert_eq!(expr.find_node(8), None);
+    let expr = compile_expression(r#" 1.02 "#);
+    assert_eq!(expr.find_node(0), None);
+    assert_eq!(expr.find_node(1), Some(&expr));
+    assert_eq!(expr.find_node(4), Some(&expr));
+    assert_eq!(expr.find_node(5), None);
 }
 
 #[test]
 fn find_node_int() {
-    let expr = TypedExpr::Int {
-        location: SrcSpan { start: 5, end: 8 },
-        typ: type_::int(),
-        value: "".into(),
-    };
-
-    assert_eq!(expr.find_node(3), None);
-    assert_eq!(expr.find_node(5), Some(&expr));
-    assert_eq!(expr.find_node(7), Some(&expr));
-    assert_eq!(expr.find_node(8), None);
+    let expr = compile_expression(r#" 1302 "#);
+    assert_eq!(expr.find_node(0), None);
+    assert_eq!(expr.find_node(1), Some(&expr));
+    assert_eq!(expr.find_node(4), Some(&expr));
+    assert_eq!(expr.find_node(5), None);
 }
 
 #[test]
 fn find_node_var() {
-    let expr = TypedExpr::Var {
-        location: SrcSpan { start: 5, end: 8 },
-        constructor: type_::ValueConstructor {
-            public: true,
-            origin: SrcSpan::default(),
-            variant: type_::ValueConstructorVariant::LocalVariable,
+    let expr = compile_expression(
+        r#"let wibble = 1
+wibble"#,
+    );
+
+    let var = TypedExpr::Var {
+        location: SrcSpan { start: 15, end: 21 },
+        constructor: ValueConstructor {
+            public: false,
+            origin: SrcSpan { start: 4, end: 10 },
+            variant: ValueConstructorVariant::LocalVariable,
             type_: type_::int(),
         },
         name: "wibble".into(),
     };
 
-    assert_eq!(expr.find_node(3), None);
-    assert_eq!(expr.find_node(5), Some(&expr));
-    assert_eq!(expr.find_node(7), Some(&expr));
-    assert_eq!(expr.find_node(8), None);
+    assert_eq!(expr.find_node(14), None);
+    assert_eq!(expr.find_node(15), Some(&var));
+    assert_eq!(expr.find_node(20), Some(&var));
+    assert_eq!(expr.find_node(21), None);
+}
+
+#[test]
+fn find_node_sequence() {
+    let expr = compile_expression(r#"1 2 3"#);
+
+    assert!(expr.find_node(0).is_some());
+    assert!(expr.find_node(1).is_none());
+    assert!(expr.find_node(2).is_some());
+    assert!(expr.find_node(3).is_none());
+    assert!(expr.find_node(4).is_some());
+    assert!(expr.find_node(5).is_none());
 }
