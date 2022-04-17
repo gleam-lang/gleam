6c219ddf2171f3404a95f42828e1c9527d537da5
HarryET <29015545+HarryET@users.noreply.github.com>
2022-04-17 16:17:29 +0100

    Clippppy

--- compiler-cli/src/lsp.rs
+++ compiler-cli/src/lsp.rs
@@ -717,9 +717,14 @@ impl LanguageServer {
 }
 
 fn uri_to_module_name(uri: &Url, root: &Path) -> Option<String> {
-    let mut uri_path = decode(&*uri.path().replace("/", "\\")).expect("Invalid formatting").to_string();
+    let mut uri_path = decode(&*uri.path().replace("/", "\\"))
+        .expect("Invalid formatting")
+        .to_string();
     if uri_path.starts_with("/") {
-        uri_path = uri_path.strip_prefix("/").expect("Failed to remove \"/\" prefix").to_string();
+        uri_path = uri_path
+            .strip_prefix("/")
+            .expect("Failed to remove \"/\" prefix")
+            .to_string();
     }
     let path = PathBuf::from(uri_path);
     let components = path
