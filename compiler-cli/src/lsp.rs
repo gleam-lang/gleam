5f4116e2992634944a25e76c57aca7f055d964bf
HarryET <29015545+HarryET@users.noreply.github.com>
2022-04-17 16:16:03 +0100

    Remove index on string

--- compiler-cli/src/lsp.rs
+++ compiler-cli/src/lsp.rs
@@ -721,7 +721,7 @@ impl LanguageServer {
 
 fn uri_to_module_name(uri: &Url, root: &Path) -> Option<String> {
     let mut uri_path = decode(&*uri.path().replace("/", "\\")).expect("Invalid formatting").to_string();
-    if uri_path[0] == "/" {
+    if uri_path.starts_with("/") {
         uri_path = uri_path.strip_prefix("/").expect("Failed to remove \"/\" prefix").to_string();
     }
     let path = PathBuf::from(uri_path);
