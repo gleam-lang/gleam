d6408c7736016a6057af3a3a78611856db3176ce
Louis Pilfold <louis@lpil.uk>
2022-05-22 17:05:11 +0100

    Shell command

--- compiler-cli/templates/erlang-parcel-entrypoint.sh
+++ compiler-cli/templates/erlang-parcel-entrypoint.sh
@@ -15,14 +15,26 @@ run() {
     -extra "$@"
 }
 
+shell() {
+  erl -pa "$BASE"/*/ebin
+}
+
 case "$COMMAND" in
   run)
     shift
     run "$@"
   ;;
 
+  shell)
+    shell
+  ;;
+
   *)
     echo "usage:" >&2
-    echo "  entrypoint.sh run" >&2
+    echo "  entrypoint.sh \$COMMAND" >&2
+    echo "" >&2
+    echo "commands:" >&2
+    echo "  run    Run the project main function" >&2
+    echo "  shell  Run an Erlang shell" >&2
     exit 1
 esac
