b8c2efdb80156fe077c25f96c33d0bfabe348698
Louis Pilfold <louis@lpil.uk>
2022-07-18 14:36:23 +0100

    Fmt

--- compiler-core/src/type_/tests.rs
+++ compiler-core/src/type_/tests.rs
@@ -31,9 +31,9 @@ macro_rules! assert_infer {
 #[macro_export]
 macro_rules! assert_module_infer {
     ($src:expr, $module:expr $(,)?) => {{
-        use $crate::uid::UniqueIdGenerator;
         use itertools::Itertools;
         use $crate::type_::{build_prelude, infer_module};
+        use $crate::uid::UniqueIdGenerator;
         let (ast, _) = $crate::parse::parse_module($src).expect("syntax error");
         let ids = UniqueIdGenerator::new();
         let mut modules = im::HashMap::new();
