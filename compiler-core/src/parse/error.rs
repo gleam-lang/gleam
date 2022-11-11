38f0bdd054204bb46315812dcec1ba09880a73b0
Ian Gonzalez <ian.gl@protonmail.com>
2022-11-11 16:33:37 -0600

    Run formatter

--- compiler-core/src/parse/error.rs
+++ compiler-core/src/parse/error.rs
@@ -203,8 +203,8 @@ utf16_codepoint, utf32_codepoint, signed, unsigned, big, little, native, size, u
             ),
             ParseErrorType::UnexpectedFunction => (
                 "Functions can only be called within other functions.",
-                vec![]
-            )
+                vec![],
+            ),
         }
     }
 }
