fb941e54e0038c3c5ee569bc5c404301110edf30
Louis Pilfold <louis@lpil.uk>
2022-11-15 19:46:06 +0000

    Fix try bug

--- compiler-core/src/erlang/tests/try_.rs
+++ compiler-core/src/erlang/tests/try_.rs
@@ -12,12 +12,27 @@ pub fn main() {
 "#
     );
 }
+
 #[test]
 fn try_in_case_subject() {
     assert_erl!(
         "pub fn x(f) {
   try x = 1 |> f
   Ok(x)
+}"
+    );
+}
+
+// https://github.com/gleam-lang/gleam/issues/1834
+#[test]
+fn in_block() {
+    assert_erl!(
+        "pub fn main() {
+  {
+    try b = Error(Nil);
+    b
+  }
+  Ok(1)
 }"
     );
 }
