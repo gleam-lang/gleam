ab177b657d025d0f95f7b3ea41d4ad996a01b864
Louis Pilfold <louis@lpil.uk>
2022-11-18 15:28:36 +0000

    Fix wasm test

--- compiler-wasm/src/lib.rs
+++ compiler-wasm/src/lib.rs
@@ -264,7 +264,7 @@ mod test {
         .unwrap();
 
         assert_eq!(
-            result.get("build/dev/erlang/gleam-wasm/build/main.erl"),
+            result.get("build/dev/erlang/gleam-wasm/_gleam_artefacts/main.erl"),
             Some(&String::from("-module(main).\n-compile(no_auto_import).\n\n-export([main/0]).\n\n-spec main() -> nil.\nmain() ->\n    some_library:function(<<\"Hello, world!\"/utf8>>).\n"))
         );
     }
