b35ecd3c58b9ca4448392837c4ffe70c877d0b57
Louis Pilfold <louis@lpil.uk>
2022-11-18 14:41:38 +0000

    Change build dir structure to make it nicer to import from JS

--- compiler-cli/src/compile_package.rs
+++ compiler-cli/src/compile_package.rs
@@ -5,7 +5,7 @@ use crate::{
 };
 use gleam_core::{
     build::{Mode, PackageCompiler, Target, TargetCodegenConfiguration},
-    metadata,
+    metadata, paths,
     type_::Module,
     uid::UniqueIdGenerator,
     Result,
@@ -58,7 +58,7 @@ fn load_libraries(ids: &UniqueIdGenerator, lib: &Path) -> Result<im::HashMap<Str
     tracing::info!("Reading precompiled module metadata files");
     let mut manifests = im::HashMap::new();
     for lib in fs::read_dir(lib)?.filter_map(Result::ok) {
-        let path = lib.path().join("build");
+        let path = lib.path().join(paths::ARTEFACT_DIRECTORY_NAME);
         if !path.is_dir() {
             continue;
         }
