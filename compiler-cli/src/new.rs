89e305f894f94dfea715d00bc62e74bec06666cd
Louis Pilfold <louis@lpil.uk>
2022-11-19 14:09:04 +0000

    New stdlib

--- compiler-cli/src/new.rs
+++ compiler-cli/src/new.rs
@@ -11,7 +11,7 @@ use strum::{Display, EnumString, EnumVariantNames};
 
 use crate::NewOptions;
 
-const GLEAM_STDLIB_VERSION: &str = "0.24";
+const GLEAM_STDLIB_VERSION: &str = "0.25";
 const GLEEUNIT_VERSION: &str = "0.6";
 const ERLANG_OTP_VERSION: &str = "25.1";
 const ELIXIR_VERSION: &str = "1.14.1";
