63f2c1962c8d73669c4e4b2a5394359412932a0c
Dave Parfitt <bookshelfdave@protonmail.com>
2022-12-18 14:59:52 -0500

    add gleam new flags to control git init and .github content creation

--- compiler-cli/src/new.rs
+++ compiler-cli/src/new.rs
@@ -72,14 +72,22 @@ impl Creator {
         crate::fs::mkdir(&self.root)?;
         crate::fs::mkdir(&self.src)?;
         crate::fs::mkdir(&self.test)?;
-        crate::fs::mkdir(&self.github)?;
-        crate::fs::mkdir(&self.workflows)?;
-        crate::fs::git_init(&self.root)?;
+        if self.options.github_init {
+            crate::fs::mkdir(&self.github)?;
+            crate::fs::mkdir(&self.workflows)?;
+        }
+        if self.options.git_init {
+            crate::fs::git_init(&self.root)?;
+        }
 
         match self.options.template {
             Template::Lib => {
-                self.gitignore()?;
-                self.github_ci()?;
+                if self.options.git_init {
+                    self.gitignore()?;
+                }
+                if self.options.github_init {
+                    self.github_ci()?;
+                }
                 self.readme()?;
                 self.gleam_toml()?;
                 self.src_module()?;
--- compiler-cli/src/new/tests.rs
+++ compiler-cli/src/new/tests.rs
@@ -9,6 +9,8 @@ fn new() {
             template: super::Template::Lib,
             name: None,
             description: "Wibble wobble".into(),
+            git_init: false,
+            github_init: false,
         },
         "1.0.0-gleam",
     )
@@ -16,16 +18,61 @@ fn new() {
 
     creator.run().unwrap();
 
-    assert!(path.join(".git").exists());
+    assert!(!path.join(".git").exists());
     assert!(path.join("README.md").exists());
     assert!(path.join("gleam.toml").exists());
     assert!(path.join("src/my_project.gleam").exists());
     assert!(path.join("test/my_project_test.gleam").exists());
-    assert!(path.join(".github/workflows/test.yml").exists());
+    assert!(!path.join(".github/workflows/test.yml").exists());
 
     let toml = crate::fs::read(path.join("gleam.toml")).unwrap();
     assert!(toml.contains("name = \"my_project\""));
     assert!(toml.contains("description = \"Wibble wobble\""));
+}
+
+#[test]
+fn new_with_git_init() {
+    let tmp = tempfile::tempdir().unwrap();
+    let path = tmp.path().join("my_project");
+
+    let creator = super::Creator::new(
+        super::NewOptions {
+            project_root: path.to_str().unwrap().to_string(),
+            template: super::Template::Lib,
+            name: None,
+            description: "Wibble wobble".into(),
+            git_init: true,
+            github_init: false,
+        },
+        "1.0.0-gleam",
+    )
+    .unwrap();
+    creator.run().unwrap();
+    assert!(path.join(".git").exists());
+    assert!(!path.join(".github").exists());
+}
+
+#[test]
+fn new_with_github_init() {
+    let tmp = tempfile::tempdir().unwrap();
+    let path = tmp.path().join("my_project");
+
+    let creator = super::Creator::new(
+        super::NewOptions {
+            project_root: path.to_str().unwrap().to_string(),
+            template: super::Template::Lib,
+            name: None,
+            description: "Wibble wobble".into(),
+            git_init: false,
+            github_init: true,
+        },
+        "1.0.0-gleam",
+    )
+    .unwrap();
+    creator.run().unwrap();
+    assert!(path.join(".github").exists());
+    assert!(!path.join(".git").exists());
+    assert!(path.join(".github/workflows/test.yml").exists());
 }
 
 #[test]
@@ -39,6 +86,8 @@ fn invalid_path() {
             template: super::Template::Lib,
             name: None,
             description: "Wibble wobble".into(),
+            git_init: false,
+            github_init: false,
         },
         "1.0.0-gleam",
     )
@@ -56,6 +105,8 @@ fn invalid_name() {
             template: super::Template::Lib,
             name: Some("-".into()),
             description: "Wibble wobble".into(),
+            git_init: false,
+            github_init: false,
         },
         "1.0.0-gleam",
     )
