443b415837d405dfb5cc25c5509d16410504e491
Louis Pilfold <louis@lpil.uk>
2022-12-24 11:36:55 +0000

    Clippy!

--- compiler-core/src/javascript/tests.rs
+++ compiler-core/src/javascript/tests.rs
@@ -117,7 +117,7 @@ macro_rules! assert_js {
         // place.
         let _ = modules.insert("gleam".to_string(), $crate::type_::build_prelude(&ids));
 
-        let (mut ast, _) = crate::parse::parse_module($src).expect("syntax error");
+        let (mut ast, _) = $crate::parse::parse_module($src).expect("syntax error");
         ast.name = vec!["my".to_string(), "mod".to_string()];
         let ast = crate::type_::infer_module(
             crate::build::Target::JavaScript,
@@ -254,7 +254,7 @@ macro_rules! assert_ts_def {
         // place.
         let _ = modules.insert("gleam".to_string(), $crate::type_::build_prelude(&ids));
 
-        let (mut ast, _) = crate::parse::parse_module($src).expect("syntax error");
+        let (mut ast, _) = $crate::parse::parse_module($src).expect("syntax error");
         ast.name = vec!["my".to_string(), "mod".to_string()];
         let ast = crate::type_::infer_module(
             crate::build::Target::JavaScript,
