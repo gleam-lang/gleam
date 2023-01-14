6942a247db2225d896d0b1e5f546ac2531cb626c
Louis Pilfold <louis@lpil.uk>
2023-01-14 19:37:48 +0000

    Check more efficiently

--- compiler-cli/src/config.rs
+++ compiler-cli/src/config.rs
@@ -19,3 +19,16 @@ pub fn read(config_path: PathBuf) -> Result<PackageConfig, Error> {
         err: Some(e.to_string()),
     })
 }
+
+pub fn ensure_config_exists() -> Result<(), Error> {
+    let path = paths::root_config();
+    if !path.is_file() {
+        return Err(Error::FileIo {
+            action: FileIoAction::Parse,
+            kind: FileKind::File,
+            path,
+            err: Some("File not found".into()),
+        });
+    }
+    return Ok(());
+}
