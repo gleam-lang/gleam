0b95b05bbd3979822fd9adef27fb4c8e1faae4cd
Louis Pilfold <louis@lpil.uk>
2023-01-27 18:10:24 +0000

    Use

--- compiler-core/src/ast/untyped.rs
+++ compiler-core/src/ast/untyped.rs
@@ -232,3 +232,11 @@ pub struct Use {
     pub call: Box<UntypedExpr>,
     pub assignments: Vec<(AssignName, SrcSpan)>,
 }
+impl Use {
+    pub(crate) fn assigned_names(&self) -> impl Iterator<Item = &str> {
+        self.assignments
+            .iter()
+            .map(|(name, _)| name)
+            .flat_map(AssignName::assigned_name)
+    }
+}
