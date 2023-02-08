09d6a0b840cca83b40f537ab9c464f5d822d1f61
Louis Pilfold <louis@lpil.uk>
2023-02-08 17:15:12 +0000

    More patterns

--- compiler-core/src/call_graph/into_dependency_order_tests.rs
+++ compiler-core/src/call_graph/into_dependency_order_tests.rs
@@ -315,6 +315,71 @@ fn pattern_underscore() {
     assert_eq!(parse_and_order(&functions).unwrap(), vec![vec!["a"]]);
 }
 
+#[test]
+fn pattern_concat() {
+    let functions = [
+        Input::Module("a", r#"{ let "a" <> c = b c }"#),
+        Input::Module("b", r#"123"#),
+        Input::External("c"),
+    ];
+    assert_eq!(
+        parse_and_order(&functions).unwrap(),
+        vec![vec!["b"], vec!["a"], vec!["c"]]
+    );
+}
+
+#[test]
+fn pattern_tuple() {
+    let functions = [
+        Input::Module("a", r#"{ let #(a, c) = b a c }"#),
+        Input::Module("b", r#"123"#),
+        Input::External("c"),
+    ];
+    assert_eq!(
+        parse_and_order(&functions).unwrap(),
+        vec![vec!["b"], vec!["a"], vec!["c"]]
+    );
+}
+
+#[test]
+fn pattern_list() {
+    let functions = [
+        Input::Module("a", r#"{ let [a, c] = b a c }"#),
+        Input::Module("b", r#"123"#),
+        Input::External("c"),
+    ];
+    assert_eq!(
+        parse_and_order(&functions).unwrap(),
+        vec![vec!["b"], vec!["a"], vec!["c"]]
+    );
+}
+
+#[test]
+fn pattern_list_spread() {
+    let functions = [
+        Input::Module("a", r#"{ let [a, ..c] = b a c }"#),
+        Input::Module("b", r#"123"#),
+        Input::External("c"),
+    ];
+    assert_eq!(
+        parse_and_order(&functions).unwrap(),
+        vec![vec!["b"], vec!["a"], vec!["c"]]
+    );
+}
+
+#[test]
+fn pattern_bit_string_segment_size_var_usage() {
+    let functions = [
+        Input::Module("a", r#"{ let <<y:size(b)>> = c y }"#),
+        Input::Module("b", r#"123"#),
+        Input::External("c"),
+    ];
+    assert_eq!(
+        parse_and_order(&functions).unwrap(),
+        vec![vec!["b"], vec!["c"], vec!["a"]]
+    );
+}
+
 fn parse_and_order(functions: &[Input]) -> Result<Vec<Vec<SmolStr>>> {
     let functions = functions
         .iter()
