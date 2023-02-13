33cd49190b3b6a5e565a965844a54ab1fb07058e
Louis Pilfold <louis@lpil.uk>
2023-02-13 22:03:26 +0000

    Hey neat, a passing test

--- compiler-core/src/fix/tests.rs
+++ compiler-core/src/fix/tests.rs
@@ -8,3 +8,22 @@ fn fix(src: &str) -> String {
 fn empty() {
     assert_eq!(fix(""), "\n")
 }
+
+#[test]
+fn import_needed() {
+    assert_eq!(
+        fix("
+pub fn main(y) {
+  try x = y
+  y
+}
+"),
+        "import gleam/result
+
+pub fn main(y) {
+  use x <- result.then(y)
+  y
+}
+"
+    )
+}
