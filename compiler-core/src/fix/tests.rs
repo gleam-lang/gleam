2719d71d08520cbfdf21f47f2128732d77b2e1bb
Louis Pilfold <louis@lpil.uk>
2023-02-13 22:27:19 +0000

    More crawling

--- compiler-core/src/fix/tests.rs
+++ compiler-core/src/fix/tests.rs
@@ -27,3 +27,67 @@ pub fn main(y) {
 "
     )
 }
+
+#[test]
+fn import_not_needed() {
+    assert_eq!(
+        fix("import gleam/result
+
+pub fn main(y) {
+  try x = y
+  y
+}
+"),
+        "import gleam/result
+
+pub fn main(y) {
+  use x <- result.then(y)
+  y
+}
+"
+    )
+}
+
+#[test]
+fn discarding_the_value() {
+    assert_eq!(
+        fix("import gleam/result
+
+pub fn main(y) {
+  try _ = y
+  y
+}
+"),
+        "import gleam/result
+
+pub fn main(y) {
+  use _ <- result.then(y)
+  y
+}
+"
+    )
+}
+
+#[test]
+fn module_name_already_taken() {
+    assert_eq!(
+        fix("import one/result
+
+pub fn main(y) {
+  try _ = y
+  y
+}
+"),
+        "import gleam/result as gleam_result
+
+import one/result
+
+pub fn main(y) {
+  use _ <- gleam_result.then(y)
+  y
+}
+"
+    )
+}
+
+// TODO: test for when the `result` and `gleam_result` names are taken.
