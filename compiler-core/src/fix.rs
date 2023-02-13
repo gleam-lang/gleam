72ae9d52199a5e8a8341b4c4c0045b529b2cd312
Louis Pilfold <louis@lpil.uk>
2023-02-13 22:03:26 +0000

    Hey neat, a passing test

--- compiler-core/src/fix.rs
+++ compiler-core/src/fix.rs
@@ -7,7 +7,10 @@ use smol_str::SmolStr;
 use strum::IntoEnumIterator;
 
 use crate::{
-    ast::{Function, Import, Statement, TargetGroup, UntypedExpr, UntypedModule, UntypedStatement},
+    ast::{
+        AssignName, AssignmentKind, CallArg, Import, Pattern, Statement, TargetGroup, UntypedExpr,
+        UntypedModule, UntypedPattern, UntypedStatement, Use,
+    },
     build::Target,
     format::{Formatter, Intermediate},
     Error, Result,
@@ -76,7 +79,7 @@ impl Fixer {
         .fix_module(module)
     }
 
-    fn fix_module(&self, module: &mut UntypedModule) {
+    fn fix_module(&mut self, module: &mut UntypedModule) {
         // Rewrite any `try`s into `use`s.
         for group in module.statements.iter_mut() {
             match group {
@@ -96,13 +99,13 @@ impl Fixer {
                 ResultModule::Existing(_) => (),
                 ResultModule::Insert(name) => {
                     let import = result_module_import_statement(&name);
-                    module.statements.push(TargetGroup::Any(vec![import]));
+                    module.statements.insert(0, TargetGroup::Any(vec![import]));
                 }
             }
         }
     }
 
-    fn fix_statement(&self, statement: &mut UntypedStatement) {
+    fn fix_statement(&mut self, statement: &mut UntypedStatement) {
         match statement {
             Statement::Function(f) => self.fix_expression(&mut f.body),
             Statement::TypeAlias(_)
@@ -114,35 +117,138 @@ impl Fixer {
         }
     }
 
-    fn fix_expression(&self, expression: &mut UntypedExpr) {
+    fn fix_expression(&mut self, expression: &mut UntypedExpr) {
         match expression {
+            UntypedExpr::Try {
+                value: try_value,
+                then: try_then,
+                pattern: try_pattern,
+                location,
+                ..
+            } => {
+                // We've seen a `try` expression, so the result module is needed.
+                self.module_used = true;
+
+                // Take ownership of the sub-expressions and patterns.
+                let mut then = placeholder_expression();
+                std::mem::swap(&mut then, &mut *try_then);
+                let mut value = placeholder_expression();
+                std::mem::swap(&mut value, &mut *try_value);
+                let mut pattern = placeholder_pattern();
+                std::mem::swap(&mut pattern, &mut *try_pattern);
+
+                // Fix the sub-expressions.
+                self.fix_expression(&mut value);
+                self.fix_expression(&mut then);
+
+                // Replace the `try` with `use`.
+                let result_then = UntypedExpr::FieldAccess {
+                    location: value.location(),
+                    label: "then".into(),
+                    container: Box::new(UntypedExpr::Var {
+                        location: *location,
+                        name: self.result_module.name().into(),
+                    }),
+                };
+                let call = UntypedExpr::Call {
+                    location: *location,
+                    fun: Box::new(result_then),
+                    arguments: vec![CallArg {
+                        label: None,
+                        location: value.location(),
+                        implicit: false,
+                        value,
+                    }],
+                };
+                let pattern_location = pattern.location();
+                let (assignment, include_pattern) = match &pattern {
+                    Pattern::Var { name, .. } => (AssignName::Variable(name.clone()), false),
+                    Pattern::Discard { name, .. } => (AssignName::Discard(name.clone()), false),
+                    _ => (AssignName::Variable("x".into()), true),
+                };
+                let use_ = UntypedExpr::Use(Use {
+                    location: *location,
+                    call: Box::new(call),
+                    assignments: vec![(assignment, pattern_location)],
+                });
+                let expressions = if include_pattern {
+                    let assign = UntypedExpr::Assignment {
+                        location: pattern_location,
+                        value: Box::new(UntypedExpr::Var {
+                            location: pattern_location,
+                            name: "x".into(),
+                        }),
+                        pattern,
+                        kind: AssignmentKind::Let,
+                        annotation: None,
+                    };
+                    vec![use_, assign, then]
+                } else {
+                    vec![use_, then]
+                };
+                let mut sequence = UntypedExpr::Sequence {
+                    location: *location,
+                    expressions,
+                };
+                std::mem::swap(expression, &mut sequence);
+            }
+
             UntypedExpr::Int { .. }
             | UntypedExpr::Float { .. }
             | UntypedExpr::String { .. }
             | UntypedExpr::Var { .. }
             | UntypedExpr::Todo { .. } => (),
 
-            UntypedExpr::Sequence { expressions, .. } => todo!(),
-            UntypedExpr::Fn { body, .. } => todo!(),
+            UntypedExpr::Negate { value: e, .. }
+            | UntypedExpr::FieldAccess { container: e, .. }
+            | UntypedExpr::TupleIndex { tuple: e, .. }
+            | UntypedExpr::Fn { body: e, .. }
+            | UntypedExpr::Assignment { value: e, .. }
+            | UntypedExpr::Use(Use { call: e, .. }) => self.fix_expression(e),
+
+            UntypedExpr::Tuple {
+                elems: expressions, ..
+            }
+            | UntypedExpr::Sequence { expressions, .. } => {
+                for expression in expressions.iter_mut() {
+                    self.fix_expression(expression);
+                }
+            }
+
             UntypedExpr::List { elements, tail, .. } => todo!(),
-            UntypedExpr::Call { fun, arguments, .. } => todo!(),
+
+            UntypedExpr::Call { fun, arguments, .. } => {
+                self.fix_expression(fun);
+                for argument in arguments.iter_mut() {
+                    self.fix_expression(&mut argument.value);
+                }
+            }
+
             UntypedExpr::BinOp { left, right, .. } => todo!(),
             UntypedExpr::PipeLine { expressions, .. } => todo!(),
-            UntypedExpr::Assignment { value, .. } => todo!(),
-            UntypedExpr::Try { value, then, .. } => unreachable!("try should have been removed"),
-            UntypedExpr::Use(_) => todo!(),
+
             UntypedExpr::Case {
                 subjects, clauses, ..
             } => todo!(),
-            UntypedExpr::FieldAccess { container, .. } => todo!(),
-            UntypedExpr::Tuple { elems, .. } => todo!(),
-            UntypedExpr::TupleIndex { tuple, .. } => todo!(),
             UntypedExpr::BitString { segments, .. } => todo!(),
             UntypedExpr::RecordUpdate {
                 spread, arguments, ..
             } => todo!(),
-            UntypedExpr::Negate { value, .. } => todo!(),
         }
+    }
+}
+
+fn placeholder_expression() -> UntypedExpr {
+    UntypedExpr::Int {
+        location: Default::default(),
+        value: "0".into(),
+    }
+}
+
+fn placeholder_pattern() -> UntypedPattern {
+    UntypedPattern::Int {
+        location: Default::default(),
+        value: "0".into(),
     }
 }
 
