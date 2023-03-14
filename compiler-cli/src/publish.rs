a8f8528a2b723eb0b9b67497cf775f4a5a7e1428
Lunarmagpie <bambolambo0@gmail.com>
2023-03-13 22:45:10 -0400

    refactoring

--- compiler-cli/src/publish.rs
+++ compiler-cli/src/publish.rs
@@ -161,7 +161,7 @@ fn do_build_hex_tarball(config: &PackageConfig) -> Result<Tarball> {
     }
     tracing::info!("Generated package Hex release tarball");
     Ok(Tarball {
-        compile_result: compile_result,
+        compile_result,
         data: tarball,
         src_files_added: src_files,
         generated_files_added: generated_files,
