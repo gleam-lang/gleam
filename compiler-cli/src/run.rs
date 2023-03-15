51b7d8e25d6d66d78f7a5b63ae7ab9b089af837c
Lunarmagpie <bambolambo0@gmail.com>
2023-03-15 00:10:45 -0400

    task: add unittests for checking if module names are valid

--- compiler-cli/src/run.rs
+++ compiler-cli/src/run.rs
@@ -273,3 +273,25 @@ fn add_deno_flag(args: &mut Vec<String>, flag: &str, flags: &DenoFlag) {
         }
     }
 }
+
+#[test]
+fn invalid_module_names() {
+    for mod_name in [
+        "",
+        "/mod/name",
+        "/mod/name/",
+        "mod/name/",
+        "/mod/",
+        "mod/",
+        "common-invalid-character",
+    ] {
+        assert!(!is_gleam_module(mod_name));
+    }
+}
+
+#[test]
+fn valid_module_names() {
+    for mod_name in ["valid", "valid/name", "valid/mod/name"] {
+        assert!(is_gleam_module(mod_name));
+    }
+}
