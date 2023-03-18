2f3deebecfdffae31e527cd902cb89bc3a22ad4a
Louis Pilfold <louis@lpil.uk>
2023-03-18 14:14:13 +0000

    Remove note

--- compiler-core/src/language_server.rs
+++ compiler-core/src/language_server.rs
@@ -15,11 +15,6 @@ use crate::{
 use lsp_types::{Position, Range};
 use std::any::Any;
 
-// TODO: Make a new router class which finds the root of the project a message
-// is for and dispatches to the correct language server, making one for that
-// root if it does not exist. This will require the compiler to be modified so
-// that it can run on projects where the root is not the cwd.
-
 #[derive(Debug)]
 pub struct LockGuard(pub Box<dyn Any>);
 
