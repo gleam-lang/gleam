61a679d78af512a2da0f00327b291be09e0dceb5
Louis Pilfold <louis@lpil.uk>
2023-06-15 22:57:33 +0100

    Report if incomplete

--- compiler-cli/src/fix.rs
+++ compiler-cli/src/fix.rs
@@ -6,6 +6,8 @@ use gleam_core::{
 use std::{path::PathBuf, str::FromStr};
 
 pub fn run(target: Option<Target>, files: Vec<String>) -> Result<()> {
+    let mut complete = true;
+
     for file_path in files {
         let path = PathBuf::from_str(&file_path).map_err(|e| Error::FileIo {
             action: FileIoAction::Open,
@@ -16,18 +18,39 @@ pub fn run(target: Option<Target>, files: Vec<String>) -> Result<()> {
 
         if path.is_dir() {
             for path in crate::fs::gleam_files_excluding_gitignore(&path) {
-                fix_file(target, path)?;
+                let file_complete = fix_file(target, path)?;
+                complete = complete && file_complete;
             }
         } else {
-            fix_file(target, path)?;
+            let file_complete = fix_file(target, path)?;
+            complete = complete && file_complete;
         }
     }
+
+    if !complete {
+        println!(
+            "I wasn't able to determine what target should be used for all
+external functions so they were not all converted to the new
+syntax.
+
+If you know what target should be used for all these ambiguous
+functions run this command again with the --target flag set:
+
+    gleam fix --target erlang
+    gleam fix --target javascript
+"
+        );
+        todo!("replace this with an actual error");
+        std::process::exit(1);
+    }
+
+    println!("All fixed!");
     Ok(())
 }
 
-fn fix_file(target: Option<Target>, path: PathBuf) -> Result<()> {
+fn fix_file(target: Option<Target>, path: PathBuf) -> Result<bool> {
     let src = crate::fs::read(&path)?;
-    let out = gleam_core::fix::parse_fix_and_format(target, &src.into(), &path)?;
+    let (out, complete) = gleam_core::fix::parse_fix_and_format(target, &src.into(), &path)?;
     crate::fs::write(&path, &out)?;
-    Ok(())
+    Ok(complete)
 }
