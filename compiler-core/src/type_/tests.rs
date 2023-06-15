4e43112530d5600ac538866cce8877635a333893
etopiei <me.etopiei@gmail.com>
2023-06-16 08:55:47 +1000

    Fix method after rebase.

--- compiler-core/src/type_/tests.rs
+++ compiler-core/src/type_/tests.rs
@@ -118,12 +118,10 @@ fn get_warnings(src: &str, deps: Vec<DependencyModule<'_>>) -> Vec<Warning> {
     warnings
         .take()
         .into_iter()
-        .map(|warning| match warning {
-            crate::Warning::Type { warning, .. } => warning,
-            crate::Warning::Parse { .. } => panic!("Unexpected parse warning"),
-            crate::Warning::InvalidSource { warning, .. } => warning,
         .filter_map(|warning| match warning {
-            _ => panic!("Unexpected warning."),
+            crate::Warning::Type { warning, .. } => Some(warning),
+            crate::Warning::Parse { .. } => panic!("Unexpected parse warning"),
+            crate::Warning::InvalidSource { .. } => None,
         })
         .collect_vec()
 }
