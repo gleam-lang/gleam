97c7d54c50bf2f111704d3c4cbabae3ec51b0ae6
shayan javani <massivefermion@protonmail.com>
2023-07-21 14:55:23 +0330

    some little changes

--- compiler-core/src/javascript/tests/case_clause_guards.rs
+++ compiler-core/src/javascript/tests/case_clause_guards.rs
@@ -200,19 +200,20 @@ fn alternative_patterns_guard() {
 #[test]
 fn field_access() {
     assert_js!(
-        "
-        type Person {
+        r#"
+        pub type Person {
           Person(username: String, name: String, age: Int)
         }
         
         pub fn main() {
-          let given_name = \"jill\"
-          let person = Person(\"credo\",\"jack\", 32)
-        
+          let given_name = "jack"
+          let raiden = Person("raiden", "jack", 31)
+          
           case given_name {
-            name if name == person.name -> \"it's jack\"
-            name -> \"it's \" <> name
+            name if name == raiden.name -> "It's jack"
+            _ -> "It's not jack"
           }
-        }"
+        }
+        "#
     )
 }
