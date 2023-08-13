7191ef0a957cb9d87d64107c8863ceb74c76ab2d
cijiugechu <cijiugechu@gmail.com>
2023-08-13 22:44:31 +0800

    bump x509-parser

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,7 @@ use regex::Regex;
 use ring::digest::{Context, SHA256};
 use serde::Deserialize;
 use serde_json::json;
+use x509_parser::prelude::FromDer;
 use std::{collections::HashMap, convert::TryFrom, convert::TryInto, io::BufReader};
 use thiserror::Error;
 use version::{Range, Version};
