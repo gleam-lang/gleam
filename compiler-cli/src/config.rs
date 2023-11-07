9bb721b03f42f9c2fa231eee4bb89cb4fe83679c
James Birtles <james@birtles.dev>
2023-11-07 20:45:26 +0000

    removed unused import as flagged by clippy

--- compiler-cli/src/config.rs
+++ compiler-cli/src/config.rs
@@ -3,7 +3,7 @@ use camino::Utf8PathBuf;
 use gleam_core::{
     config::PackageConfig,
     error::{Error, FileIoAction, FileKind},
-    manifest::{Base16Checksum, Manifest, ManifestPackage, ManifestPackageSource},
+    manifest::{Manifest, ManifestPackage, ManifestPackageSource},
     paths::ProjectPaths,
 };
 
@@ -28,7 +28,7 @@ pub fn find_package_config_for_module(
             continue;
         }
 
-        let root = package_root(&package, project_paths);
+        let root = package_root(package, project_paths);
         let mut module_path = root.join("src").join(mod_path);
         _ = module_path.set_extension("gleam");
 
@@ -51,64 +51,6 @@ fn package_root(package: &ManifestPackage, project_paths: &ProjectPaths) -> Utf8
             project_paths.build_packages_package(&package.name)
         }
     }
-}
-
-#[test]
-fn package_root_hex() {
-    let paths = ProjectPaths::new(Utf8PathBuf::from("/app"));
-    let package = ManifestPackage {
-        name: "the_package".into(),
-        version: hexpm::version::Version::new(1, 0, 0),
-        build_tools: vec!["gleam".into()],
-        otp_app: None,
-        requirements: vec![],
-        source: ManifestPackageSource::Hex {
-            outer_checksum: Base16Checksum(vec![]),
-        },
-    };
-    assert_eq!(
-        package_root(&package, &paths),
-        Utf8PathBuf::from("/app/build/packages/the_package")
-    );
-}
-
-#[test]
-fn package_root_git() {
-    let paths = ProjectPaths::new(Utf8PathBuf::from("/app"));
-    let package = ManifestPackage {
-        name: "the_package".into(),
-        version: hexpm::version::Version::new(1, 0, 0),
-        build_tools: vec!["gleam".into()],
-        otp_app: None,
-        requirements: vec![],
-        source: ManifestPackageSource::Git {
-            repo: "repo".into(),
-            commit: "commit".into(),
-        },
-    };
-    assert_eq!(
-        package_root(&package, &paths),
-        Utf8PathBuf::from("/app/build/packages/the_package")
-    );
-}
-
-#[test]
-fn package_root_local() {
-    let paths = ProjectPaths::new(Utf8PathBuf::from("/app"));
-    let package = ManifestPackage {
-        name: "the_package".into(),
-        version: hexpm::version::Version::new(1, 0, 0),
-        build_tools: vec!["gleam".into()],
-        otp_app: None,
-        requirements: vec![],
-        source: ManifestPackageSource::Local {
-            path: Utf8PathBuf::from("../wibble"),
-        },
-    };
-    assert_eq!(
-        package_root(&package, &paths),
-        Utf8PathBuf::from("/app/../wibble")
-    );
 }
 
 pub fn read(config_path: Utf8PathBuf) -> Result<PackageConfig, Error> {
@@ -134,4 +76,68 @@ pub fn ensure_config_exists(paths: &ProjectPaths) -> Result<(), Error> {
         });
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use gleam_core::manifest::Base16Checksum;
+
+    #[test]
+    fn package_root_hex() {
+        let paths = ProjectPaths::new(Utf8PathBuf::from("/app"));
+        let package = ManifestPackage {
+            name: "the_package".into(),
+            version: hexpm::version::Version::new(1, 0, 0),
+            build_tools: vec!["gleam".into()],
+            otp_app: None,
+            requirements: vec![],
+            source: ManifestPackageSource::Hex {
+                outer_checksum: Base16Checksum(vec![]),
+            },
+        };
+        assert_eq!(
+            package_root(&package, &paths),
+            Utf8PathBuf::from("/app/build/packages/the_package")
+        );
+    }
+
+    #[test]
+    fn package_root_git() {
+        let paths = ProjectPaths::new(Utf8PathBuf::from("/app"));
+        let package = ManifestPackage {
+            name: "the_package".into(),
+            version: hexpm::version::Version::new(1, 0, 0),
+            build_tools: vec!["gleam".into()],
+            otp_app: None,
+            requirements: vec![],
+            source: ManifestPackageSource::Git {
+                repo: "repo".into(),
+                commit: "commit".into(),
+            },
+        };
+        assert_eq!(
+            package_root(&package, &paths),
+            Utf8PathBuf::from("/app/build/packages/the_package")
+        );
+    }
+
+    #[test]
+    fn package_root_local() {
+        let paths = ProjectPaths::new(Utf8PathBuf::from("/app"));
+        let package = ManifestPackage {
+            name: "the_package".into(),
+            version: hexpm::version::Version::new(1, 0, 0),
+            build_tools: vec!["gleam".into()],
+            otp_app: None,
+            requirements: vec![],
+            source: ManifestPackageSource::Local {
+                path: Utf8PathBuf::from("../wibble"),
+            },
+        };
+        assert_eq!(
+            package_root(&package, &paths),
+            Utf8PathBuf::from("/app/../wibble")
+        );
+    }
 }
