750a257b590860f4bdb8c013d6bee306a6a93746
Alessandro Scandone <alescandone@gmail.com>
2023-12-04 17:16:31 +0100

    added test and fixed function

--- compiler-cli/src/fs/tests.rs
+++ compiler-cli/src/fs/tests.rs
@@ -45,6 +45,39 @@ fn git_init_already_in_git() {
 }
 
 #[test]
+fn exclude_build_dir() {
+    /*
+    a
+    |-- gleam.toml
+    |-- build
+    |  |-- f.gleam  # do not count as gleam file
+    b
+    |-- build
+    |  |-- f.gleam  # count as gleam file
+     */
+
+    let tmp_dir = tempfile::tempdir().unwrap();
+    let path = Utf8Path::from_path(tmp_dir.path()).expect("Non Utf-8 Path");
+
+    // excluded gleam file
+    {
+        let gleam_toml = path.join("a/gleam.toml").to_path_buf();
+        super::write(&gleam_toml, "").unwrap();
+
+        let gleam_file = path.join("a/build/f.gleam").to_path_buf();
+        super::write(&gleam_file, "").unwrap();
+    };
+
+    // included gleam file
+    let gleam_file = path.join("b/build/f.gleam").to_path_buf();
+    super::write(&gleam_file, "").unwrap();
+
+    let files = super::gleam_files_excluding_gitignore(path).collect::<Vec<_>>();
+
+    assert_eq!(files, vec![gleam_file]);
+}
+
+#[test]
 fn is_gleam_path_test() {
     assert!(super::is_gleam_path(
         Utf8Path::new("/some-prefix/a.gleam"),
