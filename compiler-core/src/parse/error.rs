918f686b719dd8ebd8ddbe8a9d316eec1588f129
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2024-01-01 11:07:43 +0100

    fix a missed error message

--- compiler-core/src/parse/error.rs
+++ compiler-core/src/parse/error.rs
@@ -179,7 +179,7 @@ utf16_codepoint, utf32_codepoint, signed, unsigned, big, little, native, size, u
                     _ => messages.collect(),
                 };
 
-                ("I was not expecting this.", messages)
+                ("I was not expecting this", messages)
             }
             ParseErrorType::ExpectedBoolean => ("Did you mean to negate a boolean?", vec![]),
             ParseErrorType::ConcatPatternVariableLeftHandSide => (
