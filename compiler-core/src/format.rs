6013814008a19f1f26d20c22ddff2d23166856e7
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2024-01-24 17:13:01 +0100

    address review comments

--- compiler-core/src/format.rs
+++ compiler-core/src/format.rs
@@ -1285,9 +1285,9 @@ impl<'comments> Formatter<'comments> {
             ArgNames::NamedLabelled { label, name } => docvec![label, " ", name],
             // We remove the underscore from discarded function arguments since we don't want to
             // expose this kind of detail: https://github.com/gleam-lang/gleam/issues/2561
-            ArgNames::Discard { name } => name.chars().dropping(1).as_str().to_doc(),
+            ArgNames::Discard { name } => name.strip_prefix('_').unwrap_or(name).to_doc(),
             ArgNames::LabelledDiscard { label, name } => {
-                docvec![label, " ", name.chars().dropping(1).as_str().to_doc()]
+                docvec![label, " ", name.strip_prefix('_').unwrap_or(name).to_doc()]
             }
         }
     }
