5277f303cba8b9bfe2372c0be71c634584edb76c
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2024-03-18 22:50:08 +0100

    Fixed a bug with string pattern matching codegen on the JS target

--- compiler-core/src/javascript/tests/case.rs
+++ compiler-core/src/javascript/tests/case.rs
@@ -190,3 +190,48 @@ fn anything() -> a {
 "#,
     )
 }
+
+// https://github.com/gleam-lang/gleam/issues/2759
+#[test]
+fn nested_string_prefix_match() {
+    assert_js!(
+        r#"
+fn main() {
+  case Ok(["a", "b c", "d"]) {
+    Ok(["a", "b " <> _, "d"]) -> 1
+    _ -> 1
+  }
+}
+"#
+    );
+}
+
+// https://github.com/gleam-lang/gleam/issues/2759
+#[test]
+fn nested_string_prefix_match_that_would_crash_on_js() {
+    assert_js!(
+        r#"
+fn main() {
+  case Ok(["b c", "d"]) {
+    Ok(["b " <> _, "d"]) -> 1
+    _ -> 1
+  }
+}
+"#
+    );
+}
+
+#[test]
+fn slicing_is_handled_properly_with_multiple_branches() {
+    assert_js!(
+        r#"
+pub fn main() {
+  case "12345" {
+    "0" <> rest -> rest
+    "123" <> rest -> rest
+    _ -> ""
+  }
+}
+"#
+    )
+}
