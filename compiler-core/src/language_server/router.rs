17969011579c59174a98764af78f2c7ee84eeca3
Louis Pilfold <louis@lpil.uk>
2024-03-19 20:18:21 +0000

    Correct compilation

--- compiler-core/src/language_server/router.rs
+++ compiler-core/src/language_server/router.rs
@@ -54,11 +54,15 @@ where
 
     pub fn project_for_path(
         &mut self,
-        path: &Utf8Path,
+        path: Utf8PathBuf,
     ) -> Result<Option<&mut Project<IO, Reporter>>> {
-        let path = match find_gleam_project_parent(&self.io, path) {
-            Some(x) => x,
-            None => return Ok(None),
+        let path = if self.engines.contains_key(&path) {
+            path
+        } else {
+            let Some(path) = find_gleam_project_parent(&self.io, &path) else {
+                return Ok(None);
+            };
+            path
         };
 
         let entry = match self.engines.entry(path.clone()) {
