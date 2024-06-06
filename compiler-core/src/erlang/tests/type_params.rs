7500abfc8f3db492de3d673b748cbd32dfbf078e
dvic <info@dvic.io>
2024-06-07 00:13:51 +0200

    Simplify test

--- compiler-core/src/erlang/tests/type_params.rs
+++ compiler-core/src/erlang/tests/type_params.rs
@@ -1,36 +1,10 @@
 use crate::assert_erl;
 
 #[test]
-fn unused_params() {
+fn named_args_count_once() {
     assert_erl!(
         "
-        fn foo() {
-            let parent_subject = new_subject()
-            let assert Ok(call_subject) = receive(parent_subject)
-            let assert Ok(_) = try_call(call_subject, fn(subject) { #(1, subject) })
-        }
-
-        type Subject(message) {
-            Subject(message)
-        }
-
-        fn new_subject() -> Subject(message) {
-            todo
-        }
-
-        fn receive(subject: Subject(message)) -> Result(message, Nil) {
-            todo
-        }
-
-        type CallError(msg) {
-            CalleeDown(reason: String)
-            CallTimeout
-        }
-
-        fn try_call(
-            subject: Subject(request),
-            make_request: fn(Subject(response)) -> request
-        ) -> Result(response, CallError(response)) {
+        pub fn foo() -> Result(a, a) {
             todo
         }
         "
