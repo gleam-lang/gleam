c9eb6db1b83a4dadc7e61973f118c9305e9b4658
dvic <info@dvic.io>
2024-06-12 21:10:39 +0200

    Add additional test for nested types

--- compiler-core/src/erlang/tests/type_params.rs
+++ compiler-core/src/erlang/tests/type_params.rs
@@ -10,3 +10,36 @@ fn named_args_count_once() {
         "
     );
 }
+
+#[test]
+fn nested_named_args_count_once() {
+    assert_erl!(
+        "
+        pub type Nested(a, b) {
+            VariantX(field_a: a, field_b: b)
+            VariantY(field_a: a, field_b: b)
+        }
+
+        pub type Parent(a, b) {
+            ParentX(Nested(a,b))
+            ParentY(Nested(a,b))
+        }
+
+        pub fn foo1() -> Parent(a, a) {
+            todo
+        }
+
+        pub fn foo2() -> Parent(a, b) {
+            todo
+        }
+
+        pub fn foo3(val_a: a) -> Parent(a, a) {
+            todo
+        }
+
+        pub fn foo4(val_a: a, val_b: b) -> Parent(a, b) {
+            todo
+        }
+        "
+    );
+}
