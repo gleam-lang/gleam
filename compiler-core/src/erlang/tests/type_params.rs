432b1a3a572973ac0fa2681781f9fbdedcc71a25
dvic <info@dvic.io>
2024-06-16 21:51:51 +0200

    Update tests

--- compiler-core/src/erlang/tests/type_params.rs
+++ compiler-core/src/erlang/tests/type_params.rs
@@ -15,29 +15,18 @@ fn named_args_count_once() {
 fn nested_named_args_count_once() {
     assert_erl!(
         "
-        pub type Nested(a, b) {
-            VariantX(field_a: a, field_b: b)
-            VariantY(field_a: a, field_b: b)
-        }
-
-        pub type Parent(a, b) {
-            ParentX(Nested(a,b))
-            ParentY(Nested(a,b))
-        }
-
-        pub fn foo1() -> Parent(a, a) {
+        pub fn foo() -> Result(a, Result(a, b)) {
             todo
         }
+        "
+    );
+}
 
-        pub fn foo2() -> Parent(a, b) {
-            todo
-        }
-
-        pub fn foo3(val_a: a) -> Parent(a, a) {
-            todo
-        }
-
-        pub fn foo4(val_a: a, val_b: b) -> Parent(a, b) {
+#[test]
+fn tuple_type_params_count_twice() {
+    assert_erl!(
+        "
+        pub fn foo() -> #(a, Result(a, b)) {
             todo
         }
         "
