60472a61ee54b10d178f9e5663881fc59660449a
dvic <info@dvic.io>
2024-06-16 22:16:22 +0200

    Add tests for custom types
    The snapshots are unconfirmed

--- compiler-core/src/erlang/tests/type_params.rs
+++ compiler-core/src/erlang/tests/type_params.rs
@@ -32,3 +32,48 @@ fn tuple_type_params_count_twice() {
         "
     );
 }
+
+#[test]
+fn custom_type_named_args_count_once() {
+    assert_erl!(
+        "
+        pub type Wibble(a, b) {
+            Wibble(a, b)
+        }
+
+        pub fn foo() -> Wibble(a, a) {
+            todo
+        }
+        "
+    );
+}
+
+#[test]
+fn custom_type_nested_named_args_count_once() {
+    assert_erl!(
+        "
+        pub type Wibble(a, b) {
+            Wibble(a, b)
+        }
+
+        pub fn foo() -> Wibble(a, Wibble(a, b)) {
+            todo
+        }
+        "
+    );
+}
+
+#[test]
+fn custom_type_tuple_type_params_count_twice() {
+    assert_erl!(
+        "
+        pub type Wibble(a, b) {
+            Wibble(a, b)
+        }
+
+        pub fn foo() -> #(a, Wibble(a, b)) {
+            todo
+        }
+        "
+    );
+}
