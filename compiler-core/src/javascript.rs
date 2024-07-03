dc618f254a866fbb2d03e234d8752731865e9735
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2024-07-03 20:21:03 +0200

    Make sure all constants inside a module constant definition are marked as pure

--- compiler-core/src/javascript.rs
+++ compiler-core/src/javascript.rs
@@ -17,7 +17,7 @@ use crate::{
 };
 use camino::Utf8Path;
 use ecow::EcoString;
-use expression::NeedsPureAnnotation;
+use expression::IsModuleConstantDefinition;
 use itertools::Itertools;
 
 use self::import::{Imports, Member};
@@ -466,19 +466,16 @@ impl<'a> Generator<'a> {
             "export const "
         };
 
-        let mut needs_pure_annotation = NeedsPureAnnotation::No;
-        let document =
-            expression::constant_expression(&mut needs_pure_annotation, &mut self.tracker, value)?;
-
-        let equals = match needs_pure_annotation {
-            NeedsPureAnnotation::Yes => " = /* @__PURE__ */ ",
-            NeedsPureAnnotation::No => " = ",
-        };
+        let document = expression::constant_expression(
+            IsModuleConstantDefinition::Yes,
+            &mut self.tracker,
+            value,
+        )?;
 
         Ok(docvec![
             head,
             maybe_escape_identifier_doc(name),
-            equals,
+            " = ",
             document,
             ";",
         ])
