8a56625ff9fe7c41a0c9aa3d38c7db91011ce8a8
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2024-07-18 17:57:26 +0200

    review comment!

--- compiler-core/src/type_/tests/pipes.rs
+++ compiler-core/src/type_/tests/pipes.rs
@@ -27,3 +27,17 @@ pub fn c() {
         ]
     );
 }
+
+// https://github.com/gleam-lang/gleam/pull/3406#discussion_r1683068647
+#[test]
+fn pipe_rewrite_with_missing_argument() {
+    assert_module_infer!(
+        r#"
+pub fn main() {
+  let f = fn(a, b) { fn(c) { a + b + c } }
+  1 |> f(2)
+}
+"#,
+        vec![("main", "fn() -> fn(Int) -> Int")]
+    );
+}
