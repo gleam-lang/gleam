ab2bb06f022690879f7f2c7d0631c022d0a50bdb
Dylan Johnson <mjohnson10@paypal.com>
2024-07-31 11:56:57 -0400

    add `GLEAM_CACERTS_PATH` env variable

--- compiler-cli/src/http.rs
+++ compiler-cli/src/http.rs
@@ -6,6 +6,7 @@ use gleam_core::{Error, Result};
 use http::{Request, Response};
 
 static REQWEST_CLIENT: OnceLock<reqwest::Client> = OnceLock::new();
+static CERTS_ENV_VAR: &str = "GLEAM_CACERTS_PATH";
 
 #[derive(Debug)]
 pub struct HttpClient;
@@ -27,7 +28,7 @@ impl gleam_core::io::HttpClient for HttpClient {
             .try_into()
             .expect("Unable to convert HTTP request for use by reqwest library");
         let mut response = REQWEST_CLIENT
-            .get_or_init(reqwest::Client::new)
+            .get_or_init(init_client)
             .execute(request)
             .await
             .map_err(Error::http)?;
@@ -40,5 +41,34 @@ impl gleam_core::io::HttpClient for HttpClient {
         builder
             .body(response.bytes().await.map_err(Error::http)?.to_vec())
             .map_err(Error::http)
+    }
+}
+
+fn init_client() -> reqwest::Client {
+    if let Some(cert) = get_certificate() {
+        return reqwest::Client::builder()
+            .add_root_certificate(cert)
+            .build()
+            .expect("Unable to initialize a reqwest HTTP client");
+    } else {
+        return reqwest::Client::new();
+    }
+}
+
+fn get_certificate() -> Option<reqwest::Certificate> {
+    match std::env::var(CERTS_ENV_VAR) {
+        Ok(certs_path) => {
+            let data = std::fs::read(certs_path).expect(&format!(
+                "Unable to read certs file set as `{}`",
+                CERTS_ENV_VAR
+            ));
+            let cert = reqwest::Certificate::from_pem(&data).expect(&format!(
+                "Unable to construct a certificate from certs file set as `{}`",
+                CERTS_ENV_VAR
+            ));
+
+            Some(cert)
+        }
+        _ => None,
     }
 }
