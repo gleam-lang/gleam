436b21a7da88342c3be7be394dcab3b8bdb41ade
Ameen Radwan <ameen_radwan@intuit.com>
2024-08-05 16:32:02 -0700

    more tests

--- compiler-core/src/type_/tests/external_usages.rs
+++ compiler-core/src/type_/tests/external_usages.rs
@@ -181,3 +181,96 @@ pub fn main() -> test_module_inner.Wibble {
         .into()
     );
 }
+
+#[test]
+fn external_qualified_type_in_custom_usages() {
+    let module = compile_module(
+        "test_module",
+        "
+import test_module/test_module_inner
+
+pub type Wobble {
+  Wobble(test_module_inner.Wibble)
+}
+
+pub fn main() {
+    Wobble(test_module_inner.Wibble(1))
+}
+",
+        None,
+        vec![(
+            "thepackage",
+            "test_module/test_module_inner",
+            "pub type Wibble { Wibble(Int) Wobble(String) }",
+        )],
+    )
+    .unwrap();
+    assert_eq!(
+        module.type_info.external_type_usages,
+        [(
+            "test_module/test_module_inner".into(),
+            [("Wibble".into(), [SrcSpan { start: 66, end: 90 }].into()),].into()
+        )]
+        .into()
+    );
+}
+
+#[test]
+fn external_qualified_type_in_alias_usages() {
+    let module = compile_module(
+        "test_module",
+        "
+import test_module/test_module_inner
+
+pub type Wobble = test_module_inner.Wibble
+
+pub fn main() -> Wobble {
+    test_module_inner.Wibble(1)
+}
+",
+        None,
+        vec![(
+            "thepackage",
+            "test_module/test_module_inner",
+            "pub type Wibble { Wibble(Int) Wobble(String) }",
+        )],
+    )
+    .unwrap();
+    assert_eq!(
+        module.type_info.external_type_usages,
+        [(
+            "test_module/test_module_inner".into(),
+            [("Wibble".into(), [SrcSpan { start: 57, end: 81 }].into()),].into()
+        )]
+        .into()
+    );
+}
+
+#[test]
+fn external_qualified_type_annotation_usages() {
+    let module = compile_module(
+        "test_module",
+        "
+import test_module/test_module_inner
+
+pub fn main() {
+  let w: test_module_inner.Wibble = test_module_inner.Wibble(1)
+}
+",
+        None,
+        vec![(
+            "thepackage",
+            "test_module/test_module_inner",
+            "pub type Wibble { Wibble(Int) Wobble(String) }",
+        )],
+    )
+    .unwrap();
+    assert_eq!(
+        module.type_info.external_type_usages,
+        [(
+            "test_module/test_module_inner".into(),
+            [("Wibble".into(), [SrcSpan { start: 64, end: 88 }].into()),].into()
+        )]
+        .into()
+    );
+}
