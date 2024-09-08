f6fc09786231f1225a34bfb47e5d4c5b24a1a61c
PgBiel <9021226+PgBiel@users.noreply.github.com>
2024-09-08 11:59:31 -0300

    add directories to in-memory FS

--- compiler-core/src/io/memory.rs
+++ compiler-core/src/io/memory.rs
@@ -35,6 +35,8 @@ impl InMemoryFileSystem {
         self.files.deref().borrow_mut().clear();
     }
 
+    /// Returns the contents of each file, excluding directories.
+    ///
     /// # Panics
     ///
     /// Panics if this is not the only reference to the underlying files.
@@ -44,7 +46,7 @@ impl InMemoryFileSystem {
             .expect("InMemoryFileSystem::into_files called on a clone")
             .into_inner()
             .into_iter()
-            .map(|(path, file)| (path, file.into_content()))
+            .filter_map(|(path, file)| file.into_content().map(|content| (path, content)))
             .collect()
     }
 
@@ -89,7 +91,24 @@ impl InMemoryFileSystem {
 impl FileSystemWriter for InMemoryFileSystem {
     fn delete_directory(&self, path: &Utf8Path) -> Result<(), Error> {
         let mut files = self.files.deref().borrow_mut();
+
+        if files.get(path).is_some_and(|f| !f.is_directory()) {
+            return Err(Error::FileIo {
+                kind: FileKind::Directory,
+                action: FileIoAction::Delete,
+                path: path.to_path_buf(),
+                err: None,
+            });
+        }
+
         let _ = files.remove(path);
+
+        // Remove any files in the directory
+        while let Some(file) = files.keys().find(|file| file.starts_with(path)) {
+            let file = file.clone();
+            let _ = files.remove(&file);
+        }
+
         Ok(())
     }
 
@@ -101,7 +120,31 @@ impl FileSystemWriter for InMemoryFileSystem {
         panic!("unimplemented") // TODO
     }
 
-    fn mkdir(&self, _: &Utf8Path) -> Result<(), Error> {
+    fn mkdir(&self, path: &Utf8Path) -> Result<(), Error> {
+        // Basic check to ensure we don't leave the in-memory directory root
+        if path.components().next() == Some(camino::Utf8Component::ParentDir) {
+            return Err(Error::FileIo {
+                kind: FileKind::Directory,
+                action: FileIoAction::Create,
+                path: path.to_path_buf(),
+                err: None,
+            });
+        }
+
+        for path in path.ancestors() {
+            if matches!(path.as_str(), "" | "/" | ".." | ".")
+                || path.ends_with("/..")
+                || path.ends_with("/.")
+            {
+                continue;
+            }
+            let dir = InMemoryFile::directory();
+            _ = self
+                .files
+                .deref()
+                .borrow_mut()
+                .insert(path.to_path_buf(), dir);
+        }
         Ok(())
     }
 
@@ -114,7 +157,16 @@ impl FileSystemWriter for InMemoryFileSystem {
     }
 
     fn delete_file(&self, path: &Utf8Path) -> Result<(), Error> {
-        let _ = self.files.deref().borrow_mut().remove(path);
+        let mut files = self.files.deref().borrow_mut();
+        if files.get(path).is_some_and(|f| f.is_directory()) {
+            return Err(Error::FileIo {
+                kind: FileKind::File,
+                action: FileIoAction::Delete,
+                path: path.to_path_buf(),
+                err: None,
+            });
+        }
+        let _ = files.remove(path);
         Ok(())
     }
 
@@ -123,6 +175,11 @@ impl FileSystemWriter for InMemoryFileSystem {
     }
 
     fn write_bytes(&self, path: &Utf8Path, content: &[u8]) -> Result<(), Error> {
+        // Ensure directories exist
+        if let Some(parent) = path.parent() {
+            self.mkdir(parent)?;
+        }
+
         let mut file = InMemoryFile::default();
         _ = io::Write::write(&mut file, content).expect("channel buffer write");
         _ = self
@@ -148,6 +205,7 @@ impl FileSystemReader for InMemoryFileSystem {
             .deref()
             .borrow()
             .iter()
+            .filter(|(_, file)| !file.is_directory())
             .map(|(file_path, _)| file_path.to_path_buf())
             .filter(|file_path| file_path.starts_with(dir))
             .filter(|file_path| file_path.extension() == Some("gleam"))
@@ -159,6 +217,7 @@ impl FileSystemReader for InMemoryFileSystem {
             .deref()
             .borrow()
             .iter()
+            .filter(|(_, file)| !file.is_directory())
             .map(|(file_path, _)| file_path.to_path_buf())
             .filter(|file_path| file_path.starts_with(dir))
             .filter(|file_path| file_path.extension() == Some("cache"))
@@ -168,13 +227,16 @@ impl FileSystemReader for InMemoryFileSystem {
     fn read(&self, path: &Utf8Path) -> Result<String, Error> {
         let path = path.to_path_buf();
         let files = self.files.deref().borrow();
-        let file = files.get(&path).ok_or_else(|| Error::FileIo {
-            kind: FileKind::File,
-            action: FileIoAction::Open,
-            path: path.clone(),
-            err: None,
-        })?;
-        let bytes = file.buffer.borrow();
+        let buffer = files
+            .get(&path)
+            .and_then(|file| file.repr.as_file_buffer())
+            .ok_or_else(|| Error::FileIo {
+                kind: FileKind::File,
+                action: FileIoAction::Open,
+                path: path.clone(),
+                err: None,
+            })?;
+        let bytes = buffer.borrow();
         let unicode = String::from_utf8(bytes.clone()).map_err(|err| Error::FileIo {
             kind: FileKind::File,
             action: FileIoAction::Read,
@@ -187,26 +249,33 @@ impl FileSystemReader for InMemoryFileSystem {
     fn read_bytes(&self, path: &Utf8Path) -> Result<Vec<u8>, Error> {
         let path = path.to_path_buf();
         let files = self.files.deref().borrow();
-        let file = files.get(&path).ok_or_else(|| Error::FileIo {
-            kind: FileKind::File,
-            action: FileIoAction::Open,
-            path: path.clone(),
-            err: None,
-        })?;
-        let bytes = file.buffer.borrow().clone();
+        let buffer = files
+            .get(&path)
+            .and_then(|file| file.repr.as_file_buffer())
+            .ok_or_else(|| Error::FileIo {
+                kind: FileKind::File,
+                action: FileIoAction::Open,
+                path: path.clone(),
+                err: None,
+            })?;
+        let bytes = buffer.borrow().clone();
         Ok(bytes)
     }
 
     fn is_file(&self, path: &Utf8Path) -> bool {
-        self.files.deref().borrow().contains_key(path)
+        self.files
+            .deref()
+            .borrow()
+            .iter()
+            .any(|(filepath, file)| !file.is_directory() && filepath == path)
     }
 
     fn is_directory(&self, path: &Utf8Path) -> bool {
         self.files
             .deref()
             .borrow()
-            .keys()
-            .any(|file_path| file_path.starts_with(path))
+            .iter()
+            .any(|(filepath, file)| file.is_directory() && filepath == path)
     }
 
     fn reader(&self, _path: &Utf8Path) -> Result<WrappedReader, Error> {
@@ -221,7 +290,9 @@ impl FileSystemReader for InMemoryFileSystem {
                 .borrow()
                 .iter()
                 .map(|(file_path, _)| file_path.to_path_buf())
-                .filter(|file_path| file_path.starts_with(path))
+                .filter(|file_path| {
+                    file_path.parent().is_some_and(|parent| parent == path) || path == "/"
+                })
                 .map(DirEntry::from_pathbuf)
                 .map(Ok),
         );
@@ -241,6 +312,30 @@ impl FileSystemReader for InMemoryFileSystem {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum InMemoryFileRepr {
+    File(Rc<RefCell<Vec<u8>>>),
+    Directory,
+}
+
+impl InMemoryFileRepr {
+    /// Returns this file's file buffer if this isn't a directory.
+    fn as_file_buffer(&self) -> Option<&Rc<RefCell<Vec<u8>>>> {
+        match self {
+            Self::File(buffer) => Some(buffer),
+            Self::Directory => None,
+        }
+    }
+
+    /// Returns this file's file buffer if this isn't a directory.
+    fn into_file_buffer(self) -> Option<Rc<RefCell<Vec<u8>>>> {
+        match self {
+            Self::File(buffer) => Some(buffer),
+            Self::Directory => None,
+        }
+    }
+}
+
 // An in memory sharable that can be used in place of a real file. It is a
 // shared reference to a buffer than can be cheaply cloned, all resulting copies
 // pointing to the same internal buffer.
@@ -252,22 +347,37 @@ impl FileSystemReader for InMemoryFileSystem {
 //
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct InMemoryFile {
-    buffer: Rc<RefCell<Vec<u8>>>,
+    repr: InMemoryFileRepr,
     modification_time: SystemTime,
 }
 
 impl InMemoryFile {
+    /// Creates a directory.
+    pub fn directory() -> Self {
+        Self {
+            repr: InMemoryFileRepr::Directory,
+            ..Default::default()
+        }
+    }
+
+    pub fn is_directory(&self) -> bool {
+        matches!(self.repr, InMemoryFileRepr::Directory)
+    }
+
+    /// Returns this file's contents if this is not a directory.
+    ///
     /// # Panics
     ///
     /// Panics if this is not the only reference to the underlying files.
     ///
-    pub fn into_content(self) -> Content {
-        let contents = Rc::try_unwrap(self.buffer)
+    pub fn into_content(self) -> Option<Content> {
+        let buffer = self.repr.into_file_buffer()?;
+        let contents = Rc::try_unwrap(buffer)
             .expect("InMemoryFile::into_content called with multiple references")
             .into_inner();
         match String::from_utf8(contents) {
-            Ok(s) => Content::Text(s),
-            Err(e) => Content::Binary(e.into_bytes()),
+            Ok(s) => Some(Content::Text(s)),
+            Err(e) => Some(Content::Binary(e.into_bytes())),
         }
     }
 }
@@ -275,7 +385,7 @@ impl InMemoryFile {
 impl Default for InMemoryFile {
     fn default() -> Self {
         Self {
-            buffer: Default::default(),
+            repr: InMemoryFileRepr::File(Default::default()),
             // We use a fixed time here so that the tests are deterministic. In
             // future we may want to inject this in some fashion.
             modification_time: SystemTime::UNIX_EPOCH + Duration::from_secs(663112800),
@@ -285,12 +395,20 @@ impl Default for InMemoryFile {
 
 impl io::Write for InMemoryFile {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        let mut reference = (*self.buffer).borrow_mut();
+        let Some(buffer) = self.repr.as_file_buffer() else {
+            // Not a file
+            return Err(io::Error::from(io::ErrorKind::NotFound));
+        };
+        let mut reference = (*buffer).borrow_mut();
         reference.write(buf)
     }
 
     fn flush(&mut self) -> io::Result<()> {
-        let mut reference = (*self.buffer).borrow_mut();
+        let Some(buffer) = self.repr.as_file_buffer() else {
+            // Not a file
+            return Err(io::Error::from(io::ErrorKind::NotFound));
+        };
+        let mut reference = (*buffer).borrow_mut();
         reference.flush()
     }
 }
