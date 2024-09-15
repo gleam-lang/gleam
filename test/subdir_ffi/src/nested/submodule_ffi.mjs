37494449e55483a2b0dae12e683353c3b534c64f
PgBiel <9021226+PgBiel@users.noreply.github.com>
2024-09-15 13:36:14 -0300

    add subdir_ffi integration tests

---
+++ test/subdir_ffi/src/nested/submodule_ffi.mjs
@@ -0,0 +1,7 @@
+export function main(x) {
+  return "Hello from the nested JavaScript native module!";
+}
+
+export function main2(x) {
+  return "Hello again from the nested JavaScript native module!";
+}
