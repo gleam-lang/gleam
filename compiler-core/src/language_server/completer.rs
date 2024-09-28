085e8675f8dfd02b6cf5ede017cab1a549100823
GearsDatapacks <surya@seriousinfinitude.com>
2024-09-28 22:37:01 +0100

    Add known record fields to completions

--- compiler-core/src/language_server/completer.rs
+++ compiler-core/src/language_server/completer.rs
@@ -18,8 +18,8 @@ use crate::{
     io::{BeamCompiler, CommandExecutor, FileSystemReader, FileSystemWriter},
     line_numbers::LineNumbers,
     type_::{
-        self, collapse_links, pretty::Printer, AccessorsMap, FieldMap, ModuleInterface,
-        PreludeType, Type, TypeConstructor, ValueConstructorVariant, PRELUDE_MODULE_NAME,
+        self, collapse_links, pretty::Printer, FieldMap, ModuleInterface, PreludeType,
+        RecordAccessor, Type, TypeConstructor, ValueConstructorVariant, PRELUDE_MODULE_NAME,
     },
     Result,
 };
@@ -723,13 +723,19 @@ where
         &'a self,
         importable_modules: &'a im::HashMap<EcoString, ModuleInterface>,
         type_: Arc<Type>,
-    ) -> Option<&AccessorsMap> {
+    ) -> Option<&HashMap<EcoString, RecordAccessor>> {
         let type_ = collapse_links(type_);
         match type_.as_ref() {
-            Type::Named { name, module, .. } => importable_modules
+            Type::Named {
+                name,
+                module,
+                constructor_index,
+                ..
+            } => importable_modules
                 .get(module)
                 .and_then(|i| i.accessors.get(name))
-                .filter(|a| a.publicity.is_importable() || module == &self.module.name),
+                .filter(|a| a.publicity.is_importable() || module == &self.module.name)
+                .map(|a| a.accessors_for_constructor(*constructor_index)),
             _ => None,
         }
     }
@@ -741,9 +747,8 @@ where
             self.compiler.project_compiler.get_importable_modules(),
             type_,
         )
-        .map(|accessors_map| {
-            accessors_map
-                .accessors
+        .map(|accessors| {
+            accessors
                 .values()
                 .map(|accessor| field_completion(&accessor.label, accessor.type_.clone()))
                 .collect_vec()
