ec557c6f1ce89f5e66e7012e6793f6716d0b28f9
Olivier Pinon <oliv.pinon@gmail.com>
2024-09-29 22:18:17 +0200

    Basic support for generating SourceMaps on Typescript target

--- compiler-core/src/language_server/server.rs
+++ compiler-core/src/language_server/server.rs
@@ -13,6 +13,7 @@ use crate::{
         src_span_to_lsp_range, DownloadDependencies, MakeLocker,
     },
     line_numbers::LineNumbers,
+    sourcemap::SourceMapEmitter,
     Result,
 };
 use camino::{Utf8Path, Utf8PathBuf};
@@ -280,7 +281,9 @@ where
             Err(error) => return self.path_error_response(path, error),
         };
 
-        if let Err(error) = crate::format::pretty(&mut new_text, &src, &path) {
+        if let Err(error) =
+            crate::format::pretty(&mut new_text, &src, &path, &mut SourceMapEmitter::null())
+        {
             return self.path_error_response(path, error);
         }
 
