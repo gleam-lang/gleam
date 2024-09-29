93d6e3f5f759360756b3de5c04ee2b154db29374
Olivier Pinon <oliv.pinon@gmail.com>
2024-09-30 00:14:01 +0200

    Support sourcemap for assert

--- compiler-core/src/javascript/expression.rs
+++ compiler-core/src/javascript/expression.rs
@@ -1123,6 +1123,9 @@ impl<'module> Generator<'module> {
         location: SrcSpan,
     ) -> Output<'a> {
         let checks = self.pattern_checks_doc(checks, false);
+        let (start, end) = self
+            .line_numbers
+            .line_and_column_number_of_src_span(location);
         Ok(docvec![
             "if (",
             docvec![break_("", ""), checks].nest(INDENT),
@@ -1132,7 +1135,8 @@ impl<'module> Generator<'module> {
             line(),
             "}",
         ]
-        .group())
+        .group()
+        .attach_sourcemap_location(start, end))
     }
 
     fn pattern_assignments_doc(assignments: Vec<Assignment<'_>>) -> Document<'_> {
