d247dc6ec41506a40cacbb0598edf9522816c6fb
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2024-10-07 16:59:05 +0200

    echo parsing

--- compiler-core/src/parse.rs
+++ compiler-core/src/parse.rs
@@ -465,15 +465,6 @@ where
         Ok(())
     }
 
-    fn parse_expression_unit_collapsing_single_value_blocks(
-        &mut self,
-    ) -> Result<Option<UntypedExpr>, ParseError> {
-        match self.parse_expression_unit()? {
-            Some(expression) => Ok(Some(expression)),
-            None => Ok(None),
-        }
-    }
-
     // examples:
     //   1
     //   "one"
@@ -542,6 +533,16 @@ where
                 UntypedExpr::Panic {
                     location: SrcSpan { start, end },
                     message: label,
+                }
+            }
+
+            Some((start, Token::Echo, end)) => {
+                self.advance();
+                let expression = self.parse_expression_unit()?;
+                let end = expression.as_ref().map_or(end, |e| e.location().end);
+                UntypedExpr::Echo {
+                    location: SrcSpan { start, end },
+                    expression: expression.map(Box::new),
                 }
             }
 
@@ -646,7 +647,7 @@ where
                     &|s| {
                         Parser::parse_bit_array_segment(
                             s,
-                            &Parser::parse_expression_unit_collapsing_single_value_blocks,
+                            &Parser::parse_expression_unit,
                             &Parser::expect_expression,
                             &bit_array_expr_int,
                         )
