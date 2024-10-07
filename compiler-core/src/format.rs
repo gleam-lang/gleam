e0dd6746b1553ae3cf4efc469a92ac1c3d04a8a0
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2024-10-07 15:19:03 +0200

    Define untyped echo

--- compiler-core/src/format.rs
+++ compiler-core/src/format.rs
@@ -972,6 +972,16 @@ impl<'comments> Formatter<'comments> {
                 message: Some(l), ..
             } => docvec!["todo as ", self.expr(l)],
 
+            UntypedExpr::Echo {
+                expression: None,
+                location: _,
+            } => "echo".to_doc(),
+
+            UntypedExpr::Echo {
+                expression: Some(e),
+                location: _,
+            } => docvec!["echo ", self.expr(e)],
+
             UntypedExpr::PipeLine { expressions, .. } => self.pipeline(expressions, false),
 
             UntypedExpr::Int { value, .. } => self.int(value),
@@ -1237,6 +1247,7 @@ impl<'comments> Formatter<'comments> {
             | UntypedExpr::Tuple { .. }
             | UntypedExpr::TupleIndex { .. }
             | UntypedExpr::Todo { .. }
+            | UntypedExpr::Echo { .. }
             | UntypedExpr::Panic { .. }
             | UntypedExpr::BitArray { .. }
             | UntypedExpr::RecordUpdate { .. }
@@ -2510,6 +2521,7 @@ impl<'comments> Formatter<'comments> {
             | UntypedExpr::TupleIndex { .. }
             | UntypedExpr::Todo { .. }
             | UntypedExpr::Panic { .. }
+            | UntypedExpr::Echo { .. }
             | UntypedExpr::BitArray { .. }
             | UntypedExpr::RecordUpdate { .. }
             | UntypedExpr::NegateBool { .. }
