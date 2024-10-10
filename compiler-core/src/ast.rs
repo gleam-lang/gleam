da1405c1a01062539d13ce10fd805ce0605954a5
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2024-10-10 14:44:56 +0200

    refactor pipe assignments to simplify echo code generation

--- compiler-core/src/ast.rs
+++ compiler-core/src/ast.rs
@@ -2589,6 +2589,13 @@ impl TypedAssignment {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct UseAssignment {
+    pub location: SrcSpan,
+    pub pattern: UntypedPattern,
+    pub annotation: Option<TypeAst>,
+}
+
 /// A pipeline is desugared to a series of assignments:
 ///
 /// ```gleam
