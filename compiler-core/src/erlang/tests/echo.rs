5b5b168b7c7a0904633e67f46ad01c07fc6d8eb4
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2024-10-10 15:40:32 +0200

    first attempt at erlang code generation

---
+++ compiler-core/src/erlang/tests/echo.rs
@@ -0,0 +1,96 @@
+use crate::assert_erl;
+
+#[test]
+pub fn echo_with_a_simple_expression() {
+    assert_erl!(
+        r#"
+pub fn main() {
+  echo 1
+}
+"#
+    );
+}
+
+#[test]
+pub fn echo_with_a_case_expression() {
+    assert_erl!(
+        r#"
+pub fn main() {
+  echo case 1 {
+    _ -> 2
+  }
+}
+"#
+    );
+}
+
+#[test]
+pub fn echo_with_a_panic() {
+    assert_erl!(
+        r#"
+pub fn main() {
+  echo panic
+}
+"#
+    );
+}
+
+#[test]
+pub fn echo_with_a_function_call() {
+    assert_erl!(
+        r#"
+pub fn main() {
+  echo wibble(1, 2)
+}
+
+fn wibble(n: Int, m: Int) { n + m }
+"#
+    );
+}
+
+#[test]
+pub fn echo_with_a_block() {
+    assert_erl!(
+        r#"
+pub fn main() {
+  echo {
+    Nil
+    1
+  }
+}
+"#
+    );
+}
+
+#[test]
+pub fn echo_in_a_pipeline() {
+    assert_erl!(
+        r#"
+pub fn main() {
+  [1, 2, 3]
+  |> echo
+  |> wibble
+}
+
+pub fn wibble(n) { n }
+"#
+    )
+}
+
+#[test]
+pub fn multiple_echos_in_a_pipeline() {
+    assert_erl!(
+        r#"
+pub fn main() {
+  [1, 2, 3]
+  |> echo
+  |> wibble
+  |> echo
+  |> wibble
+  |> echo
+}
+
+pub fn wibble(n) { n }
+"#
+    )
+}
