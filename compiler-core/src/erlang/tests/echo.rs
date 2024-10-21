4920aafa5d174dc59d884f3aa4949117c7d29345
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2024-10-21 19:12:22 +0200

    improve erl snapshots

--- compiler-core/src/erlang/tests/echo.rs
+++ compiler-core/src/erlang/tests/echo.rs
@@ -1,8 +1,24 @@
-use crate::assert_erl;
+macro_rules! assert_generic_echo {
+    ($src:expr $(,)?) => {{
+        let output = $crate::erlang::tests::compile_test_project(
+            $src,
+            "/root/project/test/my/mod.gleam",
+            None,
+        );
+        let output = output
+            .strip_suffix(&format!(
+                "{}\n",
+                std::include_str!("../../../templates/echo.erl")
+            ))
+            .expect("contain generic echo code from `echo.erl`");
+        let output = format!("{output}\n\n...omitted code from `templates/echo.erl`...");
+        insta::assert_snapshot!(insta::internals::AutoName, output, $src);
+    }};
+}
 
 #[test]
 pub fn echo_with_a_simple_expression() {
-    assert_erl!(
+    assert_generic_echo!(
         r#"
 pub fn main() {
   echo 1
@@ -13,7 +29,7 @@ pub fn main() {
 
 #[test]
 pub fn multiple_echos_inside_expression() {
-    assert_erl!(
+    assert_generic_echo!(
         r#"
 pub fn main() {
   echo 1
@@ -25,7 +41,7 @@ pub fn main() {
 
 #[test]
 pub fn echo_with_a_case_expression() {
-    assert_erl!(
+    assert_generic_echo!(
         r#"
 pub fn main() {
   echo case 1 {
@@ -38,7 +54,7 @@ pub fn main() {
 
 #[test]
 pub fn echo_with_a_panic() {
-    assert_erl!(
+    assert_generic_echo!(
         r#"
 pub fn main() {
   echo panic
@@ -49,7 +65,7 @@ pub fn main() {
 
 #[test]
 pub fn echo_with_a_function_call() {
-    assert_erl!(
+    assert_generic_echo!(
         r#"
 pub fn main() {
   echo wibble(1, 2)
@@ -62,7 +78,7 @@ fn wibble(n: Int, m: Int) { n + m }
 
 #[test]
 pub fn echo_with_a_block() {
-    assert_erl!(
+    assert_generic_echo!(
         r#"
 pub fn main() {
   echo {
@@ -76,7 +92,7 @@ pub fn main() {
 
 #[test]
 pub fn echo_in_a_pipeline() {
-    assert_erl!(
+    assert_generic_echo!(
         r#"
 pub fn main() {
   [1, 2, 3]
@@ -91,7 +107,7 @@ pub fn wibble(n) { n }
 
 #[test]
 pub fn multiple_echos_in_a_pipeline() {
-    assert_erl!(
+    assert_generic_echo!(
         r#"
 pub fn main() {
   [1, 2, 3]
