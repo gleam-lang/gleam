4ef8a8b48c626579fe9041a1afaa567a2d12b35f
GearsDatapacks <surya@seriousinfinitude.com>
2024-10-22 23:59:48 +0100

    Add source code to missing error snapshots

--- compiler-core/src/parse/tests.rs
+++ compiler-core/src/parse/tests.rs
@@ -24,8 +24,9 @@ macro_rules! assert_error {
 
 macro_rules! assert_module_error {
     ($src:expr) => {
-        let result = $crate::parse::tests::expect_module_error($src);
-        insta::assert_snapshot!(insta::internals::AutoName, result, $src);
+        let error = $crate::parse::tests::expect_module_error($src);
+        let output = format!("----- SOURCE CODE\n{}\n\n----- ERROR\n{}", $src, error);
+        insta::assert_snapshot!(insta::internals::AutoName, output, $src);
     };
 }
 
