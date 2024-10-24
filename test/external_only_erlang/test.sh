f147270bdc3257cdaec93d6eada288ed428654de
Louis Pilfold <louis@lpil.uk>
2024-10-24 12:31:14 +0100

    Re-enable test

--- test/external_only_erlang/test.sh
+++ test/external_only_erlang/test.sh
@@ -5,8 +5,8 @@ set -eu
 GLEAM_COMMAND=${GLEAM_COMMAND:-"cargo run --quiet --"}
 
 g() {
-	echo "Running: $GLEAM_COMMAND $@"
-	$GLEAM_COMMAND "$@"
+  echo "Running: $GLEAM_COMMAND $@"
+  $GLEAM_COMMAND "$@"
 }
 
 echo Resetting the build directory to get to a known state
@@ -21,19 +21,16 @@ echo Building and running for Erlang should succeed
 g build --target=erlang
 g run --target=erlang
 
-# TODO: FIXME: Re-enable this one the compiler understand that the previous build
-#       cache cannot be used here.
-#
-# echo Building for JavaScript should fail, even if previously a JavaScript dependency was built
-# if g build --target=javascript; then
-# 	echo "Expected build to fail"
-# 	exit 1
-# fi
+echo Building for JavaScript should fail, even if previously a JavaScript dependency was built
+if g build --target=javascript; then
+  echo "Expected build to fail"
+  exit 1
+fi
 
 echo Running for JavaScript should fail, even if previously a JavaScript dependency was built
 if g run --target=javascript; then
-	echo "Expected run to fail"
-	exit 1
+  echo "Expected run to fail"
+  exit 1
 fi
 
 echo
