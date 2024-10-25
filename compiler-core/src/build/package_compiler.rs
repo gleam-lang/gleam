707dad9ba6f9396a0e8d791952f795b11c42ac55
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2024-10-25 18:48:38 +0200

    properly handle Dicts

--- compiler-core/src/build/package_compiler.rs
+++ compiler-core/src/build/package_compiler.rs
@@ -374,8 +374,11 @@ where
             TypeScriptDeclarations::None
         };
 
-        JavaScript::new(&self.out, typescript, prelude_location, self.target_support)
-            .render(&self.io, modules)?;
+        JavaScript::new(&self.out, typescript, prelude_location, self.target_support).render(
+            &self.io,
+            modules,
+            self.stdlib_is_a_dependency(),
+        )?;
 
         if self.copy_native_files {
             self.copy_project_native_files(&self.out, &mut written)?;
@@ -426,6 +429,10 @@ where
         }
 
         Ok(())
+    }
+
+    fn stdlib_is_a_dependency(&self) -> bool {
+        self.config.dependencies.contains_key("gleam_stdlib")
     }
 }
 
