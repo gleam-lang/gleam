98181fe8f873acb198f56e79eb5245e009aef7b2
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2024-10-28 17:24:05 +0100

    use current compiler version

--- test-echo-output/src/lib.rs
+++ test-echo-output/src/lib.rs
@@ -2,34 +2,19 @@
 mod generated_tests;
 
 use camino::Utf8PathBuf;
-use gleam_core::{
-    build::{
-        ErlangAppCodegenConfiguration, Mode, NullTelemetry, Outcome, StaleTracker, Target,
-        TargetCodegenConfiguration,
-    },
-    config::PackageConfig,
-    io::FileSystemWriter,
-    warning::{VectorWarningEmitterIO, WarningEmitter},
-};
 use std::{
-    collections::{HashMap, HashSet},
     fs,
     io::Read,
     process::{Command, Stdio},
-    rc::Rc,
 };
 
 pub fn prepare(path: &str) -> String {
     let root = Utf8PathBuf::from(path).canonicalize_utf8().unwrap();
     let code = fs::read_to_string(root.as_path().join("src").join("main.gleam")).expect("main");
-    fs::remove_dir_all(root.as_path().join("build")).expect("clean build dir");
+    let _ = fs::remove_dir_all(root.as_path().join("build"));
 
-    let mut cmd = Command::new("gleam")
-        .arg("run")
-        .arg("-mmain")
-        .arg("--no-print-progress")
-        .current_dir(&root)
-        .stderr(Stdio::piped())
+    let mut cmd = gleam_run(&root)
+        .arg("-terl")
         .spawn()
         .expect("gleam run -terl");
 
@@ -43,13 +28,9 @@ pub fn prepare(path: &str) -> String {
 
     output.push_str("\n--- OUTPUT ON JAVASCRIPT TARGET\n");
 
-    let mut cmd = Command::new("gleam")
-        .arg("run")
-        .arg("-mmain")
+    // "cargo run --quiet -- run --module=main --no-print-progress"
+    let mut cmd = gleam_run(&root)
         .arg("-tjs")
-        .arg("--no-print-progress")
-        .current_dir(&root)
-        .stderr(Stdio::piped())
         .spawn()
         .expect("gleam run -tjs");
 
@@ -60,67 +41,25 @@ pub fn prepare(path: &str) -> String {
         .expect("js stderr to string");
 
     output
+}
 
-    //let root = Utf8PathBuf::from(path).canonicalize_utf8().unwrap();
+fn gleam_run(project_dir: &Utf8PathBuf) -> Command {
+    let mut command = Command::new("cargo");
 
-    //let toml = std::fs::read_to_string(root.join("gleam.toml")).unwrap();
-    //let config: PackageConfig = toml::from_str(&toml).unwrap();
+    command
+        .arg("run")
+        .arg("--quiet")
+        // We're running in the `cases` directory so we have to get back three
+        // levels to the root of the project to run the compiler.
+        .arg("--manifest-path")
+        .arg("../../../Cargo.toml")
+        .arg("--")
+        .arg("run")
+        .arg("--module")
+        .arg("main")
+        .arg("--no-print-progress")
+        .current_dir(project_dir)
+        .stderr(Stdio::piped());
 
-    //let target = match config.target {
-    //    Target::Erlang => TargetCodegenConfiguration::Erlang {
-    //        app_file: Some(ErlangAppCodegenConfiguration {
-    //            include_dev_deps: true,
-    //            package_name_overrides: HashMap::new(),
-    //        }),
-    //    },
-    //    Target::JavaScript => TargetCodegenConfiguration::JavaScript {
-    //        emit_typescript_definitions: config.javascript.typescript_declarations,
-    //        prelude_location: Utf8PathBuf::from("../prelude.mjs"),
-    //    },
-    //};
-
-    //let ids = gleam_core::uid::UniqueIdGenerator::new();
-    //let mut modules = im::HashMap::new();
-    //let warnings = VectorWarningEmitterIO::default();
-    //let warning_emitter = WarningEmitter::new(Rc::new(warnings.clone()));
-    //let filesystem = test_helpers_rs::to_in_memory_filesystem(&root);
-    //let initial_files = filesystem.paths();
-    //let root = Utf8PathBuf::from("");
-    //let out = Utf8PathBuf::from("/out/lib/the_package");
-    //let lib = Utf8PathBuf::from("/out/lib");
-    //let mut compiler = gleam_core::build::PackageCompiler::new(
-    //    &config,
-    //    Mode::Dev,
-    //    &root,
-    //    &out,
-    //    &lib,
-    //    &target,
-    //    ids,
-    //    filesystem.clone(),
-    //);
-    //compiler.write_entrypoint = false;
-    //compiler.write_metadata = true;
-    //compiler.compile_beam_bytecode = false;
-    //compiler.copy_native_files = false;
-    //let result = compiler.compile(
-    //    &warning_emitter,
-    //    &mut modules,
-    //    &mut im::HashMap::new(),
-    //    &mut StaleTracker::default(),
-    //    &mut HashSet::new(),
-    //    &NullTelemetry,
-    //);
-    //match result {
-    //    Outcome::Ok(_) => {
-    //        for path in initial_files {
-    //            filesystem.delete_file(&path).unwrap();
-    //        }
-    //        let files = filesystem.into_contents();
-    //        let warnings = warnings.take();
-    //        test_helpers_rs::TestCompileOutput { files, warnings }.as_overview_text()
-    //    }
-    //    Outcome::TotalFailure(error) | Outcome::PartialFailure(_, error) => {
-    //        test_helpers_rs::normalise_diagnostic(&error.pretty_string())
-    //    }
-    //}
+    command
 }
