210c20fb79b13d654e3310faa6a3cdd107fe1af0
PgBiel <9021226+PgBiel@users.noreply.github.com>
2024-11-05 12:17:43 -0300

    use imfs subpaths for initial files in tests
    We were previously including all paths in the filesystem, which
    included, in particular, the root. Therefore, the "initial files"
    included the root directory, causing everything to be deleted, not only
    the initial files.

--- test-project-compiler/src/lib.rs
+++ test-project-compiler/src/lib.rs
@@ -15,7 +15,7 @@ use std::rc::Rc;
 pub fn prepare(path: &str, mode: Mode) -> String {
     let root = Utf8PathBuf::from(path).canonicalize_utf8().unwrap();
     let filesystem = test_helpers_rs::to_in_memory_filesystem(&root);
-    let initial_files = filesystem.paths();
+    let initial_files = filesystem.subpaths();
 
     let toml = std::fs::read_to_string(root.join("gleam.toml")).unwrap();
     let config: PackageConfig = toml::from_str(&toml).unwrap();
