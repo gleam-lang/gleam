db328877e6c309c4bc05893aa8bd157ac375c8c1
Laurent Arnoud <laurent.arnoud@platform.sh>
2024-11-05 12:38:55 +0100

    chore: improve file create in case of error
    follow up on https://github.com/gleam-lang/gleam/pull/3787#pullrequestreview-2415286300

--- compiler-cli/src/new/tests.rs
+++ compiler-cli/src/new/tests.rs
@@ -225,8 +225,8 @@ fn existing_directory_with_one_existing_file() {
 
     crate::fs::mkdir(&path).unwrap();
 
-    let _ = std::fs::File::create(PathBuf::from(&path).join("README.md"));
-    let _ = std::fs::File::create(PathBuf::from(&path).join("my_project.gleam"));
+    let _ = std::fs::File::create(PathBuf::from(&path).join("README.md")).unwrap();
+    let _ = std::fs::File::create(PathBuf::from(&path).join("my_project.gleam")).unwrap();
 
     assert!(super::Creator::new(
         super::NewOptions {
@@ -249,7 +249,7 @@ fn existing_directory_with_non_generated_file() {
     crate::fs::mkdir(&path).unwrap();
     let file_path = PathBuf::from(&path).join("some_fake_thing_that_is_not_generated.md");
 
-    let _ = std::fs::File::create(file_path);
+    let _ = std::fs::File::create(file_path).unwrap();
 
     let creator = super::Creator::new(
         super::NewOptions {
@@ -278,7 +278,7 @@ fn conflict_with_existing_files() {
 
     crate::fs::mkdir(&path).unwrap();
 
-    let _ = std::fs::File::create(PathBuf::from(&path).join("README.md"));
+    let _ = std::fs::File::create(PathBuf::from(&path).join("README.md")).unwrap();
 
     assert_eq!(
         super::Creator::new(
