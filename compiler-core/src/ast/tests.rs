8edf4af316f66a241f46a934cc1a99bfb2c2d249
GearsDatapacks <surya@seriousinfinitude.com>
2024-11-21 11:56:20 +0000

    Use bool instead

--- compiler-core/src/ast/tests.rs
+++ compiler-core/src/ast/tests.rs
@@ -547,7 +547,7 @@ fn find_node_bool() {
                 module: PRELUDE_MODULE_NAME.into(),
                 variant_index: 0,
             },
-            type_: type_::bool_with_variant(Some(0)),
+            type_: type_::bool_with_variant(Some(true)),
         },
         name: "True".into(),
     };
