86a3384959724360d688b6ee1dd5d6af3a909768
yoshi~ <jreusch4@gmail.com>
2024-11-29 00:29:22 +0100

    implement remove by using the new dependency unlocking mechanism
    fix #3887

--- compiler-cli/src/remove.rs
+++ compiler-cli/src/remove.rs
@@ -1,11 +1,12 @@
 use camino::{Utf8Path, Utf8PathBuf};
+use ecow::EcoString;
 
 use gleam_core::{
     error::{FileIoAction, FileKind},
     Error, Result,
 };
 
-use crate::{cli, fs, UseManifest};
+use crate::{cli, fs};
 
 pub fn command(packages: Vec<String>) -> Result<()> {
     // Read gleam.toml so we can remove deps from it
@@ -45,13 +46,13 @@ pub fn command(packages: Vec<String>) -> Result<()> {
     // Write the updated config
     fs::write(Utf8Path::new("gleam.toml"), &toml.to_string())?;
     let paths = crate::find_project_paths()?;
-    _ = crate::dependencies::download(
+
+    _ = crate::dependencies::remove(
         &paths,
         cli::Reporter::new(),
-        None,
-        Vec::new(),
-        UseManifest::Yes,
+        packages.iter().map(EcoString::from).collect(),
     )?;
+
     for package_to_remove in packages {
         cli::print_removed(&package_to_remove);
     }
