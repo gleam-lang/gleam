f98779643c61d07ad3aa3e7652d44c543ddddaa2
Diemo Gebhardt <me@diemogebhardt.com>
2024-11-30 16:12:56 +0100

    Refactor binary architecture verfifcation script and extract matching patterns for better understanding

--- bin/verify-binary-architecture.sh
+++ bin/verify-binary-architecture.sh
@@ -11,27 +11,30 @@ BINARY_PATH="$2"
 # Parse target architecture
 parse_target_architecture() {
   local target_triple="$1"
-  echo "$target_triple" \
-    | grep -Eo 'x86_64|aarch64' \
-    || echo "unknown target architecture"
+  local parse="x86_64|aarch64"
+  local error="unknown target architecture"
+  echo "$target_triple" | grep -Eo "$parse" || echo "$error"
 }
 TARGET_ARCHITECTURE=$(parse_target_architecture "$TARGET_TRIPLE")
 
 # Parse and normalize binary architecture
 parse_and_normalize_binary_architecture() {
   local binary_path="$1"
+  local parse="x86_64|x86-64|arm64|aarch64|Aarch64"
+  local normalize='s/x86-64/x86_64/;s/(arm64|Aarch64)/aarch64/'
+  local error="unknown binary architecture"
   file -b "$binary_path" \
-    | grep -Eo "x86_64|x86-64|arm64|aarch64|Aarch64" | head -n1 \
-    | sed -E 's/x86-64/x86_64/;s/(arm64|Aarch64)/aarch64/' \
-    || echo "unknown"
+  | grep -Eo "$parse" | head -n1 \
+  | sed -E "$normalize" \
+  || echo "$error"
 }
-BINARY_ARCHITECTURE=$(parse_and_normalize_binary_architecture "${BINARY_PATH}")
+BINARY_ARCHITECTURE=$(parse_and_normalize_binary_architecture "$BINARY_PATH")
 
 # Verify that binary architecture matches target architecture
 if [ "$BINARY_ARCHITECTURE" != "$TARGET_ARCHITECTURE" ]; then
-  echo "Architecture mismatch for '${TARGET_TRIPLE}'!"
-  echo "Expected: '${TARGET_ARCHITECTURE}'"
-  echo "Got: '${BINARY_ARCHITECTURE}'"
+  echo "Architecture mismatch for '$TARGET_TRIPLE'!"
+  echo "Expected: '$TARGET_ARCHITECTURE'"
+  echo "Got: '$BINARY_ARCHITECTURE'"
   exit 1
 fi
 echo "Architecture match for '$TARGET_TRIPLE'!"
