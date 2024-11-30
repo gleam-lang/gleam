9d07a5a4cb2272df2ae48f1683dd4a5e4ea90a1d
Diemo Gebhardt <me@diemogebhardt.com>
2024-11-30 19:27:45 +0100

    Simplify binary architecture verification script

--- bin/verify-binary-architecture.sh
+++ bin/verify-binary-architecture.sh
@@ -7,6 +7,7 @@ if [ $# -ne 2 ]; then
 fi
 TARGET_TRIPLE="$1"
 BINARY_PATH="$2"
+BINARY_FILE_TYPE=$(file -b "$BINARY_PATH")
 
 # Architecture patterns
 X86_64_PATTERNS='x86-64|x86_64'
@@ -15,24 +16,16 @@ AARCH64_PATTERNS='arm64|aarch64|Aarch64'
 # Architecture helper functions
 parse() { grep -Eo "$X86_64_PATTERNS|$AARCH64_PATTERNS" | head -n1; }
 normalize() { sed -E "s/($X86_64_PATTERNS)/x86-64/;s/($AARCH64_PATTERNS)/AArch64/"; }
-unknown_architecture_for() { echo "unknown $1 architecture"; }
 
-# Parse and normalize target architecture
-parse_and_normalize_target_architecture() {
-  local target_triple="$1"
-  get() { echo "$target_triple"; }
-  get | parse | normalize || unknown_architecture_for "target"
-}
-TARGET_ARCHITECTURE=$(parse_and_normalize_target_architecture "$TARGET_TRIPLE")
-
-# Parse and normalize binary architecture
-parse_and_normalize_binary_architecture() {
-  local binary_path="$1"
-  debug_pipe() { tee >(cat >&2); }
-  get() { file -b "$binary_path" | debug_pipe; }
-  get | parse | normalize || unknown_architecture_for "binary"
-}
-BINARY_ARCHITECTURE=$(parse_and_normalize_binary_architecture "$BINARY_PATH")
+# Parse and normalize architectures
+TARGET_ARCHITECTURE=$(
+  echo "$TARGET_TRIPLE" | parse | normalize \
+  || echo "unknown target architecture"
+)
+BINARY_ARCHITECTURE=$(
+  echo "$BINARY_FILE_TYPE" | parse | normalize \
+  || echo "unknown binary architecture"
+)
 
 # Verify that binary architecture matches target architecture
 if [ "$BINARY_ARCHITECTURE" != "$TARGET_ARCHITECTURE" ]; then
