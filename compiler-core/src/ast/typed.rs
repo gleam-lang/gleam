5091a444f9c78878273ffff97027b11dd7aa8fba
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2025-01-03 10:09:12 +0100

    Add missing branch for echo

--- compiler-core/src/ast/typed.rs
+++ compiler-core/src/ast/typed.rs
@@ -730,6 +730,7 @@ impl TypedExpr {
             | TypedExpr::TupleIndex { .. }
             | TypedExpr::Todo { .. }
             | TypedExpr::Panic { .. }
+            | TypedExpr::Echo { .. }
             | TypedExpr::BitArray { .. }
             | TypedExpr::RecordUpdate { .. }
             | TypedExpr::NegateBool { .. }
