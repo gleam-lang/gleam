21e8551f9bd55e14ee85fae0ca70c1f94ca9940d
GearsDatapacks <surya@seriousinfinitude.com>
2025-02-24 14:21:43 +0000

    Fix rename location

--- compiler-core/src/language_server/engine.rs
+++ compiler-core/src/language_server/engine.rs
@@ -593,10 +593,11 @@ where
                     constructor:
                         ValueConstructor {
                             variant:
-                                ValueConstructorVariant::ModuleConstant { location, .. }
-                                | ValueConstructorVariant::ModuleFn { location, .. },
+                                ValueConstructorVariant::ModuleConstant { .. }
+                                | ValueConstructorVariant::ModuleFn { .. },
                             ..
                         },
+                    location,
                     ..
                 }) => success_response(*location),
                 _ => None,
