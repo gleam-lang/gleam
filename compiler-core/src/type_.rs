1f6bd922c972c9e58cc746c32c8a1b0b4cb926cf
Lioncat2002 <sankhayan2002@gmail.com>
2025-02-27 01:31:58 +0530

    feat: added warnings for deprecation and usage of internals

--- compiler-core/src/type_.rs
+++ compiler-core/src/type_.rs
@@ -863,7 +863,7 @@ impl ModuleValueConstructor {
 #[derive(Debug, Clone)]
 pub struct ModuleFunction {
     pub package: EcoString,
-    pub is_deprecated: Deprecation
+    pub deprecation: Deprecation
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -991,7 +991,7 @@ impl ModuleInterface {
 
         Ok(ModuleFunction {
             package: self.package.clone(),
-            is_deprecated: value.deprecation.clone(),
+            deprecation: value.deprecation.clone(),
         })
     }
 
