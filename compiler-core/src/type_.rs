f53dca9612a1de60c72c8e665330dc1971d24b6d
Lioncat2002 <sankhayan2002@gmail.com>
2025-02-27 00:16:37 +0530

    refactor: change type of is_deprecated to Deprecation

--- compiler-core/src/type_.rs
+++ compiler-core/src/type_.rs
@@ -885,7 +885,7 @@ impl ModuleValueConstructor {
 #[derive(Debug, Clone)]
 pub struct ModuleFunction {
     pub package: EcoString,
-    pub is_deprecated: bool
+    pub is_deprecated: Deprecation
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -1036,7 +1036,7 @@ impl ModuleInterface {
 
         Ok(ModuleFunction {
             package: self.package.clone(),
-            is_deprecated: false,
+            is_deprecated: value.deprecation.clone(),
         })
     }
 
