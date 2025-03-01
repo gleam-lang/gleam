5cece81bf432132ab50c0e3b9e5c70a0c8ea39b8
Lioncat2002 <sankhayan2002@gmail.com>
2025-03-01 17:20:42 +0530

    fix: failing checks

--- compiler-core/src/type_.rs
+++ compiler-core/src/type_.rs
@@ -863,7 +863,7 @@ impl ModuleValueConstructor {
 #[derive(Debug, Clone)]
 pub struct ModuleFunction {
     pub package: EcoString,
-    pub deprecation: Deprecation
+    pub deprecation: Deprecation,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
