f9411464339a88dcd8a92e4f9c41c09cae5b5fe4
ascandone <alescandone@gmail.com>
2025-03-06 20:08:46 +0100

    fixes after rebase

--- compiler-core/src/ast/typed.rs
+++ compiler-core/src/ast/typed.rs
@@ -205,7 +205,8 @@ impl TypedExpr {
             | TypedExpr::RecordUpdate { .. }
             | TypedExpr::NegateBool { .. }
             | TypedExpr::NegateInt { .. }
-            | TypedExpr::Invalid { .. } => false,
+            | TypedExpr::Invalid { .. }
+            | TypedExpr::Echo { .. } => false,
         }
     }
 
