0030bb16351a316843e68d311019fb7c95014312
Rodrigo Álvarez <papipo@gmail.com>
2025-03-06 17:40:25 +0100

    Add export package-info to CLI

--- compiler-core/src/docs.rs
+++ compiler-core/src/docs.rs
@@ -479,6 +479,15 @@ pub fn generate_json_package_interface(
     }
 }
 
+pub fn generate_json_package_info(path: Utf8PathBuf, config: &PackageConfig) -> OutputFile {
+    OutputFile {
+        path,
+        content: Content::Text(
+            serde_json::to_string(&config).expect("JSON module interface serialisation"),
+        ),
+    }
+}
+
 fn page_unnest(path: &str) -> String {
     let unnest = path
         .strip_prefix('/')
