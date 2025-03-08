0facb505fb1ceec108fb7e76bea64799f0d549af
ascandone <alescandone@gmail.com>
2025-03-08 01:05:04 +0100

    more granular config and fixed tests regression

--- compiler-core/src/language_server/configuration.rs
+++ compiler-core/src/language_server/configuration.rs
@@ -5,13 +5,21 @@ pub type SharedConfig = Arc<RwLock<Configuration>>;
 
 #[derive(Debug, Default, Clone, Deserialize, PartialEq, Eq)]
 #[serde(default)]
+#[serde(rename_all = "camelCase")]
 pub struct Configuration {
     pub inlay_hints: InlayHintsConfig,
 }
 
 #[derive(Debug, Default, Clone, Deserialize, PartialEq, Eq)]
 #[serde(default)]
+#[serde(rename_all = "camelCase")]
 pub struct InlayHintsConfig {
     /// Whether to show type inlay hints of multiline pipelines
     pub pipelines: bool,
+
+    /// Whether to show type inlay hints of function parameters
+    pub parameter_types: bool,
+
+    /// Whether to show type inlay hints of return types of functions
+    pub return_types: bool,
 }
