2648cd4d3feb06dd0c0be4bbc2177ece20a36069
GearsDatapacks <surya@seriousinfinitude.com>
2025-03-09 16:42:44 +0000

    Add generated proto files to src

--- src/proto.rs
+++ src/proto.rs
@@ -1,13 +1,5 @@
 #![allow(clippy::enum_variant_names)]
 
-pub mod package {
-    include!(concat!(env!("OUT_DIR"), "/package.rs"));
-}
-
-pub mod signed {
-    include!(concat!(env!("OUT_DIR"), "/signed.rs"));
-}
-
-pub mod versions {
-    include!(concat!(env!("OUT_DIR"), "/versions.rs"));
-}
+pub mod package;
+pub mod signed;
+pub mod versions;
