fe125014ced33aa628f4ee86d18637c089b7122a
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2025-03-31 18:00:06 +0200

    rebase gone wrong

--- compiler-core/src/exhaustiveness.rs
+++ compiler-core/src/exhaustiveness.rs
@@ -96,7 +96,6 @@ use num_bigint::BigInt;
 use radix_trie::{Trie, TrieCommon};
 use std::{
     cell::RefCell,
-    cmp,
     collections::{HashMap, HashSet, VecDeque},
     hash::Hash,
     sync::Arc,
@@ -2532,4 +2531,3 @@ fn superset(one: &ImHashMap<EcoString, usize>, other: &ImHashMap<EcoString, usiz
             None => false,
         })
 }
-
