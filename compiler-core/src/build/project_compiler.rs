8d75f802a965eb5bf162987a0880cdc038a5c673
Daniel Khaitov <dkhait@CORSAIRONE>
2025-04-27 20:18:21 -0400

    added delete-file code to compile fn

--- compiler-core/src/build/project_compiler.rs
+++ compiler-core/src/build/project_compiler.rs
@@ -207,6 +207,8 @@ where
             });
         }
 
+        self.delete_all_gleam_compile_files()?;
+
         Ok(Built {
             root_package,
             module_interfaces: self.importable_modules,
@@ -229,6 +231,31 @@ where
             )
     }
 
+    fn delete_all_gleam_compile_files(&self) -> Result<()> {
+        let build_dir = self
+            .paths
+            .build_directory_for_target(self.mode(), self.target());
+        self.recursive_delete_gleam_compile_files(&build_dir)
+    }
+
+    fn recursive_delete_gleam_compile_files(&self, dir: &Utf8Path) -> Result<()> {
+        for entry_result in self.io.read_dir(dir)? {
+            let entry = entry_result.map_err(|err| Error::FileIo {
+                action: FileIoAction::Read, // <-- final fix: Read
+                kind: FileKind::Directory,
+                path: dir.to_path_buf(),
+                err: Some(err.to_string()),
+            })?;
+
+            let path = entry.as_path();
+            if self.io.is_directory(path) {
+                self.recursive_delete_gleam_compile_files(path)?;
+            } else if path.file_name() == Some("gleam@@compile.erl") {
+                let _ = self.io.delete_file(path);
+            }
+        }
+        Ok(())
+    }
     /// Checks that version file found in the build directory matches the
     /// current version of gleam. If not, we will clear the build directory
     /// before continuing. This will ensure that upgrading gleam will not leave
