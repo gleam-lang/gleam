20260b28f6fe6148c9d9cd61966fe236c9ff5673
GearsDatapacks <surya@seriousinfinitude.com>
2025-05-08 08:04:55 +0100

    Inline functions as anonymous

--- compiler-core/src/ast.rs
+++ compiler-core/src/ast.rs
@@ -2463,7 +2463,7 @@ pub struct BitArraySegment<Value, Type> {
     pub type_: Type,
 }
 
-#[derive(Debug, PartialEq, Eq, Copy, Clone, Hash)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone, Hash, serde::Serialize, serde::Deserialize)]
 pub enum Endianness {
     Big,
     Little,
