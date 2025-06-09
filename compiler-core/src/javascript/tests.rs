69514227b720089ba7b5fb5b4abb19e6c801133e
GearsDatapacks <surya@seriousinfinitude.com>
2025-06-09 10:38:14 +0100

    Disable inlining for most snapshots

--- compiler-core/src/javascript/tests.rs
+++ compiler-core/src/javascript/tests.rs
@@ -43,7 +43,7 @@ pub static CURRENT_PACKAGE: &str = "thepackage";
 macro_rules! assert_js {
     ($(($name:literal, $module_src:literal)),+, $src:literal $(,)?) => {
         let compiled =
-            $crate::javascript::tests::compile_js($src, vec![$(($crate::javascript::tests::CURRENT_PACKAGE, $name, $module_src)),*]);
+            $crate::javascript::tests::compile_js($src, vec![$(($crate::javascript::tests::CURRENT_PACKAGE, $name, $module_src)),*], false);
             let mut output = String::from("----- SOURCE CODE\n");
             for (name, src) in [$(($name, $module_src)),*] {
                 output.push_str(&format!("-- {name}.gleam\n{src}\n\n"));
@@ -54,7 +54,17 @@ macro_rules! assert_js {
 
     (($dep_package:expr, $dep_name:expr, $dep_src:expr), $src:expr $(,)?) => {{
         let compiled =
-            $crate::javascript::tests::compile_js($src, vec![($dep_package, $dep_name, $dep_src)]);
+            $crate::javascript::tests::compile_js($src, vec![($dep_package, $dep_name, $dep_src)], false);
+        let output = format!(
+            "----- SOURCE CODE\n{}\n\n----- COMPILED JAVASCRIPT\n{}",
+            $src, compiled
+        );
+        insta::assert_snapshot!(insta::internals::AutoName, output, $src);
+    }};
+
+    (@inline ($dep_package:expr, $dep_name:expr, $dep_src:expr), $src:expr $(,)?) => {{
+        let compiled =
+            $crate::javascript::tests::compile_js($src, vec![($dep_package, $dep_name, $dep_src)], true);
         let output = format!(
             "----- SOURCE CODE\n{}\n\n----- COMPILED JAVASCRIPT\n{}",
             $src, compiled
@@ -64,13 +74,13 @@ macro_rules! assert_js {
 
     (($dep_package:expr, $dep_name:expr, $dep_src:expr), $src:expr, $js:expr $(,)?) => {{
         let output =
-            $crate::javascript::tests::compile_js($src, Some(($dep_package, $dep_name, $dep_src)));
+            $crate::javascript::tests::compile_js($src, Some(($dep_package, $dep_name, $dep_src)), false);
         assert_eq!(($src, output), ($src, $js.to_string()));
     }};
 
     ($src:expr $(,)?) => {{
         let compiled =
-            $crate::javascript::tests::compile_js($src, vec![]);
+            $crate::javascript::tests::compile_js($src, vec![], false);
         let output = format!(
             "----- SOURCE CODE\n{}\n\n----- COMPILED JAVASCRIPT\n{}",
             $src, compiled
@@ -80,7 +90,7 @@ macro_rules! assert_js {
 
     ($src:expr, $js:expr $(,)?) => {{
         let output =
-            $crate::javascript::tests::compile_js($src, vec![]);
+            $crate::javascript::tests::compile_js($src, vec![], false);
         assert_eq!(($src, output), ($src, $js.to_string()));
     }};
 }
@@ -122,7 +132,7 @@ macro_rules! assert_ts_def {
     }};
 }
 
-pub fn compile(src: &str, deps: Vec<(&str, &str, &str)>) -> TypedModule {
+pub fn compile(src: &str, deps: Vec<(&str, &str, &str)>, perform_inlining: bool) -> TypedModule {
     let mut modules = im::HashMap::new();
     let ids = UniqueIdGenerator::new();
     // DUPE: preludeinsertion
@@ -186,11 +196,15 @@ pub fn compile(src: &str, deps: Vec<(&str, &str, &str)>) -> TypedModule {
     .infer_module(ast, line_numbers, "src/module.gleam".into())
     .expect("should successfully infer");
 
-    inline::module(module, &modules)
+    if perform_inlining {
+        inline::module(module, &modules)
+    } else {
+        module
+    }
 }
 
-pub fn compile_js(src: &str, deps: Vec<(&str, &str, &str)>) -> String {
-    let ast = compile(src, deps);
+pub fn compile_js(src: &str, deps: Vec<(&str, &str, &str)>, perform_inlining: bool) -> String {
+    let ast = compile(src, deps, perform_inlining);
     let line_numbers = LineNumbers::new(src);
     let stdlib_package = StdlibPackage::Present;
     let output = module(ModuleConfig {
@@ -210,6 +224,6 @@ pub fn compile_js(src: &str, deps: Vec<(&str, &str, &str)>) -> String {
 }
 
 pub fn compile_ts(src: &str, deps: Vec<(&str, &str, &str)>) -> String {
-    let ast = compile(src, deps);
+    let ast = compile(src, deps, false);
     ts_declaration(&ast)
 }
