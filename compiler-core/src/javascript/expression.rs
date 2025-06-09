388838170de48a988f11d5c5cd07a863961682c6
GearsDatapacks <surya@seriousinfinitude.com>
2025-06-09 10:45:38 +0100

    Dr. Clippy loves me now

--- compiler-core/src/javascript/expression.rs
+++ compiler-core/src/javascript/expression.rs
@@ -1274,8 +1274,6 @@ impl<'module, 'a> Generator<'module, 'a> {
                 name,
                 ..
             } => {
-                dbg!(name);
-                dbg!(type_);
                 if type_.is_result_constructor() {
                     if name == "Ok" {
                         self.tracker.ok_used = true;
@@ -2470,8 +2468,6 @@ pub(crate) fn record_constructor<'a>(
     arity: u16,
     tracker: &mut UsageTracker,
 ) -> Document<'a> {
-    dbg!(&type_, name);
-
     if qualifier.is_none() && type_.is_result_constructor() {
         if name == "Ok" {
             tracker.ok_used = true;
