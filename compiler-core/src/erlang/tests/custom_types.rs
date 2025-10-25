103c2615f385c11e77364e30e9f23fa640a9d1df
Gears <gearsOfficial@proton.me>
2025-10-25 15:26:59 +0100

    Reexport Erlang types for externally annotated types

--- compiler-core/src/erlang/tests/custom_types.rs
+++ compiler-core/src/erlang/tests/custom_types.rs
@@ -4,3 +4,26 @@ use crate::assert_erl;
 fn phantom() {
     assert_erl!("pub type Map(k, v)");
 }
+
+#[test]
+fn annotated_external_type() {
+    assert_erl!(
+        r#"
+@external(erlang, "gleam_stdlib", "dict")
+pub type Dict(key, value)
+"#
+    );
+}
+
+#[test]
+fn annotated_external_type_used_in_function() {
+    assert_erl!(
+        r#"
+@external(erlang, "gleam_stdlib", "dict")
+pub type Dict(key, value)
+
+@external(erlang, "maps", "get")
+pub fn get(dict: Dict(key, value), key: key) -> Result(value, Nil)
+"#
+    );
+}
