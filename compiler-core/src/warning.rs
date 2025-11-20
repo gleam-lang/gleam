baeab704c21b03417831f150273e1be644297049
Kevin Chen <chen.kevin.f@gmail.com>
2025-11-20 02:24:27 -0500

    refactor: shortens test names

--- compiler-core/src/warning.rs
+++ compiler-core/src/warning.rs
@@ -1572,7 +1572,7 @@ fn pluralise(string: String, quantity: i64) -> String {
 }
 
 #[test]
-fn warning_deprecated_main_function_pretty() {
+fn deprecated_main_function_pretty() {
     let warning = Warning::DeprecatedMainFunction {
         module: "apple".into(),
         message: "Use some other method please!".into()
@@ -1585,7 +1585,7 @@ fn warning_deprecated_main_function_pretty() {
 }
 
 #[test]
-fn warning_internal_module_main_function_pretty() {
+fn internal_module_main_function_pretty() {
     let warning: Warning = Warning::InternalModuleMainFunction {
         module: "internal/picking".into(),
     };
