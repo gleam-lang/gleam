3bda4ede676dab487f2a400bb0f6936b082a67bc
Pedro Henrique Francisco <75535904+mine-tech-oficial@users.noreply.github.com>
2025-12-18 12:39:36 -0300

    Fixed typos

--- language-server/src/code_action.rs
+++ language-server/src/code_action.rs
@@ -1371,7 +1371,7 @@ pub fn code_action_import_module_for_type_or_value(
                             new_text: suggestion.last_name_component().to_string() + ".",
                         },
                     ],
-                    &format!("Import `{module_name}` and qualifier"),
+                    &format!("Import `{module_name}` and qualify it"),
                 ),
                 ModuleSuggestion::Imported(module_name) => {
                     let matching_import = module
@@ -1433,7 +1433,7 @@ pub fn code_action_import_module_for_type_or_value(
 
                     (
                         add_unqualified_import(name, layer, module, import, line_numbers),
-                        &format!("Import `{name}` from `{module_name}"),
+                        &format!("Import `{name}` from `{module_name}`"),
                     )
                 }
             };
