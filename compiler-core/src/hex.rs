4687c92b462d02a1622971d5c214bc52a8da59e3
Gears <gearsOfficial@proton.me>
2026-01-21 22:15:47 +0000

    Fix global caching issue for republished versions

--- compiler-core/src/hex.rs
+++ compiler-core/src/hex.rs
@@ -191,10 +191,7 @@ impl Downloader {
             }
         };
 
-        let tarball_path = paths::global_package_cache_package_tarball(
-            &package.name,
-            &package.version.to_string(),
-        );
+        let tarball_path = paths::global_package_cache_package_tarball(outer_checksum);
         if self.fs_reader.is_file(&tarball_path) {
             tracing::info!(
                 package = package.name.as_str(),
@@ -232,22 +229,32 @@ impl Downloader {
         package: &ManifestPackage,
     ) -> Result<bool> {
         let _ = self.ensure_package_downloaded(package).await?;
-        self.extract_package_from_cache(&package.name, &package.version)
+        self.extract_package_from_cache(package)
     }
 
     // It would be really nice if this was async but the library is sync
-    pub fn extract_package_from_cache(&self, name: &str, version: &Version) -> Result<bool> {
+    pub fn extract_package_from_cache(&self, package: &ManifestPackage) -> Result<bool> {
         let contents_path = Utf8Path::new("contents.tar.gz");
-        let destination = self.paths.build_packages_package(name);
+        let destination = self.paths.build_packages_package(&package.name);
+
+        let outer_checksum = match &package.source {
+            ManifestPackageSource::Hex { outer_checksum } => outer_checksum,
+            ManifestPackageSource::Git { .. } | ManifestPackageSource::Local { .. } => {
+                panic!("Attempt to download non-hex package from hex")
+            }
+        };
 
         // If the directory already exists then there's nothing for us to do
         if self.fs_reader.is_directory(&destination) {
-            tracing::info!(package = name, "Package already in build directory");
+            tracing::info!(
+                package = package.name.as_str(),
+                "Package already in build directory"
+            );
             return Ok(false);
         }
 
-        tracing::info!(package = name, "writing_package_to_target");
-        let tarball = paths::global_package_cache_package_tarball(name, &version.to_string());
+        tracing::info!(package = package.name.as_str(), "writing_package_to_target");
+        let tarball = paths::global_package_cache_package_tarball(outer_checksum);
         let reader = self.fs_reader.reader(&tarball)?;
         let mut archive = Archive::new(reader);
 
