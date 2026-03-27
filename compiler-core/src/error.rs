a36f9a420d82aff7852ab1a5c6c9d3a4aa4b3cf4
Andrey <ankddev@duck.com>
2026-03-27 17:09:50 +0000

    fix: wrong formatting of error
    I've misplaced newly created destination parameter :smile:

--- compiler-core/src/error.rs
+++ compiler-core/src/error.rs
@@ -1632,8 +1632,8 @@ Erlang modules must have unique names regardless of the subfolders where their
 {}",
                     action.text(),
                     kind.text(),
-                    path,
                     action.destination(),
+                    path,
                     err,
                 );
                 if cfg!(target_family = "windows") && matches!(action, &FileIoAction::Link(..)) {
