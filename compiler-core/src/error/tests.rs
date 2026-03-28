3fe6beb0e9e07a3d1d85e8b8f61e77f5eafb09f9
Andrey <ankddev@duck.com>
2026-03-28 07:24:41 +0000

    test: add more tests

--- compiler-core/src/error/tests.rs
+++ compiler-core/src/error/tests.rs
@@ -71,3 +71,111 @@ fn io_delete_file_error() {
     .pretty_string();
     assert_snapshot!(error);
 }
+
+#[test]
+fn io_open_file_error() {
+    let error = Error::FileIo {
+        kind: FileKind::File,
+        action: FileIoAction::Open,
+        path: "/file".into(),
+        err: Some("Critical error!".to_owned()),
+    }
+    .pretty_string();
+    assert_snapshot!(error);
+}
+
+#[test]
+fn io_parse_file_error() {
+    let error = Error::FileIo {
+        kind: FileKind::File,
+        action: FileIoAction::Parse,
+        path: "/file".into(),
+        err: Some("Critical error!".to_owned()),
+    }
+    .pretty_string();
+    assert_snapshot!(error);
+}
+
+#[test]
+fn io_read_file_error() {
+    let error = Error::FileIo {
+        kind: FileKind::File,
+        action: FileIoAction::Read,
+        path: "/file".into(),
+        err: Some("Critical error!".to_owned()),
+    }
+    .pretty_string();
+    assert_snapshot!(error);
+}
+
+#[test]
+fn io_create_directory_error() {
+    let error = Error::FileIo {
+        kind: FileKind::Directory,
+        action: FileIoAction::Create,
+        path: "/dir".into(),
+        err: Some("Critical error!".to_owned()),
+    }
+    .pretty_string();
+    assert_snapshot!(error);
+}
+
+#[test]
+fn io_write_to_file_error() {
+    let error = Error::FileIo {
+        kind: FileKind::File,
+        action: FileIoAction::WriteTo,
+        path: "/file".into(),
+        err: Some("Critical error!".to_owned()),
+    }
+    .pretty_string();
+    assert_snapshot!(error);
+}
+
+#[test]
+fn io_find_parent_of_directory_error() {
+    let error = Error::FileIo {
+        kind: FileKind::Directory,
+        action: FileIoAction::FindParent,
+        path: "/dir".into(),
+        err: Some("Critical error!".to_owned()),
+    }
+    .pretty_string();
+    assert_snapshot!(error);
+}
+
+#[test]
+fn io_canonicalise_file_error() {
+    let error = Error::FileIo {
+        kind: FileKind::File,
+        action: FileIoAction::Canonicalise,
+        path: "/file".into(),
+        err: Some("Critical error!".to_owned()),
+    }
+    .pretty_string();
+    assert_snapshot!(error);
+}
+
+#[test]
+fn io_update_file_permissions_error() {
+    let error = Error::FileIo {
+        kind: FileKind::File,
+        action: FileIoAction::UpdatePermissions,
+        path: "/file".into(),
+        err: Some("Critical error!".to_owned()),
+    }
+    .pretty_string();
+    assert_snapshot!(error);
+}
+
+#[test]
+fn io_read_metadata_of_file_error() {
+    let error = Error::FileIo {
+        kind: FileKind::File,
+        action: FileIoAction::ReadMetadata,
+        path: "/file".into(),
+        err: Some("Critical error!".to_owned()),
+    }
+    .pretty_string();
+    assert_snapshot!(error);
+}
