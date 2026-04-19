f13701db4cb9e485a3cf214268af7a83f367a4cc
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2026-04-19 15:56:52 +0200

    add tests

--- compiler-core/src/type_/tests/errors.rs
+++ compiler-core/src/type_/tests/errors.rs
@@ -3594,3 +3594,32 @@ pub fn main() {
 "
     );
 }
+
+#[test]
+fn todo_in_a_constant_produces_an_error() {
+    assert_module_error!("pub const wibble = todo");
+}
+
+#[test]
+fn todo_constant_does_not_stop_analysis() {
+    assert_module_error!(
+        "pub const wibble = [todo, todo]
+
+pub fn main() -> List(Int) {
+  // This is not an error!!
+  wibble
+}"
+    );
+}
+
+#[test]
+fn todo_constant_does_not_stop_analysis_2() {
+    assert_module_error!(
+        "pub const wibble = [todo, todo]
+
+pub fn main() -> Int {
+  // This is an error!!
+  wibble
+}"
+    );
+}
