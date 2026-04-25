21df03541b3c2703181eafa68aa8fa5c5a6179f9
Daniele <lupo.develop@gmail.com>
2026-04-25 10:50:06 +0200

    Switch pretty-print tests to insta snapshots
    Replace hardcoded assert_eq! checks with insta::assert_snapshot!(output) in compiler-cli/src/dependencies/tests.rs for the pretty_print_outdated_versions tests. This moves the expected output into snapshot files.

--- compiler-cli/src/dependencies/tests.rs
+++ compiler-cli/src/dependencies/tests.rs
@@ -1483,10 +1483,7 @@ fn test_pretty_print_outdated_versions() {
 
     let output = pretty_print_outdated_versions(12, versions);
 
-    assert_eq!(
-        output,
-        "3 of 12 packages have newer versions available.\n\nPackage                 Current   Latest\n-------                 -------   ------\ngleam_stdlib            0.45.0    0.46.0\nvery_long_package_name  12.12.12  120.12.12\nwisp                    2.1.0     2.1.1\n"
-    );
+    insta::assert_snapshot!(output);
 }
 
 #[test]
@@ -1495,7 +1492,7 @@ fn test_pretty_print_outdated_versions_no_updates() {
 
     let output = pretty_print_outdated_versions(12, versions);
 
-    assert_eq!(output, "0 of 12 packages have newer versions available.\n");
+    insta::assert_snapshot!(output);
 }
 
 #[test]
