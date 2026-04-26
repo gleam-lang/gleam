1ea3c1f03f0e34cc8cf5ecea15e2802f64a5d2c2
Giacomo Cavalieri <giacomo.cavalieri@icloud.com>
2026-04-26 20:01:32 +0200

    make record inference fault tolerant

--- compiler-core/src/type_/tests/errors.rs
+++ compiler-core/src/type_/tests/errors.rs
@@ -3580,3 +3580,17 @@ pub const wobble = Wibble(..wobble)
 fn qualified_type_with_no_name_results_in_an_error() {
     assert_module_error!("pub fn main() -> wibble. { todo }");
 }
+
+#[test]
+fn can_infer_record_fields_even_if_updated_record_does_not_exist() {
+    assert_module_error!(
+        "
+pub type Wibble { Wibble(a: Int, b: Int) }
+
+pub fn main() {
+  //                       v should see an error here
+  Wibble(..does_not_exist, a: True)
+}
+"
+    );
+}
