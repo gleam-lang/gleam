bf5ccf555a3b9c9b5e8e3bff335a2df9e84da1c3
Gavin Morrow <gavinfmorrow@gmail.com>
2026-05-03 16:15:13 -0400

    Add tests for finding references from import

--- language-server/src/tests/reference.rs
+++ language-server/src/tests/reference.rs
@@ -275,6 +275,30 @@ pub fn main() {
 }
 
 #[test]
+fn references_for_function_from_import() {
+    assert_references!(
+        (
+            "mod",
+            "
+pub fn wibble() {
+  wibble()
+}
+"
+        ),
+        "
+import mod.{wibble}
+
+pub fn main() {
+  let value = wibble()
+  mod.wibble()
+  value
+}
+",
+        find_position_of("wibble"),
+    );
+}
+
+#[test]
 fn references_for_private_constant() {
     assert_references!(
         "
@@ -387,6 +411,31 @@ pub fn main() {
 }
 ",
         find_position_of("wibble +"),
+    );
+}
+
+#[test]
+fn references_for_constant_from_import() {
+    assert_references!(
+        (
+            "mod",
+            "
+pub const wibble = 10
+
+fn wobble() {
+  wibble
+}
+"
+        ),
+        "
+import mod.{wibble}
+
+pub fn main() {
+  let value = mod.wibble
+  wibble + value
+}
+",
+        find_position_of("wibble"),
     );
 }
 
@@ -506,6 +555,31 @@ pub fn main() {
 }
 ",
         find_position_of("Wibble").nth_occurrence(3),
+    );
+}
+
+#[test]
+fn references_for_type_variant_from_import() {
+    assert_references!(
+        (
+            "mod",
+            "
+pub type Wibble { Wibble }
+
+fn wobble() {
+  Wibble
+}
+"
+        ),
+        "
+import mod.{Wibble}
+
+pub fn main() {
+  let value = mod.Wibble
+  Wibble
+}
+",
+        find_position_of("Wibble"),
     );
 }
 
@@ -704,6 +778,30 @@ pub fn main() -> Wibble {
 }
 ",
         find_position_of("Wibble").nth_occurrence(2),
+    );
+}
+
+#[test]
+fn references_for_type_from_import() {
+    assert_references!(
+        (
+            "mod",
+            "
+pub type Wibble { Wibble }
+
+fn wobble() -> Wibble {
+  todo
+}
+"
+        ),
+        "
+import mod.{type Wibble}
+
+pub fn main() -> Wibble {
+  let _: mod.Wibble = todo
+}
+",
+        find_position_of("Wibble"),
     );
 }
 
