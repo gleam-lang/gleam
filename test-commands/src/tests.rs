c075498f417cd6e007ef7c679a033b357bf82b60
Louis Pilfold <louis@lpil.uk>
2026-05-19 13:02:57 +0100

    Escript with dependencies test

--- test-commands/src/tests.rs
+++ test-commands/src/tests.rs
@@ -1,37 +1,49 @@
 use camino::Utf8PathBuf;
 use gleam_cli::{Command, ExportTarget, fs};
+use std::process;
 
-#[test]
-fn escript_success() {
-    let working_directory = Utf8PathBuf::from("./cases/escript_ok");
-    let escript_path = working_directory.join("escript_ok");
-
+fn escript_compile(case: &str) -> Result<Utf8PathBuf, gleam_core::Error> {
+    let working_directory = Utf8PathBuf::from(&format!("./cases/{case}"));
+    let escript_path = working_directory.join(case);
     fs::delete_file(&escript_path).expect("must be able to reset test directory");
 
     Command::Export(ExportTarget::Escript)
         .run(working_directory.clone())
-        .expect("should compile successfully");
+        .map(|_| escript_path)
+}
 
+fn assert_escript_compile(case: &str) -> Utf8PathBuf {
+    let escript_path = escript_compile(case).expect("should compile successfully");
     assert!(
         escript_path.exists() && escript_path.is_file(),
         "escript should have been created"
     );
+    escript_path
+}
 
-    assert!(
-        std::process::Command::new("escript")
-            .arg(escript_path)
-            .status()
-            .expect("should run successfully")
-            .success(),
-        "escrpt should be runnable"
-    )
+#[test]
+fn escript_success() {
+    let escript = assert_escript_compile("escript_ok");
+    let status = process::Command::new("escript")
+        .arg(escript)
+        .status()
+        .expect("executable escript");
+    assert!(status.success(), "escript should run OK")
+}
+
+#[test]
+fn escript_success_with_dependency() {
+    let escript = assert_escript_compile("escript_with_dependency");
+    let status = process::Command::new("escript")
+        .arg(escript)
+        .status()
+        .expect("executable escript");
+    assert!(status.success(), "escript should run OK")
 }
 
 #[test]
 fn escript_without_main_function() {
-    let working_directory = Utf8PathBuf::from("./cases/escript_without_main_function");
-    let error = Command::Export(ExportTarget::Escript)
-        .run(working_directory)
+    let error = escript_compile("escript_without_main_function")
         .expect_err("escripts require a main function")
         .pretty_string();
     insta::assert_snapshot!(error)
@@ -39,10 +51,8 @@ fn escript_without_main_function() {
 
 #[test]
 fn escript_with_wrong_arity_main_function() {
-    let working_directory = Utf8PathBuf::from("./cases/escript_with_wrong_arity_main_function");
-    let error = Command::Export(ExportTarget::Escript)
-        .run(working_directory)
-        .expect_err("escripts require zero arity main function")
+    let error = escript_compile("escript_with_wrong_arity_main_function")
+        .expect_err("escripts require a main function")
         .pretty_string();
     insta::assert_snapshot!(error)
 }
