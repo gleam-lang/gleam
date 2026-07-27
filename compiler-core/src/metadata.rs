7e12a231df2768f7bd7832b7f38480d50e7ae22e
Andrey <ankddev@duck.com>
2026-07-27 15:52:53 +0300

    migrate from bincode to bitcode

--- compiler-core/src/metadata.rs
+++ compiler-core/src/metadata.rs
@@ -22,16 +22,12 @@ use crate::{
     uid::UniqueIdGenerator,
 };
 
-pub fn encode(module: &ModuleInterface) -> Result<Vec<u8>, bincode::error::EncodeError> {
-    bincode::serde::encode_to_vec(module, bincode::config::legacy())
+pub fn encode(module: &ModuleInterface) -> Result<Vec<u8>, bitcode::Error> {
+    bitcode::serialize(module)
 }
 
-pub fn decode(
-    bytes: &[u8],
-    ids: UniqueIdGenerator,
-) -> Result<ModuleInterface, bincode::error::DecodeError> {
-    bincode::serde::decode_from_slice(bytes, bincode::config::legacy())
-        .map(|(module, _)| remap_type_variable_ids(module, ids))
+pub fn decode(bytes: &[u8], ids: UniqueIdGenerator) -> Result<ModuleInterface, bitcode::Error> {
+    bitcode::deserialize(bytes).map(|module| remap_type_variable_ids(module, ids))
 }
 
 fn remap_type_variable_ids(module: ModuleInterface, ids: UniqueIdGenerator) -> ModuleInterface {
