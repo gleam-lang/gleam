97ab87b5739e636f8da6879b4fdef85a1f7c5ea4
Rodrigo Álvarez <papipo@gmail.com>
2026-07-30 15:09:26 +0200

    Add test for erlang and elixir package compilation to beam

---
+++ test/compile_package2/src/three.ex
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2026 The Gleam contributors
+
+defmodule Three do
+  def hello(), do: "Hello from Elixir"
+end
---
+++ test/compile_package2/src/two.erl
@@ -0,0 +1,9 @@
+%% SPDX-License-Identifier: Apache-2.0
+%% SPDX-FileCopyrightText: 2021 The Gleam contributors
+
+- module(two).
+- export([hello/0]).
+
+hello() ->
+  "Hello from erlang".
+
