133b5837857bf7886b353c09d0ff1d7aa5037190
Rodrigo Álvarez <papipo@gmail.com>
2026-07-30 19:19:50 +0200

    Fix comment

--- test/compile_package2/src/three.ex
+++ test/compile_package2/src/three.ex
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: Apache-2.0
-// SPDX-FileCopyrightText: 2026 The Gleam contributors
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: 2026 The Gleam contributors
 
 defmodule Three do
   def hello(), do: "Hello from Elixir"
