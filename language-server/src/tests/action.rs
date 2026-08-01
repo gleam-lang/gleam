fa6962fdff73a1c81ccb35b1d6f4a5094db8e9d9
Andrey <ankddev@duck.com>
2026-08-01 13:07:49 +0300

    add tests for code action in bit array `size`

--- language-server/src/tests/action.rs
+++ language-server/src/tests/action.rs
@@ -16072,6 +16072,7 @@ fn convert_int_hexadecimal_to_decimal_in_constants() {
         find_position_of("0x6f").to_selection()
     );
 }
+
 #[test]
 fn convert_negative_int_decimal_to_binary_in_constants() {
     assert_code_action!(
@@ -16439,6 +16440,138 @@ fn convert_negative_int_hexadecimal_to_decimal_in_patterns() {
         "Convert to `-111`",
         "pub fn go(x) {
   let assert <<-0x6f>> = x
+}",
+        find_position_of("0x6f").to_selection()
+    );
+}
+
+#[test]
+fn convert_int_decimal_to_binary_in_size_options() {
+    assert_code_action!(
+        "Convert to `0b1101111`",
+        "pub fn go(x) {
+  let assert <<2:size(111)>> = x
+}",
+        find_position_of("111").to_selection()
+    );
+}
+
+#[test]
+fn convert_int_decimal_to_octal_in_size_options() {
+    assert_code_action!(
+        "Convert to `0o157`",
+        "pub fn go(x) {
+  let assert <<2:size(111)>> = x
+}",
+        find_position_of("111").to_selection()
+    );
+}
+
+#[test]
+fn convert_int_decimal_to_hexadecimal_in_size_options() {
+    assert_code_action!(
+        "Convert to `0x6f`",
+        "pub fn go(x) {
+  let assert <<2:size(111)>> = x
+}",
+        find_position_of("111").to_selection()
+    );
+}
+
+#[test]
+fn convert_int_binary_to_octal_in_size_options() {
+    assert_code_action!(
+        "Convert to `0o157`",
+        "pub fn go(x) {
+  let assert <<2:size(0b1101111)>> = x
+}",
+        find_position_of("0b1101111").to_selection()
+    );
+}
+
+#[test]
+fn convert_int_binary_to_decimal_in_size_options() {
+    assert_code_action!(
+        "Convert to `111`",
+        "pub fn go(x) {
+  let assert <<2:size(0b1101111)>> = x
+}",
+        find_position_of("0b1101111").to_selection()
+    );
+}
+
+#[test]
+fn convert_int_binary_to_hexadecimal_in_size_options() {
+    assert_code_action!(
+        "Convert to `0x6f`",
+        "pub fn go(x) {
+  let assert <<2:size(0b1101111)>> = x
+}",
+        find_position_of("0b1101111").to_selection()
+    );
+}
+
+#[test]
+fn convert_int_octal_to_binary_in_size_options() {
+    assert_code_action!(
+        "Convert to `0b1101111`",
+        "pub fn go(x) {
+  let assert <<2:size(0o157)>> = x
+}",
+        find_position_of("0o157").to_selection()
+    );
+}
+
+#[test]
+fn convert_int_octal_to_decimal_in_size_options() {
+    assert_code_action!(
+        "Convert to `111`",
+        "pub fn go(x) {
+  let assert <<2:size(0o157)>> = x
+}",
+        find_position_of("0o157").to_selection()
+    );
+}
+
+#[test]
+fn convert_int_octal_to_hexadecimal_in_size_options() {
+    assert_code_action!(
+        "Convert to `0x6f`",
+        "pub fn go(x) {
+  let assert <<2:size(0o157)>> = x
+}",
+        find_position_of("0o157").to_selection()
+    );
+}
+
+#[test]
+fn convert_int_hexadecimal_to_binary_in_size_options() {
+    assert_code_action!(
+        "Convert to `0b1101111`",
+        "pub fn go(x) {
+  let assert <<2:size(0x6f)>> = x
+}",
+        find_position_of("0x6f").to_selection()
+    );
+}
+
+#[test]
+fn convert_int_hexadecimal_to_octal_in_size_options() {
+    assert_code_action!(
+        "Convert to `0o157`",
+        "pub fn go(x) {
+  let assert <<2:size(0x6f)>> = x
+}",
+        find_position_of("0x6f").to_selection()
+    );
+}
+
+#[test]
+fn convert_int_hexadecimal_to_decimal_in_size_options() {
+    assert_code_action!(
+        "Convert to `111`",
+        "pub fn go(x) {
+  let assert <<2:size(0x6f)>> = x
 }",
         find_position_of("0x6f").to_selection()
     );
