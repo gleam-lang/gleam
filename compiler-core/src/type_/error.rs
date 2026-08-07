e2fb953e9faac195cf5dab85ce052e1c1fccd098
0xda157 <da157@voidq.com>
2026-08-06 17:07:30 -0700

    compiler-core: rename UnnecessarySpreadOperator to RecordUpdateWithoutChanges

--- compiler-core/src/type_/error.rs
+++ compiler-core/src/type_/error.rs
@@ -244,7 +244,7 @@ pub enum Error {
         reason: UnsafeRecordUpdateReason,
     },
 
-    UnnecessarySpreadOperator {
+    RecordUpdateWithoutChanges {
         location: SrcSpan,
         arity: usize,
     },
@@ -1396,7 +1396,7 @@ impl Error {
             | Error::UnknownRecordField { location, .. }
             | Error::IncorrectArity { location, .. }
             | Error::UnsafeRecordUpdate { location, .. }
-            | Error::UnnecessarySpreadOperator { location, .. }
+            | Error::RecordUpdateWithoutChanges { location, .. }
             | Error::IncorrectTypeArity { location, .. }
             | Error::TypeUsedAsAConstructor { location, .. }
             | Error::CouldNotUnify { location, .. }
