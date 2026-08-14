6ed5d9de5e1de88d76cc06199ed376deecf3a58f
Rodrigo Álvarez <papipo@gmail.com>
2026-08-14 14:25:17 +0200

    feat: add --prod flag to compile-package

--- compiler-cli/src/compile_package.rs
+++ compiler-cli/src/compile_package.rs
@@ -49,9 +49,11 @@ pub fn command(options: CompilePackage) -> Result<()> {
 
     tracing::info!("Compiling package");
 
+    let mode = if options.prod { Mode::Prod } else { Mode::Dev };
+
     let mut compiler = PackageCompiler::new(
         &config,
-        Mode::Dev,
+        mode,
         &options.package_directory,
         &options.output_directory,
         &options.libraries_directory,
