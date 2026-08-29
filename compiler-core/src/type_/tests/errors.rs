a93ccd836649d1f62f9e0b58177e366ba655a4e6
rebecca <84042103+yoshi-monster@users.noreply.github.com>
2026-08-29 20:08:28 +0200

    rustfmt

--- compiler-core/src/type_/tests/errors.rs
+++ compiler-core/src/type_/tests/errors.rs
@@ -2658,8 +2658,11 @@ const my_wobble: wibble.Wobble = Nil
 #[test]
 fn error_uses_unqualified_imported_type_alias() {
     assert_module_error!(
-        ("aliases", "pub type Aliased =
-  Result(Int, String)"),
+        (
+            "aliases",
+            "pub type Aliased =
+  Result(Int, String)"
+        ),
         r#"
 import aliases.{type Aliased}
 
