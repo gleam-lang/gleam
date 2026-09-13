dd573fbc8e18e6cac393963e09073096b24942a5
ImmConCon <somewhat.fluffy@gmail.com>
2026-09-13 02:49:03 +0200

    replace im with imbl in compiler-wasm

--- compiler-wasm/src/lib.rs
+++ compiler-wasm/src/lib.rs
@@ -19,7 +19,7 @@ use gleam_core::{
     warning::{VectorWarningEmitterIO, WarningEmitter},
 };
 use hexpm::version::Version;
-use im::HashMap;
+use imbl::HashMap;
 use std::{cell::RefCell, collections::HashSet, rc::Rc};
 use wasm_filesystem::WasmFileSystem;
 
@@ -180,8 +180,8 @@ pub fn format_source(source: &str) -> Result<String, String> {
 
 fn do_compile_package(project: Project, target: Target) -> Result<(), Error> {
     let ids = UniqueIdGenerator::new();
-    let mut type_manifests = im::HashMap::new();
-    let mut defined_modules = im::HashMap::new();
+    let mut type_manifests = imbl::HashMap::new();
+    let mut defined_modules = imbl::HashMap::new();
     let warning_emitter = WarningEmitter::new(Rc::new(project.warnings));
     let config = PackageConfig {
         name: "library".into(),
