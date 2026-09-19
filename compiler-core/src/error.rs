5a928da0be3bdc98255b5323741e3b52367e07f8
Vivid <absolutely.vivid@fastmail.org>
2026-09-19 02:40:11 +0000

    Document error::did_you_mean now that it's public

--- compiler-core/src/error.rs
+++ compiler-core/src/error.rs
@@ -946,6 +946,8 @@ fn edit_distance_with_substrings(a: &str, b: &str, limit: usize) -> Option<usize
     (score <= limit).then_some(score)
 }
 
+/// Returns a "Did you mean ...?" suggestion using the string from `options` that's most similar to
+/// `name`.
 pub fn did_you_mean(name: &str, options: &[EcoString]) -> Option<String> {
     // If only one option is given, return that option.
     // This seems to solve the `unknown_variable_3` test.
